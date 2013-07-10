2dd72342deda095a477811c976dda47831d9355b
Spence Green <spence@spencegreen.com>
2013-07-09 18:18:19 -0700

    Update jetty and gson

--- src/edu/stanford/nlp/dcoref/SieveCoreferenceSystem.java
+++ src/edu/stanford/nlp/dcoref/SieveCoreferenceSystem.java
@@ -1106,7 +1106,6 @@ public class SieveCoreferenceSystem {
             src.set(1,j);
 
             IntTuple ant = positions.get(antecedent);
-            if(ant==null) continue;
             //correct=(chosen==goldLinks.contains(new Pair<IntTuple, IntTuple>(src,ant)));
             boolean coreferent = golds.containsKey(m.mentionID)
             && golds.containsKey(antecedent.mentionID)
