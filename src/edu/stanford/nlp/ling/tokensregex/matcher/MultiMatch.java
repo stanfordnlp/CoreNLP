4d359078baaf62bce483a5a7192d32a0939341c0
Spence Green <spence@spencegreen.com>
2013-08-10 06:10:47 -0700

    Merge branch 'master' into fix-wspace

--- src/edu/stanford/nlp/ling/tokensregex/matcher/MultiMatch.java
+++ src/edu/stanford/nlp/ling/tokensregex/matcher/MultiMatch.java
@@ -1,7 +1,10 @@
 package edu.stanford.nlp.ling.tokensregex.matcher;
 
+import edu.stanford.nlp.util.HasInterval;
+import edu.stanford.nlp.util.Interval;
 import edu.stanford.nlp.util.StringUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,26 +13,48 @@ import java.util.List;
  * @author Angel Chang
  */
 public class MultiMatch<K,V> extends Match<K,V> {
-  List<List<K>> multimatched;
-  List<V> multivalues;
+  List<Match<K,V>> multimatches;
 
   public MultiMatch() {}
 
-  public MultiMatch(List<K> matched, V value, int begin, int end, List<List<K>> multimatched, List<V> multivalues) {
+  public MultiMatch(List<K> matched, V value, int begin, int end, List<Match<K,V>> multimatches) {
     this.matched = matched;
     this.value = value;
     this.begin = begin;
     this.end = end;
-    this.multimatched = multimatched;
-    this.multivalues = multivalues;
+    this.multimatches = multimatches;
+  }
+
+  public List<Match<K,V>> getMultimatches() {
+    return multimatches;
   }
 
   public List<List<K>> getMultimatched() {
+    if (multimatches == null) return null;
+    List<List<K>> multimatched = new ArrayList<List<K>>(multimatches.size());
+    for (Match<K,V> m:multimatches) {
+      multimatched.add(m.getMatched());
+    }
     return multimatched;
   }
 
   public List<V> getMultivalues() {
+    if (multimatches == null) return null;
+    List<V> multivalues = new ArrayList<V>(multimatches.size());
+    for (Match<K,V> m:multimatches) {
+      multivalues.add(m.getValue());
+    }
     return multivalues;
+  }
+
+  // Offsets in the original string to which each multimatch is aligned to
+  public List<HasInterval<Integer>> getMultioffsets() {
+    if (multimatches == null) return null;
+    List<HasInterval<Integer>> multioffsets = new ArrayList<HasInterval<Integer>>(multimatches.size());
+    for (Match<K,V> m:multimatches) {
+      multioffsets.add(m.getInterval());
+    }
+    return multioffsets;
   }
 
   @Override
@@ -40,27 +65,15 @@ public class MultiMatch<K,V> extends Match<K,V> {
 
     MultiMatch that = (MultiMatch) o;
 
-    if (multimatched != null ? !multimatched.equals(that.multimatched) : that.multimatched != null) return false;
-    if (multivalues != null ? !multivalues.equals(that.multivalues) : that.multivalues != null) return false;
+    if (multimatches != null ? !multimatches.equals(that.multimatches) : that.multimatches != null) return false;
 
     return true;
   }
 
-  @Override
-  public int hashCode() {
-    int result = super.hashCode();
-    result = 31 * result + (multimatched != null ? multimatched.hashCode() : 0);
-    result = 31 * result + (multivalues != null ? multivalues.hashCode() : 0);
-    return result;
-  }
-
   public String toString() {
     StringBuilder sb = new StringBuilder();
-    if (multimatched != null && multivalues != null) {
-      sb.append("[" + StringUtils.join(multimatched, "-") + "]");
-      sb.append(" -> ").append(StringUtils.join(multivalues, "-"));
-      sb.append(" at (").append(begin);
-      sb.append(",").append(end).append(")");
+    if (multimatches != null) {
+      sb.append("[" + StringUtils.join(getMultimatches(), ", ") + "]");
     } else {
       sb.append(super.toString());
     }
