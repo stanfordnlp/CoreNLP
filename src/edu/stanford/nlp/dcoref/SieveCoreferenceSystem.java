cbe1f4e86b12b68ad9ccc4ef2cc65df46d4d5bcc
Gabor Angeli <angeli@cs.stanford.edu>
2013-08-19 17:16:40 -0700

    Merge branch 'kbp-ir' of jacob:/u/nlp/git/javanlp into kbp-ir

--- src/edu/stanford/nlp/dcoref/SieveCoreferenceSystem.java
+++ src/edu/stanford/nlp/dcoref/SieveCoreferenceSystem.java
@@ -141,8 +141,9 @@ public class SieveCoreferenceSystem {
    * Array of sieve passes to be used in the system
    * Ordered from highest precision to lowest!
    */
+  /** Not final because may change when running optimize sieve ordering but otherwise should stay fixed */
   private /*final */DeterministicCorefSieve [] sieves;
-  public /*final*/ String [] sieveClassNames;
+  private /*final*/ String [] sieveClassNames;
 
   /**
    * Dictionaries of all the useful goodies (gender, animacy, number etc. lists)
@@ -152,27 +153,28 @@ public class SieveCoreferenceSystem {
   /**
    * Semantic knowledge: WordNet
    */
-  public final Semantics semantics;
+  private final Semantics semantics;
 
-  public LogisticClassifier<String, String> singletonPredictor;
+  private LogisticClassifier<String, String> singletonPredictor;
+
+  // Below are member variables used for scoring (not thread safe)
 
   /** Current sieve index */
-  public int currentSieve;
+  private int currentSieve;
 
   /** counter for links in passes (Pair<correct links, total links>)  */
-  public List<Pair<Integer, Integer>> linksCountInPass;
-
+  private List<Pair<Integer, Integer>> linksCountInPass;
 
   /** Scores for each pass */
-  public List<CorefScorer> scorePairwise;
-  public List<CorefScorer> scoreBcubed;
-  public List<CorefScorer> scoreMUC;
+  private List<CorefScorer> scorePairwise;
+  private List<CorefScorer> scoreBcubed;
+  private List<CorefScorer> scoreMUC;
 
   private List<CorefScorer> scoreSingleDoc;
 
   /** Additional scoring stats */
-  int additionalCorrectLinksCount;
-  int additionalLinksCount;
+  private int additionalCorrectLinksCount;
+  private int additionalLinksCount;
 
   public SieveCoreferenceSystem(Properties props) throws Exception {
     // initialize required fields
@@ -327,6 +329,8 @@ public class SieveCoreferenceSystem {
   public boolean doScore() { return doScore; }
   public Dictionaries dictionaries() { return dictionaries; }
   public Semantics semantics() { return semantics; }
+  public String sieveClassName(int sieveIndex)  {
+    return (sieveIndex >= 0 && sieveIndex < sieveClassNames.length)? sieveClassNames[sieveIndex]:null; }
 
   /**
    * Needs the following properties:
