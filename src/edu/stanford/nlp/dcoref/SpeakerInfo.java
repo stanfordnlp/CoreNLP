33d8fe6f7332d0c3c69f7098a54a16370bb54b5e
Gabor Angeli <angeli@cs.stanford.edu>
2013-09-17 13:06:05 -0700

    Renamed libraries to conform to the README

--- src/edu/stanford/nlp/dcoref/SpeakerInfo.java
+++ src/edu/stanford/nlp/dcoref/SpeakerInfo.java
@@ -17,13 +17,7 @@ public class SpeakerInfo {
   private String speakerName;
   private String[] speakerNameStrings; // tokenized speaker name
   private String speakerDesc;
-  // TODO: it is possible for this set to have Mentions with different
-  // corefClusterIds.  In that case, the results are actually not
-  // deterministic, since the cluster id of the speaker will depend on
-  // the order in which mentions are iterated over.  This will change
-  // from execution to execution in a HashSet.
   private Set<Mention> mentions = Generics.newHashSet();  // Mentions that corresponds to the speaker...
-  private Mention originalMention;            // the mention used when creating this SpeakerInfo
   private boolean speakerIdIsNumber;          // speaker id is a number (probably mention id)
   private boolean speakerIdIsAutoDetermined;  // speaker id was auto determined by system
   private Mention mainMention;
@@ -32,9 +26,8 @@ public class SpeakerInfo {
 
   private static final Pattern DEFAULT_SPEAKER_PATTERN = Pattern.compile("PER\\d+");
   protected static final Pattern WHITESPACE_PATTERN = Pattern.compile("\\s+|_+");
-  public SpeakerInfo(String speakerName, Mention originalMention) {
+  public SpeakerInfo(String speakerName) {
     this.speakerId = speakerName;
-    this.originalMention = originalMention;
     int commaPos = speakerName.indexOf(',');
     if (commaPos > 0) {
       // drop everything after the ,
@@ -92,9 +85,6 @@ public class SpeakerInfo {
 
 
   public int getCorefClusterId() {
-    if (originalMention != null && originalMention.corefClusterID >= 0) {
-      return originalMention.corefClusterID;
-    }
     int corefClusterId = -1;     // Coref cluster id that corresponds to this speaker
     for (Mention m:mentions) {
       if (m.corefClusterID >= 0) {
@@ -103,10 +93,6 @@ public class SpeakerInfo {
       }
     }
     return corefClusterId;
-  }
-
-  public String toString() {
-    return speakerId;
   }
 
 }
