88e83e30e548bef1634939d55c2b1b37ad361df3
Gabor Angeli <angeli@cs.stanford.edu>
2013-10-02 21:35:20 -0700

    Candidate protobuf definitions

--- src/edu/stanford/nlp/trees/Tree.java
+++ src/edu/stanford/nlp/trees/Tree.java
@@ -20,7 +20,14 @@ import edu.stanford.nlp.ling.Sentence;
 import edu.stanford.nlp.ling.TaggedWord;
 import edu.stanford.nlp.ling.Word;
 import edu.stanford.nlp.ling.CoreAnnotations;
-import edu.stanford.nlp.util.*;
+import edu.stanford.nlp.util.Filter;
+import edu.stanford.nlp.util.Filters;
+import edu.stanford.nlp.util.Generics;
+import edu.stanford.nlp.util.IntPair;
+import edu.stanford.nlp.util.MutableInteger;
+import edu.stanford.nlp.util.Pair;
+import edu.stanford.nlp.util.Scored;
+import edu.stanford.nlp.util.XMLUtils;
 
 /**
  * The abstract class <code>Tree</code> is used to collect all of the
@@ -422,8 +429,8 @@ public abstract class Tree extends AbstractCollection<Tree> implements Label, La
    * @return an IntPair: the SpanAnnotation of this node.
    */
   public IntPair getSpan() {
-    if(label() instanceof CoreMap && ((CoreMap) label()).has(CoreAnnotations.SpanAnnotation.class))
-      return ((CoreMap) label()).get(CoreAnnotations.SpanAnnotation.class);
+    if(label() instanceof CoreLabel && ((CoreLabel) label()).has(CoreAnnotations.SpanAnnotation.class))
+      return ((CoreLabel) label()).get(CoreAnnotations.SpanAnnotation.class);
     return null;
   }
 
@@ -2772,12 +2779,9 @@ public abstract class Tree extends AbstractCollection<Tree> implements Label, La
       }
     }
 
-    Label label = label();
-    if (label instanceof CoreMap) {
-    CoreMap afl = (CoreMap) label();
-      afl.set(CoreAnnotations.BeginIndexAnnotation.class, start);
-      afl.set(CoreAnnotations.EndIndexAnnotation.class, end);
-    }
+    CoreLabel afl = (CoreLabel) label();
+    afl.set(CoreAnnotations.BeginIndexAnnotation.class, start);
+    afl.set(CoreAnnotations.EndIndexAnnotation.class, end);
     return new Pair<Integer, Integer>(start, end);
   }
 
