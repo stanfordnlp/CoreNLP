045d9341919f09cefacfc03cb0386c6e18b1b0c6
John Bauer <horatio@gmail.com>
2013-10-21 15:50:54 -0700

    Update the ReadSentimentDataset to split the trees into train/dev/test

--- src/edu/stanford/nlp/sentiment/ReadSentimentDataset.java
+++ src/edu/stanford/nlp/sentiment/ReadSentimentDataset.java
@@ -1,11 +1,16 @@
 package edu.stanford.nlp.sentiment;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 import edu.stanford.nlp.io.IOUtils;
+import edu.stanford.nlp.io.RuntimeIOException;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.process.PTBEscapingProcessor;
 import edu.stanford.nlp.trees.Tree;
@@ -14,6 +19,11 @@ import edu.stanford.nlp.util.CollectionUtils;
 import edu.stanford.nlp.util.Function;
 import edu.stanford.nlp.util.Generics;
 
+/**
+ * Reads the sentiment dataset and writes it to the appropriate files.
+ *
+ * @author John Bauer
+ */
 public class ReadSentimentDataset {
   public static Tree convertTree(List<Integer> parentPointers, List<String> sentence, Map<List<String>, Integer> phraseIds, Map<Integer, Double> sentimentScores, PTBEscapingProcessor escaper) {
     int maxNode = 0;
@@ -98,15 +108,50 @@ public class ReadSentimentDataset {
     connect(parentPointers, subtrees, connected, parentPointers.get(index));
   }
 
-  // TODO: add the ability to split the training, dev and test data
+  private static void writeTrees(String filename, List<Tree> trees, List<Integer> treeIds) {
+    try {
+      FileOutputStream fos = new FileOutputStream(filename);
+      BufferedWriter bout = new BufferedWriter(new OutputStreamWriter(fos));
+      
+      for (Integer id : treeIds) {
+        bout.write(trees.get(id).toString());
+        bout.write("\n");
+      }
+      bout.flush();
+      fos.close();
+    } catch (IOException e) {
+      throw new RuntimeIOException(e);
+    }
+  }
+
+  /**
+   * This program converts the format of the Sentiment data set
+   * prepared by Richard, Jean, etc. into trees readable with the
+   * normal TreeReaders.
+   * <br>
+   * An example command line is
+   * <br>
+   * <code>java edu.stanford.nlp.sentiment.ReadSentimentDataset -dictionary stanfordSentimentTreebank/dictionary.txt -sentiment stanfordSentimentTreebank/sentiment_labels.txt -tokens stanfordSentimentTreebank/SOStr.txt -parse stanfordSentimentTreebank/STree.txt  -split stanfordSentimentTreebank/datasetSplit.txt  -train train.txt -dev dev.txt -test test.txt</code>
+   * <br>
+   * The arguments are as follows: <br>
+   * <code>-dictionary</code>, <code>-sentiment</code>,
+   * <code>-tokens</code>, <code>-parse</code>, <code>-split</code>
+   * Path to the corresponding files from the dataset <br>
+   * <code>-train</code>, <code>-dev</code>, <code>-test</code>
+   * Paths for saving the corresponding output files <br>
+   * Each of these arguments is required.
+   */
   public static void main(String[] args) {
     String dictionaryFilename = null;
     String sentimentFilename = null;
     String tokensFilename = null;
     String parseFilename = null;
+    String splitFilename = null;
 
+    String trainFilename = null;
+    String devFilename = null;
+    String testFilename = null;
 
-    // TODO: document arguments
     int argIndex = 0;
     while (argIndex < args.length) {
       if (args[argIndex].equalsIgnoreCase("-dictionary")) {
@@ -121,18 +166,34 @@ public class ReadSentimentDataset {
       } else if (args[argIndex].equalsIgnoreCase("-parse")) {
         parseFilename = args[argIndex + 1];
         argIndex += 2;
+      } else if (args[argIndex].equalsIgnoreCase("-split")) {
+        splitFilename = args[argIndex + 1];
+        argIndex += 2;
+      } else if (args[argIndex].equalsIgnoreCase("-train")) {
+        trainFilename = args[argIndex + 1];
+        argIndex += 2;
+      } else if (args[argIndex].equalsIgnoreCase("-dev")) {
+        devFilename = args[argIndex + 1];
+        argIndex += 2;
+      } else if (args[argIndex].equalsIgnoreCase("-test")) {
+        testFilename = args[argIndex + 1];
+        argIndex += 2;
       } else {
         System.err.println("Unknown argument " + args[argIndex]);
         System.exit(2);
       }
     }
 
+    // Sentence file is formatted
+    //   w1|w2|w3...
     List<List<String>> sentences = Generics.newArrayList();
     for (String line : IOUtils.readLines(tokensFilename, "utf-8")) {
       String[] sentence = line.split("\\|");
       sentences.add(Arrays.asList(sentence));
     }
 
+    // Split and read the phrase ids file.  This file is in the format
+    //   w1 w2 w3 ... | id
     Map<List<String>, Integer> phraseIds = Generics.newHashMap();
     for (String line : IOUtils.readLines(dictionaryFilename)) {
       String[] pieces = line.split("\\|");
@@ -141,7 +202,9 @@ public class ReadSentimentDataset {
       phraseIds.put(Arrays.asList(sentence), id);
     }
 
-
+    // Split and read the sentiment scores file.  Each line of this
+    // file is of the format:
+    //   phrasenum | score
     Map<Integer, Double> sentimentScores = Generics.newHashMap();
     for (String line : IOUtils.readLines(sentimentFilename)) {
       if (line.startsWith("phrase")) {
@@ -153,8 +216,10 @@ public class ReadSentimentDataset {
       sentimentScores.put(id, score);
     }
 
+    // Read lines from the tree structure file.  This is a file of parent pointers for each tree.
     int index = 0;
     PTBEscapingProcessor escaper = new PTBEscapingProcessor();
+    List<Tree> trees = Generics.newArrayList();
     for (String line : IOUtils.readLines(parseFilename)) {
       String[] pieces = line.split("\\|");
       List<Integer> parentPointers = CollectionUtils.transformAsList(Arrays.asList(pieces), new Function<String, Integer>() { 
@@ -162,8 +227,25 @@ public class ReadSentimentDataset {
         });
       Tree tree = convertTree(parentPointers, sentences.get(index), phraseIds, sentimentScores, escaper);
       ++index;
-
-      System.out.println(tree);
+      trees.add(tree);
     }
+
+    Map<Integer, List<Integer>> splits = Generics.newHashMap();
+    splits.put(1, Generics.<Integer>newArrayList());
+    splits.put(2, Generics.<Integer>newArrayList());
+    splits.put(3, Generics.<Integer>newArrayList());
+    for (String line : IOUtils.readLines(splitFilename)) {
+      if (line.startsWith("sentence_index")) {
+        continue;
+      }
+      String[] pieces = line.split(",");
+      Integer treeId = Integer.valueOf(pieces[0]) - 1;
+      Integer fileId = Integer.valueOf(pieces[1]);
+      splits.get(fileId).add(treeId);
+    }
+
+    writeTrees(trainFilename, trees, splits.get(1));
+    writeTrees(devFilename, trees, splits.get(2));
+    writeTrees(testFilename, trees, splits.get(3));
   }
 }
