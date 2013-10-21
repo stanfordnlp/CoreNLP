2e114557b5ae82b5ec57ed91de6eca734be3588a
John Bauer <horatio@gmail.com>
2013-10-21 15:50:06 -0700

    Fill in some blanks, such as by reusing code from the DVParser project

--- src/edu/stanford/nlp/sentiment/Options.java
+++ src/edu/stanford/nlp/sentiment/Options.java
@@ -2,6 +2,8 @@ package edu.stanford.nlp.sentiment;
 
 import java.io.Serializable;
 
+import edu.stanford.nlp.trees.TreebankLanguagePack;
+
 public class Options implements Serializable {
   public double scalingForInit;
 
@@ -12,4 +14,12 @@ public class Options implements Serializable {
   public int numHid;
 
   public boolean lowercaseWordVectors;
+
+  public TreebankLanguagePack langpack;
+
+  /**
+   * No symantic untying - use the same category for all categories.
+   * This results in all nodes getting the same matrix.
+   */
+  public boolean simplifiedModel;
 }
