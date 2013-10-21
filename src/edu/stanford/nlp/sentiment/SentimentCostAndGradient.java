3cb76b48d324921ecf3b87b4b20bd4b862a01686
John Bauer <horatio@gmail.com>
2013-10-21 15:50:09 -0700

    Skeleton for Sentiment training code

---
+++ src/edu/stanford/nlp/sentiment/SentimentCostAndGradient.java
@@ -0,0 +1,7 @@
+package edu.stanford.nlp.sentiment;
+
+import edu.stanford.nlp.optimization.AbstractCachingDiffFunction;
+
+public class SentimentCostAndGradient extends AbstractCachingDiffFunction {
+
+}
