e3b443515f78dafa5687524541d8ff561de3f130
John Bauer <horatio@gmail.com>
2013-10-21 16:28:02 -0700

    Add some various comments

--- src/edu/stanford/nlp/rnn/SimpleTensor.java
+++ src/edu/stanford/nlp/rnn/SimpleTensor.java
@@ -7,6 +7,16 @@ import java.util.NoSuchElementException;
 
 import org.ejml.simple.SimpleMatrix;
 
+/**
+ * This class defines a block tensor, somewhat like a three
+ * dimensional matrix.  This can be created in various ways, such as
+ * by providing an array of SimpleMatrix slices, by providing the
+ * initial size to create a 0-initialized tensor, or by creating a
+ * random matrix.
+ *
+ * @author John Bauer
+ * @author Richard Socher
+ */
 public class SimpleTensor implements Serializable {
   private SimpleMatrix[] slices;
 
@@ -14,6 +24,9 @@ public class SimpleTensor implements Serializable {
   final int numCols;
   final int numSlices;
 
+  /**
+   * Creates a zero initialized tensor
+   */
   public SimpleTensor(int numRows, int numCols, int numSlices) {
     slices = new SimpleMatrix[numSlices];
     for (int i = 0; i < numSlices; ++i) {
@@ -25,6 +38,11 @@ public class SimpleTensor implements Serializable {
     this.numSlices = numSlices;
   }
 
+  /**
+   * Copies the data in the slices.  Slices are copied rather than
+   * reusing the original SimpleMatrix objects.  Each slice must be
+   * the same size.
+   */
   public SimpleTensor(SimpleMatrix[] slices) {
     this.numRows = slices[0].numRows();
     this.numCols = slices[0].numCols();
@@ -39,6 +57,10 @@ public class SimpleTensor implements Serializable {
     
   }
 
+  /**
+   * Returns a randomly initialized tensor with values draft from the
+   * uniform distribution between minValue and maxValue.
+   */
   public static SimpleTensor random(int numRows, int numCols, int numSlices, double minValue, double maxValue, java.util.Random rand) {
     SimpleTensor tensor = new SimpleTensor(numRows, numCols, numSlices);
     for (int i = 0; i < numSlices; ++i) {
@@ -47,22 +69,39 @@ public class SimpleTensor implements Serializable {
     return tensor;
   }
 
+  /**
+   * Number of rows in the tensor
+   */
   public int numRows() {
     return numRows;
   }
 
+  /**
+   * Number of columns in the tensor
+   */
   public int numCols() {
     return numCols;
   }
 
+  /**
+   * Number of slices in the tensor
+   */
   public int numSlices() {
     return numSlices;
   }
 
+  /**
+   * Total number of elements in the tensor
+   */
   public int getNumElements() {
     return numRows * numCols * numSlices;
   }
 
+  /**
+   * Returns a new tensor which has the values of the original tensor
+   * scaled by <code>scaling</code>.  The original object is
+   * unaffected.
+   */
   public SimpleTensor scale(double scaling) {
     SimpleTensor result = new SimpleTensor(numRows, numCols, numSlices);
     for (int slice = 0; slice < numSlices; ++slice) {
@@ -71,6 +110,9 @@ public class SimpleTensor implements Serializable {
     return result;
   }
 
+  /**
+   * Returns <code>other</code> added to the current object, which is unaffected.
+   */
   public SimpleTensor plus(SimpleTensor other) {
     if (other.numRows != numRows || other.numCols != numCols || other.numSlices != numSlices) {
       throw new IllegalArgumentException("Sizes of tensors do not match.  Our size: " + numRows + "," + numCols + "," + numSlices + "; other size " + other.numRows + "," + other.numCols + "," + other.numSlices);
@@ -82,6 +124,10 @@ public class SimpleTensor implements Serializable {
     return result;
   }
 
+  /**
+   * Performs elementwise multiplication on the tensors.  The original
+   * objects are unaffected.
+   */
   public SimpleTensor elementMult(SimpleTensor other) {
     if (other.numRows != numRows || other.numCols != numCols || other.numSlices != numSlices) {
       throw new IllegalArgumentException("Sizes of tensors do not match.  Our size: " + numRows + "," + numCols + "," + numSlices + "; other size " + other.numRows + "," + other.numCols + "," + other.numSlices);
@@ -93,6 +139,9 @@ public class SimpleTensor implements Serializable {
     return result;
   }
 
+  /**
+   * Returns the sum of all elements in the tensor.
+   */
   public double elementSum() {
     double sum = 0.0;
     for (SimpleMatrix slice : slices) {
@@ -101,6 +150,10 @@ public class SimpleTensor implements Serializable {
     return sum;
   }
 
+  /**
+   * Use the given <code>matrix</code> in place of <code>slice</code>.
+   * Does not copy the <code>matrix</code>, but rather uses the actual object.
+   */
   public void setSlice(int slice, SimpleMatrix matrix) {
     if (slice < 0 || slice >= numSlices) {
       throw new IllegalArgumentException("Unexpected slice number " + slice + " for tensor with " + numSlices + " slices");
@@ -114,6 +167,11 @@ public class SimpleTensor implements Serializable {
     slices[slice] = matrix;
   }
 
+  /**
+   * Returns the SimpleMatrix at <code>slice</code>.
+   * <br>
+   * The actual slice is returned - do not alter this unless you know what you are doing.
+   */
   public SimpleMatrix getSlice(int slice) {
     if (slice < 0 || slice >= numSlices) {
       throw new IllegalArgumentException("Unexpected slice number " + slice + " for tensor with " + numSlices + " slices");
@@ -121,6 +179,10 @@ public class SimpleTensor implements Serializable {
     return slices[slice];
   }
 
+  /**
+   * Returns a column vector where each entry is the nth bilinear
+   * product of the nth slices of the two tensors.
+   */
   public SimpleMatrix bilinearProducts(SimpleMatrix in) {
     if (in.numCols() != 1) {
       throw new AssertionError("Expected a column vector");
@@ -140,6 +202,9 @@ public class SimpleTensor implements Serializable {
     return out;
   }
 
+  /**
+   * Returns an iterator over the <code>SimpleMatrix</code> objects contained in the tensor.
+   */
   public Iterator<SimpleMatrix> iteratorSimpleMatrix() {
     return Arrays.asList(slices).iterator();
   }
