aa0714dac503e8245e19db415d50b9f17ebdeff9
John Bauer <horatio@gmail.com>
2013-10-21 15:50:47 -0700

    Fix a bug in the tensor delta down

--- src/edu/stanford/nlp/sentiment/SentimentCostAndGradient.java
+++ src/edu/stanford/nlp/sentiment/SentimentCostAndGradient.java
@@ -232,7 +232,7 @@ public class SentimentCostAndGradient extends AbstractCachingDiffFunction {
     SimpleMatrix fullVector = RNNUtils.concatenate(leftVector, rightVector);
     for (int slice = 0; slice < size; ++slice) {
       SimpleMatrix scaledFullVector = fullVector.scale(deltaFull.get(slice));
-      deltaTensor = deltaTensor.plus(Wt.getSlice(slice).mult(Wt.getSlice(slice).transpose()).mult(scaledFullVector));
+      deltaTensor = deltaTensor.plus(Wt.getSlice(slice).plus(Wt.getSlice(slice).transpose()).mult(scaledFullVector));
     }
     return deltaTensor.plus(WTDeltaNoBias);
   }
