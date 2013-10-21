04ffcf2729dd78e40eb2f26f4adf1e4822d53779
John Bauer <horatio@gmail.com>
2013-10-21 15:50:35 -0700

    Fix a bug in the word vector initialization

--- src/edu/stanford/nlp/sentiment/SentimentModel.java
+++ src/edu/stanford/nlp/sentiment/SentimentModel.java
@@ -202,7 +202,7 @@ public class SentimentModel implements Serializable {
     this.wordVectors = Generics.newTreeMap();
     for (String word : words) {
       // TODO: how do we initialize this?
-      SimpleMatrix vector = SimpleMatrix.random(numHid, 1, -1.0/Math.sqrt((double)numHid),1.0/Math.sqrt((double)numHid),rand);
+      SimpleMatrix vector = SimpleMatrix.random(op.numHid, 1, -1.0/Math.sqrt((double)op.numHid),1.0/Math.sqrt((double)op.numHid),rand);
       wordVectors.put(word, vector);
     }
   }
