943875f80371d2762f48b538b4cf72c7f1c7ac3e
John Bauer <horatio@gmail.com>
2013-10-21 15:50:27 -0700

    Add (untested) option handling code to the RNN Sentiment Model

--- src/edu/stanford/nlp/sentiment/RNNTrainOptions.java
+++ src/edu/stanford/nlp/sentiment/RNNTrainOptions.java
@@ -14,4 +14,28 @@ public class RNNTrainOptions implements Serializable {
   public double learningRate;
 
   public double scalingForInit;
+
+  public int setOption(String[] args, int argIndex) {
+    if (args[argIndex].equalsIgnoreCase("-batchSize")) {
+      batchSize = Integer.valueOf(args[argIndex + 1]);
+      return argIndex + 2;
+    } else if (args[argIndex].equalsIgnoreCase("-iterations")) {
+      iterations = Integer.valueOf(args[argIndex + 1]);
+      return argIndex + 2;
+    } else if (args[argIndex].equalsIgnoreCase("-debugOutputSeconds")) {
+      debugOutputSeconds = Integer.valueOf(args[argIndex + 1]);
+      return argIndex + 2;
+    } else if (args[argIndex].equalsIgnoreCase("-maxTrainTimeSeconds")) {
+      maxTrainTimeSeconds = Integer.valueOf(args[argIndex + 1]);
+      return argIndex + 2;
+    } else if (args[argIndex].equalsIgnoreCase("-learningRate")) {
+      learningRate = Double.valueOf(args[argIndex + 1]);
+      return argIndex + 2;
+    } else if (args[argIndex].equalsIgnoreCase("-scalingForInit")) {
+      scalingForInit = Double.valueOf(args[argIndex + 1]);
+      return argIndex + 2;
+    } else {
+      return argIndex;
+    }
+  }
 }
