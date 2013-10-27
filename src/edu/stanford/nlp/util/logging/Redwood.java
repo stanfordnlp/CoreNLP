e1f597225a56830dc7515862acae9a3915932771
Gabor Angeli <angeli@cs.stanford.edu>
2013-10-26 19:00:41 -0700

    Multithread ensemble relation extractor

--- src/edu/stanford/nlp/util/logging/Redwood.java
+++ src/edu/stanford/nlp/util/logging/Redwood.java
@@ -1037,7 +1037,7 @@ public class Redwood {
      */
     public static void threadAndRun(String title, Iterable<Runnable> runnables, int numThreads){
       // (short circuit if single thread)
-      if (numThreads == 1) {
+      if (numThreads == 1 || isThreaded) {
         startTrack( "Threads (" + title + ")" );
         for (Runnable toRun : runnables) { toRun.run(); }
         endTrack( "Threads (" + title + ")" );
