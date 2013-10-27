6194e63dc82d0b0296d1d4c1f9af8b3f0b12a33f
Gabor Angeli <angeli@cs.stanford.edu>
2013-10-26 19:00:13 -0700

    Commit bare minimum training script

--- src/edu/stanford/nlp/optimization/SGDMinimizer.java
+++ src/edu/stanford/nlp/optimization/SGDMinimizer.java
@@ -118,6 +118,7 @@ public class SGDMinimizer<T extends Function> extends StochasticMinimizer<T> {
     final double[] grads = new double[dim];
 
     final DiffFunction f = new DiffFunction() {
+      @Override
       public double[] derivativeAt(double[] x) {
         double val = Math.PI * valuePow(x, Math.PI - 1);
         for (int i = 0; i < dim; i++) {
@@ -139,6 +140,7 @@ public class SGDMinimizer<T extends Function> extends StochasticMinimizer<T> {
         return Math.pow(val * 0.5, pow);
       }
 
+      @Override
       public int domainDimension() {
         return dim;
       }
