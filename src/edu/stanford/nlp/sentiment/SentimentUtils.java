c4be07adbf3db03d0da4177d45f853251f1daa03
John Bauer <horatio@gmail.com>
2013-10-28 02:25:07 -0700

    Merge branch 'master' of /u/nlp/git/javanlp

--- src/edu/stanford/nlp/sentiment/SentimentUtils.java
+++ src/edu/stanford/nlp/sentiment/SentimentUtils.java
@@ -46,5 +46,22 @@ public class SentimentUtils {
     return trees;
   }
 
+  public static String sentimentString(int sentiment) {
+    switch(sentiment) {
+    case 0:
+      return "Very negative";
+    case 1:
+      return "Negative";
+    case 2:
+      return "Neutral";
+    case 3:
+      return "Positive";
+    case 4:
+      return "Very positive";
+    default:
+      return "Unknown sentiment label " + sentiment;
+    }
+  }
+
 
 }
