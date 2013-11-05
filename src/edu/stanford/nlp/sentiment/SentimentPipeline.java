9019786f8bb95820686f60a1a0a393d7ab20d195
John Bauer <horatio@gmail.com>
2013-11-05 01:55:10 -0800

    Add some documentation. Add a feature to allow printing the sentiment labels at each node.

--- src/edu/stanford/nlp/sentiment/SentimentPipeline.java
+++ src/edu/stanford/nlp/sentiment/SentimentPipeline.java
@@ -6,6 +6,8 @@ import java.util.Properties;
 
 import edu.stanford.nlp.io.IOUtils;
 import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.Label;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.rnn.RNNCoreAnnotations;
@@ -15,16 +17,41 @@ import edu.stanford.nlp.util.CoreMap;
 /**
  * A wrapper class which creates a suitable pipeline for the sentiment
  * model and processes raw text.
+ *<br>
+ * The main program has the following options: <br>
+ * <code>-parserModel</code> Which parser model to use, defaults to englishPCFG.ser.gz <br>
+ * <code>-sentimentModel</code> Which sentiment model to use, defaults to sentiment.ser.gz <br>
+ * <code>-file</code> Which file to process. <br>
+ * <code>-stdin</code> Read one line at a time from stdin. <br>
+ * <code>-pennTrees</code> Output trees with scores at each binarized node.  Defaults to printing just the root. <br>
  *
  * @author John Bauer
  */
 public class SentimentPipeline {
+  static void setSentimentLabels(Tree tree) {
+    if (tree.isLeaf()) {
+      return;
+    }
+
+    for (Tree child : tree.children()) {
+      setSentimentLabels(child);
+    }
+
+    Label label = tree.label();
+    if (!(label instanceof CoreLabel)) {
+      throw new IllegalArgumentException("Required a tree with CoreLabels");
+    }
+    CoreLabel cl = (CoreLabel) label;
+    cl.setValue(Integer.toString(RNNCoreAnnotations.getPredictedClass(tree)));
+  }
+
   public static void main(String[] args) throws IOException {
     String parserModel = null;
     String sentimentModel = null;
 
     String filename = null;
     boolean stdin = false;
+    boolean pennTrees = false;
 
     for (int argIndex = 0; argIndex < args.length; ) {
       if (args[argIndex].equalsIgnoreCase("-sentimentModel")) {
@@ -38,6 +65,9 @@ public class SentimentPipeline {
         argIndex += 2;
       } else if (args[argIndex].equalsIgnoreCase("-stdin")) {
         stdin = true;
+        argIndex++;
+      } else if (args[argIndex].equalsIgnoreCase("-pennTrees")) {
+        pennTrees = true;
         argIndex++;
       } else {
         System.err.println("Unknown argument " + args[argIndex + 1]);
@@ -73,9 +103,15 @@ public class SentimentPipeline {
 
       for (CoreMap sentence : annotation.get(CoreAnnotations.SentencesAnnotation.class)) {
         Tree tree = sentence.get(SentimentCoreAnnotations.AnnotatedTree.class);
-        int sentiment = RNNCoreAnnotations.getPredictedClass(tree);
         System.err.println(sentence);
-        System.err.println("  Predicted sentiment: " + SentimentUtils.sentimentString(sentiment));
+        if (pennTrees) {
+          Tree copy = tree.deepCopy();
+          setSentimentLabels(copy);
+          System.err.println(copy);
+        } else {
+          int sentiment = RNNCoreAnnotations.getPredictedClass(tree);
+          System.err.println("  Predicted sentiment: " + SentimentUtils.sentimentString(sentiment));
+        }
       }
     } else {
       System.err.println("Reading in text from stdin.");
@@ -92,8 +128,14 @@ public class SentimentPipeline {
           Annotation annotation = pipeline.process(line);
           for (CoreMap sentence : annotation.get(CoreAnnotations.SentencesAnnotation.class)) {
             Tree tree = sentence.get(SentimentCoreAnnotations.AnnotatedTree.class);
-            int sentiment = RNNCoreAnnotations.getPredictedClass(tree);
-            System.err.println("  " + SentimentUtils.sentimentString(sentiment));
+            if (pennTrees) {
+              Tree copy = tree.deepCopy();
+              setSentimentLabels(copy);
+              System.err.println(copy);
+            } else {
+              int sentiment = RNNCoreAnnotations.getPredictedClass(tree);
+              System.err.println("  " + SentimentUtils.sentimentString(sentiment));
+            }
           }
         } else {
           // Output blank lines for blank lines so the tool can be
