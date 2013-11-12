a16021757d0f208dbfef711abbf032a9100c50da
Thang Luong <lmthang@stanford.edu>
2013-11-12 02:09:16 -0800

    Merge branch 'master' of origin

--- src/edu/stanford/nlp/parser/dvparser/DVModel.java
+++ src/edu/stanford/nlp/parser/dvparser/DVModel.java
@@ -521,6 +521,8 @@ public class DVModel implements Serializable {
         word = op.wordFunction.apply(word);
       }
 
+      wordVectors.put(word, vector);
+
       if (op.lexOptions.numHid <= 0) {
         op.lexOptions.numHid = vector.getNumElements();
       }
