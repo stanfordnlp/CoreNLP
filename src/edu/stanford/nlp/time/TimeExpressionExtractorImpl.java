c25c8d839ace028192a8724e7486cf8af109cbd9
Angel Chang <angelx@cs.stanford.edu>
2013-11-13 12:33:46 -0800

    Remove deprecated SUTime classes.

--- src/edu/stanford/nlp/time/TimeExpressionExtractorImpl.java
+++ src/edu/stanford/nlp/time/TimeExpressionExtractorImpl.java
@@ -62,7 +62,7 @@ public class TimeExpressionExtractorImpl implements TimeExpressionExtractor {
     if (options.grammarFilename != null) {
       timexPatterns = new GenericTimeExpressionPatterns(options);
     } else {
-      timexPatterns = new EnglishTimeExpressionPatterns(options);
+      throw new IllegalArgumentException("Time rules file is not specified");
     }
     this.expressionExtractor = timexPatterns.createExtractor();
     this.expressionExtractor.setLogger(logger);
