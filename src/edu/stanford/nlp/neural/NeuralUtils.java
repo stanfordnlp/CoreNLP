fc8aff80fca5035a86960824adde1d6b00cf13a3
John Bauer <horatio@gmail.com>
2013-11-19 03:47:49 -0800

    Merge branch 'master' of /u/nlp/git/javanlp

--- src/edu/stanford/nlp/neural/NeuralUtils.java
+++ src/edu/stanford/nlp/neural/NeuralUtils.java
@@ -26,14 +26,20 @@ public class NeuralUtils {
   }
   
   /**
-   * Compute dot product between two column vectors.
+   * Compute dot product between two vectors.
    */
   public static double dot(SimpleMatrix vector1, SimpleMatrix vector2){
+    double score = Double.NaN;
     if(vector1.numRows()==1){ // vector1: row vector, assume that vector2 is a row vector too 
-      return vector1.mult(vector2.transpose()).get(0); 
-    } else { // vector1: col vector, assume that vector2 is also a column vector.
-      return vector1.transpose().mult(vector2).get(0);
+      score = vector1.mult(vector2.transpose()).get(0); 
+    } else if (vector1.numCols()==1){ // vector1: col vector, assume that vector2 is also a column vector.
+      score = vector1.transpose().mult(vector2).get(0);
+    } else {
+      System.err.println("! Error in neural.Utils.dot: vector1 is a matrix " + vector1.numRows() + " x " + vector1.numCols());
+      System.exit(1);
     }
+
+    return score;
   }
   
   /**
