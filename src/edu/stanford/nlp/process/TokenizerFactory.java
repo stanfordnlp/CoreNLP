7b2e2efdd461390faf1c949961ffe09e7fe8d5a6
Mengqiu Wang <mengqiu@stanford.edu>
2013-12-09 14:56:09 -0800

    Merge branch 'yarowsky_ngai'

--- src/edu/stanford/nlp/process/TokenizerFactory.java
+++ src/edu/stanford/nlp/process/TokenizerFactory.java
@@ -5,18 +5,19 @@ import edu.stanford.nlp.objectbank.IteratorFromReaderFactory;
 import java.io.Reader;
 
 /**
- * A TokenizerFactory is used to convert a java.io.Reader
- * into a Tokenizer (or an Iterator) over the Objects represented by the text
+ * A TokenizerFactory is used to convert a java.io.Reader into a Tokenizer
+ * (an extension of Iterator) over objects of type T represented by the text
  * in the java.io.Reader.  It's mainly a convenience, since you could cast
  * down anyway.
  *
  * <i>IMPORTANT NOTE:</i><br/>
  *
- * A TokenizerFactory should also provide a static method: <br>
- * {@code public static TokenizerFactory<? extends HasWord> newTokenizerFactory();}
+ * A TokenizerFactory should also provide two static methods: <br>
+ * {@code public static TokenizerFactory<? extends HasWord> newTokenizerFactory(); }
+ * {@code public static TokenizerFactory<Word> newWordTokenizerFactory(String options); }
  * <br/>
- * This is expected by certain JavaNLP code which wants to produce a
- * TokenizerFactory by reflection.
+ * These are expected by certain JavaNLP code (e.g., LexicalizedParser),
+ * which wants to produce a TokenizerFactory by reflection.
  *
  * @author Christopher Manning
  *
@@ -29,4 +30,5 @@ public interface TokenizerFactory<T> extends IteratorFromReaderFactory<T> {
   public Tokenizer<T> getTokenizer(Reader r, String extraOptions);
 
   public void setOptions(String options);
+
 }
