7d888b867c0fc57500a4d664cb4b147f6a0a7b16
John Bauer <horatio@gmail.com>
2014-01-13 01:00:39 -0800

    Remove some dobj dependencies that shouldn't happen

--- doc/corenlp/corenlp.sh
+++ doc/corenlp/corenlp.sh
@@ -5,14 +5,8 @@
 #    ./corenlp.sh -file filename
 #    ./corenlp.sh -file filename -outputFormat text 
 
-OS=`uname`
-# Apparently macs don't support readlink -e
-if [ "$OS" == "Darwin" ]; then
-  scriptdir=`dirname $0`
-else
-  scriptpath=$(readlink -e "$0") || scriptpath=$0
-  scriptdir=$(dirname "$scriptpath")
-fi
+scriptpath=$(readlink -e "$0")
+scriptdir=$(dirname "$scriptpath")
 
 echo java -mx3g -cp \"$scriptdir/*\" edu.stanford.nlp.pipeline.StanfordCoreNLP $*
 java -mx3g -cp "$scriptdir/*" edu.stanford.nlp.pipeline.StanfordCoreNLP $*
