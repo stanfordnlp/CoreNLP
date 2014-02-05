1b40bd8e4ed38b50398c8df41785de6c286eb665
Gabor Angeli <angeli@cs.stanford.edu>
2014-02-04 17:22:41 -0800

    Simple exemplar test beginnings

--- src/edu/stanford/nlp/parser/tools/ChangeBaseParserWeight.java
+++ src/edu/stanford/nlp/parser/tools/ChangeBaseParserWeight.java
@@ -6,17 +6,19 @@ import edu.stanford.nlp.parser.lexparser.LexicalizedParser;
 import edu.stanford.nlp.util.Generics;
 
 /**
- * A simple tool to change flags embedded
+ * A simple tool to change the default baseParserWeight flag embedded
  * in a LexicalizedParser model.
  * <br>
  * Expected arguments: <br>
  * <code> -input model </code> <br>
  * <code> -output model </code> <br>
- * <code> [list of arguments to set] </code> <br>
+ * <code> -baseParserWeight weight </code> <br>
+ *
+ * TODO: could be generalized to allow updating of many different options
  *
  * @author John Bauer
  */
-public class UpdateParserOptions {
+public class ChangeBaseParserWeight {
   public static void main(String[] args) {
     String input = null;
     String output = null;
