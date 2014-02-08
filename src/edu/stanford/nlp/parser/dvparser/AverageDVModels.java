4a9d7457934350576e9e37a7f253066f4a5d3cce
John Bauer <horatio@gmail.com>
2014-02-07 17:50:47 -0800

    Fix a vexing problem with AverageDVModels - the maps HashMaps instead of TreeMaps

--- src/edu/stanford/nlp/parser/dvparser/AverageDVModels.java
+++ src/edu/stanford/nlp/parser/dvparser/AverageDVModels.java
@@ -46,7 +46,7 @@ public class AverageDVModels {
   }
 
   public static TwoDimensionalMap<String, String, SimpleMatrix> averageBinaryMatrices(List<TwoDimensionalMap<String, String, SimpleMatrix>> maps) {
-    TwoDimensionalMap<String, String, SimpleMatrix> averages = new TwoDimensionalMap<String, String, SimpleMatrix>();
+    TwoDimensionalMap<String, String, SimpleMatrix> averages = TwoDimensionalMap.treeMap();
     for (Pair<String, String> binary : getBinaryMatrixNames(maps)) {
       int count = 0;
       SimpleMatrix matrix = null;
@@ -69,7 +69,7 @@ public class AverageDVModels {
   }
 
   public static Map<String, SimpleMatrix> averageUnaryMatrices(List<Map<String, SimpleMatrix>> maps) {
-    Map<String, SimpleMatrix> averages = Generics.newHashMap();
+    Map<String, SimpleMatrix> averages = Generics.newTreeMap();
     for (String name : getUnaryMatrixNames(maps)) {
       int count = 0;
       SimpleMatrix matrix = null;
