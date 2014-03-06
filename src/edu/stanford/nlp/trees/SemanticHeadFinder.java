dde8ea8c0b3092ff1e81567faeb335e19ad8e877
Spence Green <spence@spencegreen.com>
2014-03-06 15:40:15 -0800

    Fix post-processing bug

--- src/edu/stanford/nlp/trees/SemanticHeadFinder.java
+++ src/edu/stanford/nlp/trees/SemanticHeadFinder.java
@@ -235,7 +235,7 @@ public class SemanticHeadFinder extends ModCollinsHeadFinder {
     return headIdx;
   }
 
-  // Note: The first two SBARQ patterns only work when the SQ
+  // Note: so far, both of these patterns only work when the SQ
   // structure has already been removed in CoordinationTransformer.
   static final TregexPattern[] headOfCopulaTregex = {
     // Matches phrases such as "what is wrong"
@@ -245,10 +245,6 @@ public class SemanticHeadFinder extends ModCollinsHeadFinder {
     // for example, "Who am I to judge?"
     // !$++ ADJP matches against "Why is the dog pink?"
     TregexPattern.compile("SBARQ < (WHNP=head $++ (/^VB/ < " + EnglishGrammaticalRelations.copularWordRegex + " $+ NP !$++ ADJP))"),
-
-    // Actually somewhat limited in scope, this detects "Tuesday it is", 
-    // "Such a great idea this was", etc
-    TregexPattern.compile("SINV < (NP=head $++ (NP $++ (VP < (/^(?:VB|AUX)/ < " + EnglishGrammaticalRelations.copularWordRegex + "))))"),
   };
 
   static final TregexPattern[] headOfConjpTregex = {
@@ -311,7 +307,7 @@ public class SemanticHeadFinder extends ModCollinsHeadFinder {
       // if none of the above patterns match, use the standard method
     }
 
-    if (motherCat.equals("SBARQ") || motherCat.equals("SINV")) { 
+    if (motherCat.equals("SBARQ")) { 
       if (!makeCopulaHead) {
         for (TregexPattern pattern : headOfCopulaTregex) {
           TregexMatcher matcher = pattern.matcher(t);
