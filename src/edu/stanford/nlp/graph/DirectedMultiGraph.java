68cdbf73818c9d76ccad723dd4e86651a7479954
Gabor Angeli <angeli@cs.stanford.edu>
2014-03-25 20:42:20 -0700

    Fix multigraph; begin tests for inferential path mining

--- src/edu/stanford/nlp/graph/DirectedMultiGraph.java
+++ src/edu/stanford/nlp/graph/DirectedMultiGraph.java
@@ -138,7 +138,7 @@ public class DirectedMultiGraph<V, E> implements Graph<V, E> /* Serializable */{
     }
     boolean foundOut = outgoingEdges.containsKey(source) && outgoingEdges.get(source).containsKey(dest) &&
         outgoingEdges.get(source).get(dest).remove(data);
-    boolean foundIn = incomingEdges.containsKey(source) && incomingEdges.get(source).containsKey(dest) &&
+    boolean foundIn = incomingEdges.containsKey(dest) && incomingEdges.get(dest).containsKey(source) &&
         incomingEdges.get(dest).get(source).remove(data);
     if (foundOut && !foundIn) {
       throw new AssertionError("Edge found in outgoing but not incoming");
