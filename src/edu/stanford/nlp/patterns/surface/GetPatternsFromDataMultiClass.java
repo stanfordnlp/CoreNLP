ea069794705ab6fb4105bd766a9c5cdb97ff3732
sonalg <sonalg@stanford.edu>
2014-05-14 09:12:11 -0700

    resolved merge conflict

--- src/edu/stanford/nlp/patterns/surface/GetPatternsFromDataMultiClass.java
+++ src/edu/stanford/nlp/patterns/surface/GetPatternsFromDataMultiClass.java
@@ -1896,21 +1896,17 @@ public class GetPatternsFromDataMultiClass implements Serializable {
       if (gold.equals(guess) && !gold.equalsIgnoreCase(background)) {
         entityTP.incrementCount(gold);
         wordTP.incrementCount(line.word());
-      } else if (!gold.equals(guess) && !gold.equalsIgnoreCase(background)
-          && guess.equalsIgnoreCase(background)) {
+      } else if (!gold.equals(guess) && !gold.equalsIgnoreCase(background) && guess.equalsIgnoreCase(background)) {
         entityFN.incrementCount(gold);
         wordFN.incrementCount(line.word());
 
-      } else if (!gold.equals(guess) && !guess.equalsIgnoreCase(background)
-          && gold.equalsIgnoreCase(background)) {
+      } else if (!gold.equals(guess) && !guess.equalsIgnoreCase(background) && gold.equalsIgnoreCase(background)) {
         wordFP.incrementCount(line.word());
         entityFP.incrementCount(guess);
       } else if (gold.equals(guess) && !gold.equalsIgnoreCase(background)) {
         wordTN.incrementCount(line.word());
-      } else if (!(gold.equalsIgnoreCase(background) && guess
-          .equalsIgnoreCase(background)))
-        throw new RuntimeException(
-            "don't know reached here. not meant for more than one entity label");
+      } else if (!(gold.equalsIgnoreCase(background) && guess.equalsIgnoreCase(background)))
+        throw new RuntimeException("don't know reached here. not meant for more than one entity label");
 
     }
 
@@ -2089,44 +2085,40 @@ public class GetPatternsFromDataMultiClass implements Serializable {
     for (Entry<String, List<CoreLabel>> docEn : testSentences.entrySet()) {
       List<CoreLabel> doc = docEn.getValue();
       for (CoreLabel l : doc) {
-        for (Entry<String, Class<? extends Key<String>>> anscl : constVars.answerClass
-            .entrySet()) {
-          l.set(CoreAnnotations.AnswerAnnotation.class,
-              constVars.backgroundSymbol);
+        for (Entry<String, Class<? extends Key<String>>> anscl : constVars.answerClass.entrySet()) {
+          l.set(CoreAnnotations.AnswerAnnotation.class, constVars.backgroundSymbol);
           if (l.get(anscl.getValue()).equals(anscl.getKey())) {
             l.set(CoreAnnotations.AnswerAnnotation.class, anscl.getKey());
           }
         }
       }
       countResults(doc, entityTP, entityFP, entityFN,
-          constVars.backgroundSymbol, wordTP, wordTN, wordFP, wordFN,
-          CoreAnnotations.AnswerAnnotation.class, evalPerEntity); //
+                   constVars.backgroundSymbol, wordTP, wordTN, wordFP, wordFN,
+                   CoreAnnotations.AnswerAnnotation.class, evalPerEntity); //
     }
     // System.out.println("False Positives: "
     // + Counters.toSortedString(wordFP, wordFP.size(), "%s:%.2f", ";"));
     // System.out.println("False Negatives: "
     // + Counters.toSortedString(wordFN, wordFN.size(), "%s:%.2f", ";"));
 
+
     Redwood.log(Redwood.DBG, "\n\n True Positives: " + entityTP);
     Redwood.log(Redwood.DBG, "\n\n False Positives: " + entityFP);
     Redwood.log(Redwood.DBG, "\n\n False Negatives: " + entityFN);
-    Counter<String> precision = Counters.division(entityTP,
-        Counters.add(entityTP, entityFP));
-    Counter<String> recall = Counters.division(entityTP,
-        Counters.add(entityTP, entityFN));
+    Counter<String> precision = Counters.division(entityTP, Counters.add(entityTP, entityFP));
+    Counter<String> recall = Counters.division(entityTP, Counters.add(entityTP, entityFN));
     Redwood.log(Redwood.FORCE, "\n Precision: " + precision);
     Redwood.log(Redwood.FORCE, "\n Recall: " + recall);
     Redwood.log(Redwood.FORCE, "\n F1 score:  " + FScore(precision, recall, 1));
     Redwood.log(Redwood.FORCE, "Total: " + Counters.add(entityFP, entityTP));
 
+
   }
 
-  public static <D> Counter<D> FScore(Counter<D> precision, Counter<D> recall,
-      double beta) {
+  public static <D> Counter<D> FScore(Counter<D> precision, Counter<D> recall, double beta) {
     double betasq = beta * beta;
-    return Counters.divisionNonNaN(
-        Counters.scale(Counters.product(precision, recall), (1 + betasq)),
-        (Counters.add(Counters.scale(precision, betasq), recall)));
+    return Counters.divisionNonNaN(Counters.scale(Counters.product(precision, recall), (1 + betasq)),
+                                   (Counters.add(Counters.scale(precision, betasq), recall)));
   }
 
   public static List<File> getAllFiles(String file) {
@@ -2135,9 +2127,9 @@ public class GetPatternsFromDataMultiClass implements Serializable {
       File filef = new File(tokfile);
       String ext = ".*";
       File dir = null;
-      if (filef.isDirectory())
+      if (filef.isDirectory()) {
         dir = filef;
-      else {
+      } else {
         dir = filef.getParentFile();
         ext = filef.getName();
       }
@@ -2155,7 +2147,6 @@ public class GetPatternsFromDataMultiClass implements Serializable {
   @SuppressWarnings({ "rawtypes" })
   public static void main(String[] args) {
     try {
-
       Properties props = StringUtils.argsToPropertiesWithResolve(args);
 
       Map<String, Set<SurfacePattern>> ignorePatterns = new HashMap<String, Set<SurfacePattern>>();
@@ -2168,8 +2159,7 @@ public class GetPatternsFromDataMultiClass implements Serializable {
 
       String seedWordsFiles = props.getProperty("seedWordsFiles");
       if (seedWordsFiles == null) {
-        throw new RuntimeException(
-            "Needs both seedWordsFiles and file parameters to run this class!\nseedWordsFiles has format: label1,filewithlistofwords1;label2,filewithlistofwords2;...");
+        throw new RuntimeException("Needs both seedWordsFiles and file parameters to run this class!\nseedWordsFiles has format: label1,filewithlistofwords1;label2,filewithlistofwords2;...");
       }
       for (String seedFile : seedWordsFiles.split(";")) {
         String[] t = seedFile.split(",");
@@ -2178,13 +2168,13 @@ public class GetPatternsFromDataMultiClass implements Serializable {
         Set<String> seedWords4Label = new HashSet<String>();
         for (String line : IOUtils.readLines(seedWordsFile)) {
           line = line.trim();
-          if (line.isEmpty() || line.startsWith("#"))
+          if (line.isEmpty() || line.startsWith("#")) {
             continue;
+          }
           seedWords4Label.add(line);
         }
         seedWords.put(label, seedWords4Label);
-        Redwood.log(Redwood.FORCE, "Number of seed words for label " + label
-            + " is " + seedWords4Label.size());
+        Redwood.log(Redwood.FORCE, "Number of seed words for label " + label + " is " + seedWords4Label.size());
       }
 
       Map<String, Class> answerClasses = new HashMap<String, Class>();
@@ -2204,99 +2194,80 @@ public class GetPatternsFromDataMultiClass implements Serializable {
       String file = props.getProperty("file");
 
       String posModelPath = props.getProperty("posModelPath");
-      boolean lowercase = Boolean.parseBoolean(props
-          .getProperty("lowercaseText"));
-      boolean useTargetNERRestriction = Boolean.parseBoolean(props
-          .getProperty("useTargetNERRestriction"));
-      boolean useTargetParserParentRestriction = Boolean.parseBoolean(props
-          .getProperty("useTargetParserParentRestriction"));
-      boolean useContextNERRestriction = Boolean.parseBoolean(props
-          .getProperty("useContextNERRestriction"));
+      boolean lowercase = Boolean.parseBoolean(props.getProperty("lowercaseText"));
+      boolean useTargetNERRestriction = Boolean.parseBoolean(props.getProperty("useTargetNERRestriction"));
+      boolean useTargetParserParentRestriction = Boolean.parseBoolean(props.getProperty("useTargetParserParentRestriction"));
+      boolean useContextNERRestriction = Boolean.parseBoolean(props.getProperty("useContextNERRestriction"));
 
       boolean evaluate = Boolean.parseBoolean(props.getProperty("evaluate"));
-      boolean addEvalSentsToTrain = Boolean.parseBoolean(props
-          .getProperty("addEvalSentsToTrain"));
-      String evalFileWithGoldLabels = props
-          .getProperty("evalFileWithGoldLabels");
+      boolean addEvalSentsToTrain = Boolean.parseBoolean(props.getProperty("addEvalSentsToTrain"));
+      String evalFileWithGoldLabels = props.getProperty("evalFileWithGoldLabels");
 
-      if (file == null
-          && (evalFileWithGoldLabels == null || addEvalSentsToTrain == false)) {
-        throw new RuntimeException("No training data! file is " + file
-            + " and evalFileWithGoldLabels is " + evalFileWithGoldLabels
-            + " and addEvalSentsToTrain is " + addEvalSentsToTrain);
+      if (file == null && (evalFileWithGoldLabels == null || addEvalSentsToTrain == false)) {
+        throw new RuntimeException("No training data! file is " + file +
+                                   " and evalFileWithGoldLabels is " + evalFileWithGoldLabels +
+                                   " and addEvalSentsToTrain is " + addEvalSentsToTrain);
       }
 
       // Read training file
       if (file != null) {
         List<File> allFiles = GetPatternsFromDataMultiClass.getAllFiles(file);
-        if (fileFormat == null || fileFormat.equalsIgnoreCase("text")
-            || fileFormat.equalsIgnoreCase("txt")) {
+        if (fileFormat == null || fileFormat.equalsIgnoreCase("text") || fileFormat.equalsIgnoreCase("txt")) {
           for (File f : allFiles) {
             Redwood.log(Redwood.DBG, "Annotating text in " + f);
-
+            
             String text = IOUtils.stringFromFile(f.getAbsolutePath());
             sents.putAll(tokenize(text, posModelPath, lowercase,
-                useTargetNERRestriction || useContextNERRestriction, f.getName()
-                    + "-", useTargetParserParentRestriction,
-                    props.getProperty("numThreads")));
+                                  useTargetNERRestriction || useContextNERRestriction, 
+                                  f.getName() + "-", useTargetParserParentRestriction,
+                                  props.getProperty("numThreads")));
           }
 
         } else if (fileFormat.equalsIgnoreCase("ser")) {
-          for (File f : allFiles)
-            sents.putAll((Map<String, List<CoreLabel>>) IOUtils
-                .readObjectFromFile(f));
-        } else
-          throw new RuntimeException(
-              "Cannot identify the file format. Valid values are text (or txt) and ser, where the serialized file is of the type Map<String, List<CoreLabel>>.");
+          for (File f : allFiles) {
+            sents.putAll((Map<String, List<CoreLabel>>) IOUtils.readObjectFromFile(f));
+          }
+        } else {
+          throw new RuntimeException("Cannot identify the file format. Valid values are text (or txt) and ser, where the serialized file is of the type Map<String, List<CoreLabel>>.");
+        }
       }
 
       // Read Evaluation File
       Map<String, List<CoreLabel>> evalsents = new HashMap<String, List<CoreLabel>>();
       if (evaluate) {
         Map setClassForTheseLabels = new HashMap<String, Class>();
-        boolean splitOnPunct = Boolean.parseBoolean(props.getProperty(
-            "splitOnPunct", "true"));
-        List<File> allFiles = GetPatternsFromDataMultiClass
-            .getAllFiles(evalFileWithGoldLabels);
+        boolean splitOnPunct = Boolean.parseBoolean(props.getProperty("splitOnPunct", "true"));
+        List<File> allFiles = GetPatternsFromDataMultiClass.getAllFiles(evalFileWithGoldLabels);
         int numFile = 0;
-        if (fileFormat == null || fileFormat.equalsIgnoreCase("text")
-            || fileFormat.equalsIgnoreCase("txt")) {
+        if (fileFormat == null || fileFormat.equalsIgnoreCase("text") || fileFormat.equalsIgnoreCase("txt")) {
           for (File f : allFiles) {
             numFile++;
-            Redwood.log(Redwood.DBG, "Annotating text in " + f + ". Num file "
-                + numFile);
-            List<CoreMap> sentsCMs = AnnotatedTextReader.parseFile(
-                new BufferedReader(new FileReader(f)), seedWords.keySet(),
-                setClassForTheseLabels, true, splitOnPunct, lowercase,
-                f.getName());
+            Redwood.log(Redwood.DBG, "Annotating text in " + f + ". Num file " + numFile);
+            List<CoreMap> sentsCMs = AnnotatedTextReader.parseFile(new BufferedReader(new FileReader(f)), seedWords.keySet(),
+                                                                   setClassForTheseLabels, true, splitOnPunct, lowercase,
+                                                                   f.getName());
             evalsents.putAll(runPOSNEROnTokens(sentsCMs, posModelPath,
-                useTargetNERRestriction || useContextNERRestriction, "",
-                useTargetParserParentRestriction,
-                props.getProperty("numThreads")));
+                                               useTargetNERRestriction || useContextNERRestriction, "",
+                                               useTargetParserParentRestriction,
+                                               props.getProperty("numThreads")));
           }
 
         } else if (fileFormat.equalsIgnoreCase("ser")) {
           for (File f : allFiles) {
-            evalsents
-                .putAll((Map<? extends String, ? extends List<CoreLabel>>) IOUtils
-                    .readObjectFromFile(f));
+            evalsents.putAll((Map<? extends String, ? extends List<CoreLabel>>) IOUtils.readObjectFromFile(f));
           }
         }
         if (addEvalSentsToTrain) {
-          Redwood.log(Redwood.DBG, "Adding " + evalsents.size()
-              + " eval sents to the training set");
+          Redwood.log(Redwood.DBG, "Adding " + evalsents.size() + " eval sents to the training set");
           sents.putAll(evalsents);
         }
       }
 
       boolean learn = Boolean.parseBoolean(props.getProperty("learn", "true"));
 
-      boolean labelUsingSeedSets = Boolean.parseBoolean(props.getProperty(
-          "labelUsingSeedSets", "true"));
-      GetPatternsFromDataMultiClass g = new GetPatternsFromDataMultiClass(
-          props, sents, seedWords, labelUsingSeedSets);
-      String saveEvalSentencesSerFile = props
-          .getProperty("saveEvalSentencesSerFile");
+      boolean labelUsingSeedSets = Boolean.parseBoolean(props.getProperty("labelUsingSeedSets", "true"));
+      GetPatternsFromDataMultiClass g = new GetPatternsFromDataMultiClass(props, sents, seedWords, labelUsingSeedSets);
+      String saveEvalSentencesSerFile = props.getProperty("saveEvalSentencesSerFile");
       if (saveEvalSentencesSerFile != null) {
         IOUtils.writeObjectToFile(evalsents, saveEvalSentencesSerFile);
       }
@@ -2308,8 +2279,7 @@ public class GetPatternsFromDataMultiClass implements Serializable {
       Execution.fillOptions(g, props);
       if (learn) {
 
-        Redwood.log(Redwood.FORCE, "Total number of training sentences "
-            + Data.sents.size());
+        Redwood.log(Redwood.FORCE, "Total number of training sentences " + Data.sents.size());
 
         String sentsOutFile = props.getProperty("sentsOutFile");
 
@@ -2317,13 +2287,15 @@ public class GetPatternsFromDataMultiClass implements Serializable {
 
         String patternOutFile = props.getProperty("patternOutFile");
 
-        g.iterateExtractApply(p0, p0Set, wordsOutputFile, sentsOutFile,
-            patternOutFile, ignorePatterns);
+        g.iterateExtractApply(p0, p0Set, wordsOutputFile, sentsOutFile, patternOutFile, ignorePatterns);
 
         if (evaluate) {
-          boolean evalPerEntity = Boolean.parseBoolean(props.getProperty(
-              "evalPerEntity", "true"));
-          g.evaluate(evalsents, evalPerEntity);
+          if (evalsents.size() == 0) 
+            System.err.println("No eval sentences provided to evaluate! Make sure evalFileWithGoldLabels is set or turn off the evaluate flag");
+          else{
+            boolean evalPerEntity = Boolean.parseBoolean(props.getProperty("evalPerEntity", "true"));
+            g.evaluate(evalsents, evalPerEntity);
+          }
         }
       }
     } catch (Exception e) {
