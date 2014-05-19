0186d79cb81f9d5b6213c903b2a191cf4d46be59
John Bauer <horatio@gmail.com>
2014-05-19 15:30:42 -0700

    Merge branch 'master' of origin into srparser

--- src/edu/stanford/nlp/parser/lexparser/ParserQueryFactory.java
+++ src/edu/stanford/nlp/parser/lexparser/ParserQueryFactory.java
@@ -1,5 +1,9 @@
 package edu.stanford.nlp.parser.lexparser;
 
+import java.util.List;
+
+import edu.stanford.nlp.parser.metrics.Eval;
+
 /**
  * An interface which indicates the class involved can return
  * ParserQuery objects.  Useful for something that wants to use
@@ -7,9 +11,18 @@ package edu.stanford.nlp.parser.lexparser;
  * ParserQuery source.  For example, 
  * {@link edu.stanford.nlp.parser.lexparser.EvaluateTreebank} 
  * does this.
+ * <br>
+ * TODO: perhaps this should actually just be an AbstractParser or something like that
  *
  * @author John Bauer
  */
 public interface ParserQueryFactory {
   ParserQuery parserQuery();
+
+  /**
+   * Returns a list of extra Eval objects to use when scoring the parser.
+   * TODO: perhaps this should go elsewhere, or perhaps we should make
+   * this an "AbstractParser" interface of some kind
+   */
+  List<Eval> getExtraEvals();
 }
