a87a2c834c7ce3299e68e3b54942ae9a3c2a3502
John Bauer <horatio@gmail.com>
2014-06-06 22:30:03 -0700

    Rename FrenchXMLTreeReader

--- src/edu/stanford/nlp/international/french/scripts/SplitCanditoTrees.java
+++ src/edu/stanford/nlp/international/french/scripts/SplitCanditoTrees.java
@@ -28,7 +28,7 @@ import edu.stanford.nlp.stats.TwoDimensionalCounter;
 import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.trees.TreeReaderFactory;
 import edu.stanford.nlp.trees.TreeTransformer;
-import edu.stanford.nlp.trees.international.french.FrenchTreeReader;
+import edu.stanford.nlp.trees.international.french.FrenchXMLTreeReader;
 import edu.stanford.nlp.trees.international.french.FrenchXMLTreeReaderFactory;
 import edu.stanford.nlp.trees.tregex.TregexMatcher;
 import edu.stanford.nlp.trees.tregex.TregexPattern;
@@ -103,7 +103,7 @@ public final class SplitCanditoTrees {
       String canonicalFilename =
         file.getName().substring(0, file.getName().lastIndexOf('.'));
 
-      FrenchTreeReader tr = (FrenchTreeReader)
+      FrenchXMLTreeReader tr = (FrenchXMLTreeReader)
         trf.newTreeReader(new BufferedReader
                           (new InputStreamReader
                            (new FileInputStream(file),"ISO8859_1")));
