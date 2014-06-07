5736b2eb8d4e732ff96241c1de0b6fdc7a0dd1d7
Will Monroe <wmonroe4@cs.stanford.edu>
2014-06-07 00:44:53 -0700

    Add wrapper feature to Arabic segmenter

--- src/edu/stanford/nlp/international/arabic/process/ArabicSegmenterFeatureFactory.java
+++ src/edu/stanford/nlp/international/arabic/process/ArabicSegmenterFeatureFactory.java
@@ -151,6 +151,20 @@ public class ArabicSegmenterFeatureFactory<IN extends CoreLabel> extends Feature
 
     features.add(charc + charp + charp2 + "-cngram");
 
+    // "Wrapper" feature: identity of first and last two chars of the current word.
+    // This helps detect ma+_+sh in dialect, as well as avoiding segmenting possessive
+    // pronouns if the word starts with al-.
+    if (c.word().length() > 3) {
+      String start = c.word().substring(0, 2);
+      String end = c.word().substring(c.word().length() - 2);
+      if (c.index() == 2) {
+        features.add(start + "_" + end + "-begin-wrap");
+      }
+      if (c.index() == c.word().length() - 1) {
+        features.add(start + "_" + end + "-end-wrap");
+      }
+    }
+    
     // Indicator transition feature
     features.add("cliqueCp2C");
     
