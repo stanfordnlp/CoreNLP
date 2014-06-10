802d5f4519c4a2a30d0ca4be3a41b7b09d1fe8e7
Angel Chang <angelx@cs.stanford.edu>
2014-06-10 16:16:25 -0700

    Update TokenRegex to allow more types of simple strings (those not enclosed in "")

--- itest/src/edu/stanford/nlp/ling/tokensregex/TokenSequenceMatcherITest.java
+++ itest/src/edu/stanford/nlp/ling/tokensregex/TokenSequenceMatcherITest.java
@@ -845,6 +845,47 @@ public class TokenSequenceMatcherITest extends TestCase {
     assertEquals(6, m.end("$contextnext"));
   }
 
+  public void testTokenSequenceMatcher10() throws IOException {
+    CoreMap doc = createDocument("the number is five or 5 or 5.0 or but not 5x or -5 or 5L.");
+
+    // Test simplified pattern with number
+    TokenSequencePattern p = TokenSequencePattern.compile( "(five|5|5x|5.0|-5|5L)");
+
+    TokenSequenceMatcher m = p.getMatcher(doc.get(CoreAnnotations.TokensAnnotation.class));
+    boolean match = m.find();
+    assertTrue(match);
+    assertEquals(1, m.groupCount());
+    assertEquals("five", m.group(1));
+
+    match = m.find();
+    assertTrue(match);
+    assertEquals(1, m.groupCount());
+    assertEquals("5", m.group(1));
+
+    match = m.find();
+    assertTrue(match);
+    assertEquals(1, m.groupCount());
+    assertEquals("5.0", m.group(1));
+
+    match = m.find();
+    assertTrue(match);
+    assertEquals(1, m.groupCount());
+    assertEquals("5x", m.group(1));
+
+    match = m.find();
+    assertTrue(match);
+    assertEquals(1, m.groupCount());
+    assertEquals("-5", m.group(1));
+
+    match = m.find();
+    assertTrue(match);
+    assertEquals(1, m.groupCount());
+    assertEquals("5L", m.group(1));
+
+    match = m.find();
+    assertFalse(match);
+  }
+
   public void testTokenSequenceMatcherPosNNP() throws IOException {
     CoreMap doc = createDocument(testText1);
 
