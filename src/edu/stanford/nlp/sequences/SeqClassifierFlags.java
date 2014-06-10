e03a4ed279d3f15e2ac6cdf408799838f64dda17
Gabor Angeli <angeli@cs.stanford.edu>
2014-06-10 16:52:37 -0700

    Merge branch 'master' of jamie:/u/nlp/git/javanlp

--- src/edu/stanford/nlp/sequences/SeqClassifierFlags.java
+++ src/edu/stanford/nlp/sequences/SeqClassifierFlags.java
@@ -998,6 +998,9 @@ public class SeqClassifierFlags implements Serializable {
   public transient String serializeWeightsTo = null;
   public boolean geDebug = false;
   public boolean doFeatureDiscovery = false;
+  public transient String loadWeightsFrom = null;
+  public transient String loadClassIndexFrom = null;
+  public transient String serializeClassIndexTo = null;
 
   // "ADD VARIABLES ABOVE HERE"
 
@@ -2472,6 +2475,12 @@ public class SeqClassifierFlags implements Serializable {
         geDebug = Boolean.parseBoolean(val);
       } else if (key.equalsIgnoreCase("doFeatureDiscovery")){
         doFeatureDiscovery = Boolean.parseBoolean(val);
+      } else if (key.equalsIgnoreCase("loadWeightsFrom")) {
+        loadWeightsFrom = val;
+      } else if (key.equalsIgnoreCase("loadClassIndexFrom")) {
+        loadClassIndexFrom = val;
+      } else if (key.equalsIgnoreCase("serializeClassIndexTo")) {
+        serializeClassIndexTo = val;
         // ADD VALUE ABOVE HERE
       } else if (key.length() > 0 && !key.equals("prop")) {
         System.err.println("Unknown property: |" + key + '|');
