6b9b746d5dd93c55788e64f5ae665cc28c1d3f4c
sonalg <sonalg@stanford.edu>
2014-06-10 16:19:04 -0700

    trying to make it parallel

--- src/edu/stanford/nlp/util/PropertiesUtils.java
+++ src/edu/stanford/nlp/util/PropertiesUtils.java
@@ -1,9 +1,6 @@
 package edu.stanford.nlp.util;
 
-import java.io.IOException;
 import java.io.PrintStream;
-import java.io.StringReader;
-import java.io.StringWriter;
 import java.lang.reflect.Type;
 import java.util.Enumeration;
 import java.util.HashMap;
@@ -35,27 +32,6 @@ public class PropertiesUtils {
     return ! (value.equals("false") || value.equals("no") || value.equals("off"));
   }
 
-  // Convert from properties to string and from string to properties
-  public static String asString(Properties props) {
-    try {
-      StringWriter sw = new StringWriter();
-      props.store(sw, null);
-      return sw.toString();
-    } catch (IOException ex) {
-      throw new RuntimeException(ex);
-    }
-  }
-
-  public static Properties fromString(String str) {
-    try {
-      StringReader sr = new StringReader(str);
-      Properties props = new Properties();
-      props.load(sr);
-      return props;
-    } catch (IOException ex) {
-      throw new RuntimeException(ex);
-    }
-  }
 
   // printing -------------------------------------------------------------------
 
