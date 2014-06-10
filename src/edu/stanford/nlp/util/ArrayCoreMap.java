44f58059f6a0a59a8c7945a4188a89793c62e0a6
Gabor Angeli <angeli@cs.stanford.edu>
2014-06-10 16:19:39 -0700

    Added comments and clearer naming to JointBayes

--- src/edu/stanford/nlp/util/ArrayCoreMap.java
+++ src/edu/stanford/nlp/util/ArrayCoreMap.java
@@ -587,8 +587,8 @@ public class ArrayCoreMap implements CoreMap, Serializable {
     int keysCode = 0;
     int valuesCode = 0;
     for (int i = 0; i < size; i++) {
-      keysCode += keys[i].hashCode();
-      valuesCode += (values[i] != null ? values[i].hashCode() : 0);
+      keysCode += (i < keys.length ? keys[i].hashCode() : 0);
+      valuesCode += (i < values.length && values[i] != null ? values[i].hashCode() : 0);
     }
 
     if (createdCalledSet) {
