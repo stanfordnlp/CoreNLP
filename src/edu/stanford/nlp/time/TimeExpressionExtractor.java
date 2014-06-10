9c274b123e2ee5806d37b1e53913ae84b005bd89
Arun Tejasvi Chaganty <chaganty@stanford.edu>
2014-06-10 16:31:39 -0700

    Merge branch 'kbp' of /u/nlp/git/javanlp into kbp

--- src/edu/stanford/nlp/time/TimeExpressionExtractor.java
+++ src/edu/stanford/nlp/time/TimeExpressionExtractor.java
@@ -15,8 +15,31 @@ public interface TimeExpressionExtractor {
 
   void init(Options options);
 
+  /**
+   * Returns list of CoreMaps indicating what the time expressions are
+   * @param annotation - Annotation holding tokenized text from which the time expressions are to be extracted
+   *
+   * @param docAnnotation - Annotation for the entire document
+   *                        Uses the following annotations:
+   *                          CoreAnnotations.DocDateAnnotation.class (String representing document date)
+   *                          TimeExpression.TimeIndexAnnotation.class (Holds index used to generated tids)
+   * @return List of CoreMaps
+   */
+  List<CoreMap> extractTimeExpressionCoreMaps(CoreMap annotation, CoreMap docAnnotation);
+
+  /**
+   * Returns list of CoreMaps indicating what the time expressions are
+   * @param annotation - Annotation holding tokenized text from which the time expressions are to be extracted
+   * @param docDate - String representing document date
+   * @return List of CoreMaps
+   */
   List<CoreMap> extractTimeExpressionCoreMaps(CoreMap annotation, String docDate);
 
-//  List<TimeExpression> extractTimeExpressions(CoreMap annotation, String docDateStr);
+  /**
+   * Indicates that all annotations on the document has been completed
+   * Performs cleanup on the document annotation
+   * @param docAnnotation
+   */
+  void finalize(CoreMap docAnnotation);
 
 }
