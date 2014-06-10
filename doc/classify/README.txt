d65ada38cde2041ade57175b97018a88c7a745c6
sonalg <sonalg@stanford.edu>
2014-06-10 16:32:43 -0700

    some changes

--- doc/classify/README.txt
+++ doc/classify/README.txt
@@ -1,4 +1,4 @@
-Stanford Classifier v3.2.0 - 2013-06-19
+Stanford Classifier v2.1.8 - 2013-04-04
 -------------------------------------------------
 
 Copyright (c) 2003-2012 The Board of Trustees of 
@@ -75,8 +75,6 @@ LICENSE
 -------------------------
 CHANGES
 -------------------------
-
-2013-06-19    3.2.0     Update for compatibility 
 
 2013-04-04    2.1.8     Update to maintain compatibility 
 
--- src/edu/stanford/nlp/util/CollectionUtils.java
+++ src/edu/stanford/nlp/util/CollectionUtils.java
@@ -138,6 +138,24 @@ public class CollectionUtils {
     }
     return diff;
   }
+  
+  /**
+   * Returns all objects in list1 that are not in list2
+   *
+   * @param <T> Type of items in the collection
+   * @param list1 First collection
+   * @param list2 Second collection
+   * @return The collection difference list1 - list2
+   */
+  public static <T> Set<T> diffAsSet(Collection<T> list1, Collection<T> list2) {
+    Set<T> diff = new HashSet<T>();
+    for (T t : list1) {
+      if (!list2.contains(t)) {
+        diff.add(t);
+      }
+    }
+    return diff;
+  }
 
   // Utils for loading and saving Collections to/from text files
 
@@ -887,4 +905,21 @@ public class CollectionUtils {
     }
     return transformed;
   }
+  
+  /**
+   * get all values corresponding to the indices (if they exist in the map)
+   * @param map
+   * @param indices
+   * @return
+   */
+  public static<T,V> List<V> getAll(Map<T, V> map, Collection<T> indices){
+    List<V> result = new ArrayList<V>();
+    for(T i: indices)
+      if(map.containsKey(i)){
+        result.add(map.get(i));
+      }
+    return result;
+  }
+  
+  
 }
