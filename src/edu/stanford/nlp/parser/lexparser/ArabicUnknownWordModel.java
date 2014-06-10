cbd462cdd23a6276f33995a51e14c128ec459f14
sonalg <sonalg@stanford.edu>
2014-06-10 16:10:56 -0700

    added a function in collectionvaluedmap to add many values at ojnce

--- src/edu/stanford/nlp/parser/lexparser/ArabicUnknownWordModel.java
+++ src/edu/stanford/nlp/parser/lexparser/ArabicUnknownWordModel.java
@@ -1,6 +1,11 @@
 package edu.stanford.nlp.parser.lexparser;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import edu.stanford.nlp.stats.ClassicCounter;
+import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.util.Index;
 
 /**
@@ -10,7 +15,8 @@ import edu.stanford.nlp.util.Index;
  * <i>Implementation note: the contents of this class tend to overlap somewhat
  * with {@link EnglishUnknownWordModel} and were originally included in {@link BaseLexicon}.
  *
- * @author Roger Levy
+ * @author Dan Klein
+ * @author Galen Andrew
  * @author Christopher Manning
  * @author Anna Rafferty
  */
@@ -28,9 +34,9 @@ public class ArabicUnknownWordModel extends BaseUnknownWordModel {
   protected int unknownSuffixSize = 0;
   protected int unknownPrefixSize = 0;
 
-  public ArabicUnknownWordModel(Options op, Lexicon lex,
-                                Index<String> wordIndex,
-                                Index<String> tagIndex,
+  public ArabicUnknownWordModel(Options op, Lexicon lex, 
+                                Index<String> wordIndex, 
+                                Index<String> tagIndex, 
                                 ClassicCounter<IntTaggedWord> unSeenCounter) {
     super(op, lex, wordIndex, tagIndex, unSeenCounter, null, null, null);
     unknownLevel = op.lexOptions.useUnknownWordSignatures;
@@ -53,7 +59,7 @@ public class ArabicUnknownWordModel extends BaseUnknownWordModel {
    * lines containing the data.
    */
   public ArabicUnknownWordModel(Options op, Lexicon lex,
-                                Index<String> wordIndex,
+                                Index<String> wordIndex, 
                                 Index<String> tagIndex) {
     this(op, lex, wordIndex, tagIndex, new ClassicCounter<IntTaggedWord>());
   }
