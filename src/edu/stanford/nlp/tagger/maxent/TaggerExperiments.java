d7f0ccfef970b3bf1d9899fcaf785dba217c578d
Spence Green <spence@spencegreen.com>
2014-06-10 16:28:22 -0700

    Separate IncrementalFeaturizer and IsolatedPhraseFeaturizer

--- src/edu/stanford/nlp/tagger/maxent/TaggerExperiments.java
+++ src/edu/stanford/nlp/tagger/maxent/TaggerExperiments.java
@@ -75,7 +75,9 @@ public class TaggerExperiments extends Experiments {
     feats = new TaggerFeatures(maxentTagger.tags, this);
   }
 
-  /** This method gets feature statistics from a training file found in the TaggerConfig. */
+  /** This method gets feature statistics from a training file found in the TaggerConfig.
+   *  It is the start of the training process.
+   */
   protected TaggerExperiments(TaggerConfig config, MaxentTagger maxentTagger) throws IOException {
     this(maxentTagger);
 
