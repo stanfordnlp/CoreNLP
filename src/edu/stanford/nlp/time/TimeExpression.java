a0700f1177e969de8067b56c14aea52a14f9b49a
Spence Green <spence@spencegreen.com>
2014-06-10 16:51:52 -0700

    Initial commit

--- src/edu/stanford/nlp/time/TimeExpression.java
+++ src/edu/stanford/nlp/time/TimeExpression.java
@@ -43,7 +43,7 @@ public class TimeExpression extends MatchedExpression {
   }
 
   //int tid;     // Time ID
-  SUTime.Temporal origTemporal;  // never read. Can delete?
+  SUTime.Temporal origTemporal;  // todo [2013]: never read. Can delete? (Set in TimeExpressionExtractorImpl)
   //int anchorTimeId = -1;
 
   public TimeExpression(MatchedExpression expr)
@@ -134,6 +134,7 @@ public class TimeExpression extends MatchedExpression {
     }
     return null;
   }
+
   public void setTemporal(SUTime.Temporal temporal) {
     this.value = new Expressions.PrimitiveValue<SUTime.Temporal>("Temporal", temporal);
   }
