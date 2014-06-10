07676c1022ccdf7075a9524ed81ebcb03ac19cc8
Mengqiu Wang <mengqiu@stanford.edu>
2014-06-10 16:09:55 -0700

    Merge branch 'higher_order_crf'

--- src/edu/stanford/nlp/wordseg/Sighan2005DocumentReaderAndWriter.java
+++ src/edu/stanford/nlp/wordseg/Sighan2005DocumentReaderAndWriter.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.PrintWriter;
 import java.io.Reader;
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Map;
@@ -46,7 +47,10 @@ import edu.stanford.nlp.fsm.DFSATransition;
  * @author Pi-Chuan Chang
  * @author Michel Galley (Viterbi seearch graph printing)
  */
-public class Sighan2005DocumentReaderAndWriter implements DocumentReaderAndWriter<CoreLabel>, LatticeWriter<CoreLabel, String, Integer> {
+public class Sighan2005DocumentReaderAndWriter implements DocumentReaderAndWriter<CoreLabel>, LatticeWriter<CoreLabel, String, Integer>, Serializable {
+
+  private static final long serialVersionUID = 3260295150250263237L;
+
 
   private static final boolean DEBUG = false;
   private static final boolean DEBUG_MORE = false;
@@ -105,7 +109,9 @@ public class Sighan2005DocumentReaderAndWriter implements DocumentReaderAndWrite
   }
 
 
-  public class CTBDocumentParser implements Function<String,List<CoreLabel>> {
+  class CTBDocumentParser implements Function<String,List<CoreLabel>>, Serializable {
+    private static final long serialVersionUID = 3260297180259462337L;
+
     private String defaultMap = "char=0,answer=1";
     public String[] map = StringUtils.mapStringToArray(defaultMap);
 
@@ -431,7 +437,5 @@ public class Sighan2005DocumentReaderAndWriter implements DocumentReaderAndWrite
         tagLatticeToAnswerLattice(tDest, newASource, newAnswer, nodeId, pos+1, newCost, stateLinks, answerLattice, docArray);
     }
   }
-
-  private static final long serialVersionUID = 3260295150250263237L;
 
 }
