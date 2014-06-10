9d01ba90194f82e5aba1d0fb4bc7d9636e3a6107
sonalg <sonalg@stanford.edu>
2014-06-10 16:12:52 -0700

    get phrases

--- src/edu/stanford/nlp/trees/GrammaticalRelation.java
+++ src/edu/stanford/nlp/trees/GrammaticalRelation.java
@@ -8,7 +8,6 @@ import edu.stanford.nlp.util.Generics;
 import edu.stanford.nlp.util.StringUtils;
 
 import java.io.Serializable;
-import java.lang.ref.SoftReference;
 import java.util.*;
 import java.util.regex.Pattern;
 
@@ -208,16 +207,8 @@ public class GrammaticalRelation implements Comparable<GrammaticalRelation>, Ser
     return reln;
   }
 
-  private static Map<String, SoftReference<GrammaticalRelation>> valueOfCache = new HashMap<String, SoftReference<GrammaticalRelation>>();
   public static GrammaticalRelation valueOf(String s) {
-    GrammaticalRelation value = null;
-    SoftReference<GrammaticalRelation> possiblyCachedValue = valueOfCache.get(s);
-    if (possiblyCachedValue != null) { value = possiblyCachedValue.get(); }
-    if (value == null) {
-      value = valueOf(Language.English, s);
-      valueOfCache.put(s, new SoftReference<GrammaticalRelation>(value));
-    }
-    return value;
+    return valueOf(Language.English, s);
   }
 
   /**
