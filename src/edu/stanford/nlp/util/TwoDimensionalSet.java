d07dc86fffd85a5f937cc1c38cbf9b3199b8ee22
John Bauer <horatio@gmail.com>
2014-06-10 16:51:06 -0700

    Add javadoc

--- src/edu/stanford/nlp/util/TwoDimensionalSet.java
+++ src/edu/stanford/nlp/util/TwoDimensionalSet.java
@@ -40,6 +40,9 @@ public class TwoDimensionalSet<K1, K2> implements Serializable, Iterable<Pair<K1
     return result;
   }
 
+  /**
+   * Adds all the keys in the given TwoDimensionalMap.  Returns true iff at least one key is added.
+   */
   public boolean addAllKeys(TwoDimensionalMap<? extends K1, ? extends K2, ?> map) {
     boolean result = false;
     for (TwoDimensionalMap.Entry<? extends K1, ? extends K2, ?> entry : map) {
