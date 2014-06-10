f76d551690a8442cf84ca8b0a70b6eea16b9e43d
Arun Tejasvi Chaganty <chaganty@stanford.edu>
2014-06-10 16:42:00 -0700

    Cleaned up KBPSlotfill to not segfault on non-KBP relations

--- test/src/edu/stanford/nlp/io/IOUtilsTest.java
+++ test/src/edu/stanford/nlp/io/IOUtilsTest.java
@@ -84,7 +84,39 @@ public class IOUtilsTest extends TestCase {
     Assert.assertEquals("!zipped!text", StringUtils.join(iterable, "!"));
     Assert.assertEquals("!zipped!text", StringUtils.join(iterable, "!"));
   }
-  
+
+  private void checkLineIterable(boolean includeEol) throws IOException {
+    String[] expected = {
+            "abcdefhij\r\n",
+            "klnm\r\n",
+            "opqrst\n",
+            "uvwxyz\r",
+            "I am a longer line than the rest\n",
+            "12345"
+    };
+    String testString = StringUtils.join(expected, "");
+
+    Reader reader = new StringReader(testString);
+    int i = 0;
+    Iterable<String> iterable = IOUtils.getLineIterable(reader, 10, includeEol);
+    for (String line:iterable) {
+      String expLine = expected[i];
+      if (!includeEol) expLine = expLine.replaceAll("\\r|\\n", "");
+      assertEquals("Checking line " + i, expLine, line);
+      i++;
+    }
+    assertEquals("Check got all lines", expected.length, i);
+    IOUtils.closeIgnoringExceptions(reader);
+  }
+
+  public void testLineIterableWithEol() throws IOException {
+    checkLineIterable(true);
+  }
+
+  public void testLineIterableWithoutEol() throws IOException {
+    checkLineIterable(false);
+  }
+
   public void testIterFilesRecursive() throws IOException {
     File dir = new File(this.dir, "recursive");
     File a = new File(dir, "x/a");
@@ -133,4 +165,163 @@ public class IOUtilsTest extends TestCase {
     }
     return set;
   }
+
+  /**
+   * Tests that slurpFile can get files from within the classpath
+   */
+  public void testSlurpFile() {
+    String contents;
+    try {
+      contents = IOUtils.slurpFile("edu/stanford/nlp/io/test.txt", "utf-8");
+    } catch (IOException e) {
+      throw new RuntimeIOException(e);
+    }
+
+    assertEquals("This is a test sentence.", contents.trim());
+
+    try {
+      contents = IOUtils.slurpFile("edu/stanford/nlp/io/test.txt");
+    } catch (IOException e) {
+      throw new RuntimeIOException(e);
+    }
+
+    assertEquals("This is a test sentence.", contents.trim());
+
+    try {
+      contents = IOUtils.slurpFile("edu/stanford/nlp/io/test.txtzzz");
+      throw new AssertionError("Should not have found unknown file");
+    } catch (IOException e) {
+      // yay
+    }
+
+    contents = IOUtils.slurpFileNoExceptions("edu/stanford/nlp/io/test.txt");
+    assertEquals("This is a test sentence.", contents.trim());
+
+
+    try {
+      contents = IOUtils.slurpFileNoExceptions("edu/stanford/nlp/io/test.txtzzz");
+      throw new AssertionError("Should not have found unknown file");
+    } catch (RuntimeIOException e) {
+      // yay
+    }
+  }
+
+  public void testCpSourceFileTargetNotExists() throws IOException {
+    File source = File.createTempFile("foo", ".file");
+    IOUtils.writeStringToFile("foo", source.getPath(), "utf-8");
+    File dst = File.createTempFile("foo", ".file");
+    assertTrue(dst.delete());
+    IOUtils.cp(source, dst);
+    assertEquals("foo", IOUtils.slurpFile(dst));
+    assertTrue( source.delete() );
+    assertTrue( dst.delete() );
+  }
+
+  public void testCpSourceFileTargetExists() throws IOException {
+    File source = File.createTempFile("foo", ".file");
+    IOUtils.writeStringToFile("foo", source.getPath(), "utf-8");
+    File dst = File.createTempFile("foo", ".file");
+    IOUtils.cp(source, dst);
+    assertEquals("foo", IOUtils.slurpFile(dst));
+    assertTrue( source.delete() );
+    assertTrue( dst.delete() );
+  }
+
+  public void testCpSourceFileTargetIsDir() throws IOException {
+    File source = File.createTempFile("foo", ".file");
+    IOUtils.writeStringToFile("foo", source.getPath(), "utf-8");
+    File dst = File.createTempFile("foo", ".file");
+    assertTrue(dst.delete());
+    assertTrue(dst.mkdir());
+    IOUtils.cp(source, dst);
+    assertEquals("foo", IOUtils.slurpFile(dst.getPath() + File.separator + source.getName()));
+    assertTrue( source.delete() );
+    assertTrue( new File(dst.getPath() + File.separator + source.getName()).delete() );
+    assertTrue( dst.delete() );
+  }
+
+  public void testCpSourceDirTargetNotExists() throws IOException {
+    // create source
+    File sourceDir = File.createTempFile("foo", ".file");
+    assertTrue( sourceDir.delete() );
+    assertTrue( sourceDir.mkdir() );
+    File foo = new File(sourceDir + File.separator + "foo");
+    IOUtils.writeStringToFile("foo", foo.getPath(), "utf-8");
+
+    // create destination
+    File dst = File.createTempFile("foo", ".file");
+    assertTrue(dst.delete());
+
+    // copy
+    IOUtils.cp(sourceDir, dst, true);
+    assertEquals("foo", IOUtils.slurpFile(dst.getPath() + File.separator + "foo"));
+
+    // clean up
+    assertTrue( foo.delete() );
+    assertTrue( sourceDir.delete() );
+    assertTrue( new File(dst.getPath() + File.separator + "foo").delete() );
+    assertTrue( dst.delete() );
+  }
+
+  public void testCpSourceDirTargetIsDir() throws IOException {
+    // create source
+    File sourceDir = File.createTempFile("foo", ".file");
+    assertTrue( sourceDir.delete() );
+    assertTrue( sourceDir.mkdir() );
+    File foo = new File(sourceDir + File.separator + "foo");
+    IOUtils.writeStringToFile("foo", foo.getPath(), "utf-8");
+
+    // create destination
+    File dst = File.createTempFile("foo", ".file");
+    assertTrue( dst.delete() );
+    assertTrue( dst.mkdir() );
+
+    // copy
+    IOUtils.cp(sourceDir, dst, true);
+    assertEquals("foo", IOUtils.slurpFile(dst.getPath() + File.separator + sourceDir.getName() + File.separator + "foo"));
+
+    // clean up
+    assertTrue( foo.delete() );
+    assertTrue( sourceDir.delete() );
+    assertTrue( new File(dst.getPath() + File.separator + sourceDir.getName() + File.separator + "foo").delete() );
+    assertTrue( new File(dst.getPath() + File.separator + sourceDir.getName()).delete() );
+    assertTrue( dst.delete() );
+  }
+
+  public void testCpRecursive() throws IOException {
+    // create source
+    // d1/
+    //   d2/
+    //     foo
+    //   bar
+    File sourceDir = File.createTempFile("directory", ".file");
+    assertTrue( sourceDir.delete() );
+    assertTrue( sourceDir.mkdir() );
+    File sourceSubDir = new File(sourceDir + File.separator + "d2");
+    assertTrue( sourceSubDir.mkdir() );
+    File foo = new File(sourceSubDir + File.separator + "foo");
+    IOUtils.writeStringToFile("foo", foo.getPath(), "utf-8");
+    File bar = new File(sourceDir + File.separator + "bar");
+    IOUtils.writeStringToFile("bar", bar.getPath(), "utf-8");
+
+    // create destination
+    File dst = File.createTempFile("dst", ".file");
+    assertTrue( dst.delete() );
+
+    // copy
+    IOUtils.cp(sourceDir, dst, true);
+    assertEquals("foo", IOUtils.slurpFile(dst + File.separator + "d2" + File.separator + "foo"));
+    assertEquals("bar", IOUtils.slurpFile(dst + File.separator + "bar"));
+
+    // clean up
+    assertTrue( foo.delete() );
+    assertTrue( bar.delete() );
+    assertTrue( sourceSubDir.delete() );
+    assertTrue( sourceDir.delete() );
+    assertTrue( new File(dst + File.separator + "d2" + File.separator + "foo").delete() );
+    assertTrue( new File(dst + File.separator + "d2").delete() );
+    assertTrue( new File(dst + File.separator + "bar").delete() );
+    assertTrue( dst.delete() );
+  }
+
 }
