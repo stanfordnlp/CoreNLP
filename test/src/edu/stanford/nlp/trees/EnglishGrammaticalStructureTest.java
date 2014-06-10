9d0a1c3c30becfd4cb4604f7fb8db695fb23b74b
Christopher Manning <manning@stanford.edu>
2014-06-10 16:05:47 -0700

    Break unnecessary depenency of optimization package on sequences.

--- test/src/edu/stanford/nlp/trees/EnglishGrammaticalStructureTest.java
+++ test/src/edu/stanford/nlp/trees/EnglishGrammaticalStructureTest.java
@@ -92,6 +92,8 @@ public class EnglishGrammaticalStructureTest extends TestCase {
          "( (S (NP-SBJ (NN Interest)) (VP (VBZ is) (ADJP-PRD (ADJP (NP-ADV (DT a) (JJ great) (NN deal)) (JJR higher)) (SBAR (IN than) (S (NP-SBJ (PRP it)) (VP (VBD was) (ADJP-PRD (-NONE- *?*)) (ADVP-TMP (NP (DT a) (NN year)) (RB ago))))))) (. .)))",
          "( (S (NP-SBJ (DT The) (NN strike)) (VP (MD may) (VP (VB have) (VP (VBN ended) (SBAR-TMP (ADVP (RB almost)) (IN before) (S (NP-SBJ (PRP it)) (VP (VBD began)))))))))",
          "( (S (SBAR-ADV (IN Although) (S (VP (VBN set) (PP-LOC (IN in) (NP (NNP Japan)))))) (, ,) (NP-SBJ-2 (NP (DT the) (NN novel) (POS 's)) (NN texture)) (VP (VBZ is) (ADJP (JJ American))) (. .)))",
+         "( (S-IMP (INTJ (UH please)) (NP-SBJ (-NONE- *PRO*)) (VP (VB specify) (NP (WDT which) (NML (NNP royal) (CC or) (NNP carnival)) (NN ship))) (NFP -LRB-:)))",
+         "(NP (DT those) (RRC (ADVP-TMP (RB still)) (PP-LOC (IN under) (NP (NNP GASB) (NNS rules)))))",
          // You'd like this one to come out with an nsubjpass, but there are many other cases that are tagging mistakes. Decide what to do
          // "( (S-HLN (NP-SBJ-1 (NN ABORTION) (NN RULING)) (VP (VBN UPHELD) (NP (-NONE- *-1))) (: :)))",
     };
@@ -346,6 +348,19 @@ public class EnglishGrammaticalStructureTest extends TestCase {
             "nsubj(American-11, texture-9)\n" +
             "cop(American-11, is-10)\n" +
             "root(ROOT-0, American-11)\n",
+        "discourse(specify-2, please-1)\n" +
+            "root(ROOT-0, specify-2)\n" +
+            "det(ship-7, which-3)\n" +
+            "nn(ship-7, royal-4)\n" +
+            "cc(royal-4, or-5)\n" +
+            "conj(royal-4, carnival-6)\n" +
+            "dobj(specify-2, ship-7)\n" +
+            "discourse(specify-2, -LRB-:-8)\n",
+        "root(ROOT-0, those-1)\n" +
+                "advmod(under-3, still-2)\n" +
+                "rcmod(those-1, under-3)\n" +
+                "nn(rules-5, GASB-4)\n" +
+                "pobj(under-3, rules-5)\n",
         // "nn(RULING-2, ABORTION-1)\n" +
         //         "nsubjpass(UPHELD-3, RULING-2)\n" +
         //         "root(ROOT-0, UPHELD-3)\n",
