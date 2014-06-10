bc591d6f51e0208dd2eb7b68e77382269190dfac
Gabor Angeli <angeli@cs.stanford.edu>
2014-06-10 16:45:04 -0700

    Minor tweaks

--- src/edu/stanford/nlp/pipeline/PTBTokenizerAnnotator.java
+++ src/edu/stanford/nlp/pipeline/PTBTokenizerAnnotator.java
@@ -9,18 +9,19 @@ import edu.stanford.nlp.process.PTBTokenizer;
 import edu.stanford.nlp.process.Tokenizer;
 
 /**
- * This class will PTB tokenize the input.  It assumes that the original
- * String is under the CoreAnnotations.TextAnnotation field
+ * This class will PTB tokenize the input
+ * string.  It assumes that the original String
+ * is under the CoreAnnotations.TextAnnotation field
  * and it will add the output from the
- * InvertiblePTBTokenizer ({@code List<CoreLabel>}) under
- * CoreAnnotation.TokensAnnotation.
+ * InvertiblePTBTokenizer (List&lt;CoreLabel&gt;) under
+ * Annotation.TokensAnnotation.
  *
  * @author Jenny Finkel
- * @author Christopher Manning
  */
 public class PTBTokenizerAnnotator extends TokenizerAnnotator {
 
-  private final TokenizerFactory<CoreLabel> factory;
+  private TokenizerFactory<CoreLabel> factory =
+    PTBTokenizer.factory(false, true);
 
   public static final String DEFAULT_OPTIONS = "invertible,ptb3Escaping=true";
 
@@ -32,6 +33,10 @@ public class PTBTokenizerAnnotator extends TokenizerAnnotator {
    this(verbose, DEFAULT_OPTIONS);
   }
 
+  public PTBTokenizerAnnotator(String options){
+   this(true, options);
+  }
+
   public PTBTokenizerAnnotator(boolean verbose, String options) {
     super(verbose);
     factory = PTBTokenizer.factory(new CoreLabelTokenFactory(), options);
@@ -41,5 +46,4 @@ public class PTBTokenizerAnnotator extends TokenizerAnnotator {
   Tokenizer<CoreLabel> getTokenizer(Reader r) {
     return factory.getTokenizer(r);
   }
-
 }
