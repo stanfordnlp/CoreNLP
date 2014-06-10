d908daa8994b4bd7240148e9a9e94e8ad28aca27
Angel Chang <angelx@cs.stanford.edu>
2014-06-10 16:06:57 -0700

    Hack CleanXmlAnnotator.java to store utterance/speaker information from XML. Hack dcoref to use stored utterance information. Minor refactoring and spelling corrections.

--- src/edu/stanford/nlp/pipeline/AnnotatorFactory.java
+++ src/edu/stanford/nlp/pipeline/AnnotatorFactory.java
@@ -5,36 +5,30 @@ import edu.stanford.nlp.util.Factory;
 import java.util.Properties;
 
 /**
- * A Factory for creating a certain type of Annotator.
- *
- * @author Mihai Surdeanu
+ * Creates annotators
+ * @author Mihai
  */
 public abstract class AnnotatorFactory implements Factory<Annotator> {
-
-  private static final long serialVersionUID = -1554647325549869340L;
-
   protected final Properties properties;
 
   protected AnnotatorFactory(Properties properties) {
-    // Let's copy the properties, just in case somebody messes with this object later.
-    // By using stringPropertyNames(), we also pick up any defaults the Properties has.
+    // let's copy the properties, just in case somebody messes with this object later
     this.properties = new Properties();
-    for (String key : properties.stringPropertyNames()) {
-      this.properties.setProperty(key, properties.getProperty(key));
+    for(Object key: properties.keySet()) {
+      this.properties.setProperty((String) key, properties.getProperty((String) key));
     }
   }
 
   /**
-   * Creates and returns an Annotator given the local properties.
-   *
+   * Creates and returns an annotator given the local properties
    * @return A new instance of the type T
    */
   @Override
   public abstract Annotator create();
 
   /**
-   * Creates the annotator's signature given the current properties.
-   * We use this to understand if the user wants to recreate
+   * Creates the annotator's signature given the current properties
+   * We use to understand if the user wants to recreate
    * the same annotator type but with different parameters.
    */
   public abstract String signature();
