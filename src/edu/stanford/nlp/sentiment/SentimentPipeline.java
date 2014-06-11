46674ce07e14ffdd98b2f31834e39dcb6f1264aa
Spence Green <spence@spencegreen.com>
2014-06-10 17:32:55 -0700

    Set drop-unknown-words default to false

--- src/edu/stanford/nlp/sentiment/SentimentPipeline.java
+++ src/edu/stanford/nlp/sentiment/SentimentPipeline.java
@@ -41,13 +41,7 @@ public class SentimentPipeline {
     String filename = null;
 
     for (int argIndex = 0; argIndex < args.length; ) {
-      if (args[argIndex].equalsIgnoreCase("-sentimentModel")) {
-        sentimentModel = args[argIndex + 1];
-        argIndex += 2;
-      } else if (args[argIndex].equalsIgnoreCase("-parserModel")) {
-        parserModel = args[argIndex + 1];
-        argIndex += 2;
-      } else if (args[argIndex].equalsIgnoreCase("-file")) {
+      if (args[argIndex].equalsIgnoreCase("-file")) {
         filename = args[argIndex + 1];
         argIndex += 2;
       } else {
