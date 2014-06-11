02c8d1be132a3c06ebf021cbc8775d754b0e2147
Kenneth Heafield <github@kheafield.com>
2014-06-10 18:51:57 -0700

    MurmurHash for int[] because Java can't reinterpret raw data

--- src/edu/stanford/nlp/parser/dvparser/CrossValidateTestOptions.java
+++ src/edu/stanford/nlp/parser/dvparser/CrossValidateTestOptions.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import edu.stanford.nlp.io.IOUtils;
-import edu.stanford.nlp.parser.lexparser.ArgUtils;
+import edu.stanford.nlp.parser.common.ArgUtils;
 import edu.stanford.nlp.parser.lexparser.EvaluateTreebank;
 import edu.stanford.nlp.parser.lexparser.LexicalizedParser;
 import edu.stanford.nlp.trees.Treebank;
