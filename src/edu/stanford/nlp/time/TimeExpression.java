2a10e519f68e6402c70fde86c1af14990c50193f
sonalg <sonalg@stanford.edu>
2014-06-10 17:00:42 -0700

    writing ctakes files

--- src/edu/stanford/nlp/time/TimeExpression.java
+++ src/edu/stanford/nlp/time/TimeExpression.java
@@ -23,15 +23,6 @@ import java.util.List;
 public class TimeExpression extends MatchedExpression {
 
   /**
-   * The CoreMap key for storing a SUTime.TimeIndex (for looking up Timex Id).
-   */
-  public static class TimeIndexAnnotation implements CoreAnnotation<SUTime.TimeIndex> {
-    public Class<SUTime.TimeIndex> getType() {
-      return SUTime.TimeIndex.class;
-    }
-  }
-
-  /**
    * The CoreMap key for storing a TimeExpression annotation.
    */
   public static class Annotation implements CoreAnnotation<TimeExpression> {
@@ -52,7 +43,7 @@ public class TimeExpression extends MatchedExpression {
   }
 
   //int tid;     // Time ID
-  SUTime.Temporal origTemporal;  // todo [2013]: never read. Can delete? (Set in TimeExpressionExtractorImpl)
+  SUTime.Temporal origTemporal;  // never read. Can delete?
   //int anchorTimeId = -1;
 
   public TimeExpression(MatchedExpression expr)
@@ -143,7 +134,6 @@ public class TimeExpression extends MatchedExpression {
     }
     return null;
   }
-
   public void setTemporal(SUTime.Temporal temporal) {
     this.value = new Expressions.PrimitiveValue<SUTime.Temporal>("Temporal", temporal);
   }
