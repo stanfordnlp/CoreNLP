5b84d9e9128efef4a590daae6df12ce08b87a9f5
Sonal Gupta <sonalg@stanford.edu>
2014-06-10 17:14:18 -0700

    restricting

--- src/edu/stanford/nlp/dcoref/CorefChain.java
+++ src/edu/stanford/nlp/dcoref/CorefChain.java
@@ -265,7 +265,7 @@ public class CorefChain implements Serializable {
     private static final long serialVersionUID = 3657691243504173L;
   }
 
-  protected static class CorefMentionComparator implements Comparator<CorefMention> {
+  protected static class MentionComparator implements Comparator<CorefMention> {
     @Override
     public int compare(CorefMention m1, CorefMention m2) {
       if(m1.sentNum < m2.sentNum) return -1;
@@ -281,24 +281,6 @@ public class CorefChain implements Serializable {
       }
     }
   }
-
-  protected static class MentionComparator implements Comparator<Mention> {
-    @Override
-    public int compare(Mention m1, Mention m2) {
-      if(m1.sentNum < m2.sentNum) return -1;
-      else if(m1.sentNum > m2.sentNum) return 1;
-      else{
-        if(m1.startIndex < m2.startIndex) return -1;
-        else if(m1.startIndex > m2.startIndex) return 1;
-        else {
-          if(m1.endIndex > m2.endIndex) return -1;
-          else if(m1.endIndex < m2.endIndex) return 1;
-          else return 0;
-        }
-      }
-    }
-  }
-
   public CorefChain(CorefCluster c, Map<Mention, IntTuple> positions){
     chainID = c.clusterID;
     // Collect mentions
@@ -309,7 +291,7 @@ public class CorefChain implements Serializable {
       CorefMention men = new CorefMention(m, positions.get(m));
       mentions.add(men);
     }
-    Collections.sort(mentions, new CorefMentionComparator());
+    Collections.sort(mentions, new MentionComparator());
     // Find representative mention
     for (CorefMention men : mentions) {
       IntPair position = new IntPair(men.sentNum, men.headIndex);
@@ -335,7 +317,7 @@ public class CorefChain implements Serializable {
         this.mentions.add(m);
       }
     }
-    Collections.sort(mentions, new CorefMentionComparator());
+    Collections.sort(mentions, new MentionComparator());
   }
 
   public String toString(){
