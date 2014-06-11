7e66aa890f2581606bce4904a4186b7f262bc783
Thang Luong <lmthang@stanford.edu>
2014-06-10 17:02:02 -0700

    Merge branch 'master' of origin into lmthang

--- src/edu/stanford/nlp/pipeline/GenericAnnotationSerializer.java
+++ src/edu/stanford/nlp/pipeline/GenericAnnotationSerializer.java
@@ -1,31 +1,56 @@
 package edu.stanford.nlp.pipeline;
 
-import edu.stanford.nlp.pipeline.Annotation;
-import edu.stanford.nlp.pipeline.AnnotationSerializer;
+import edu.stanford.nlp.util.Pair;
 
 import java.io.*;
 
 /**
  * Serializes Annotation objects using the default Java serializer
  */
-public class GenericAnnotationSerializer implements AnnotationSerializer {
+public class GenericAnnotationSerializer extends AnnotationSerializer {
+
+  /** Turns out, an ObjectOutputStream cannot append to a file. This is dumb. */
+  public class AppendingObjectOutputStream extends ObjectOutputStream {
+    public AppendingObjectOutputStream(OutputStream out) throws IOException {
+      super(out);
+    }
+    @Override
+    protected void writeStreamHeader() throws IOException {
+      // do not write a header, but reset
+      reset();
+    }
+  }
 
   @Override
-  public Annotation load(InputStream is) throws IOException, ClassNotFoundException, ClassCastException {
-    ObjectInputStream objectInput = new ObjectInputStream(is);
+  public OutputStream append(Annotation corpus, OutputStream os) throws IOException {
+    if (os instanceof AppendingObjectOutputStream) {
+      ((AppendingObjectOutputStream) os).writeObject(corpus);
+      return os;
+    } else if (os instanceof ObjectOutputStream) {
+      ObjectOutputStream objectOutput = new AppendingObjectOutputStream(os);
+      objectOutput.writeObject(corpus);
+      return objectOutput;
+    } else {
+      ObjectOutputStream objectOutput = new ObjectOutputStream(os);
+      objectOutput.writeObject(corpus);
+      return objectOutput;
+    }
+  }
+
+  @Override
+  public Pair<Annotation, InputStream> read(InputStream is) throws IOException, ClassNotFoundException, ClassCastException {
+    ObjectInputStream objectInput;
+    if (is instanceof ObjectInputStream) {
+      objectInput = (ObjectInputStream) is;
+    } else {
+      objectInput = new ObjectInputStream(is);
+    }
     Object annotation = objectInput.readObject();
     if(annotation == null) return null;
     if(! (annotation instanceof Annotation)){
       throw new ClassCastException("ERROR: Serialized data does not contain an Annotation!");
     }
-    return (Annotation) annotation;
+    return Pair.makePair((Annotation) annotation, (InputStream) objectInput);
   }
 
-  @Override
-  public void save(Annotation corpus, OutputStream os) throws IOException {
-    ObjectOutputStream objectOutput = new ObjectOutputStream(os);
-    objectOutput.writeObject(corpus);
-    objectOutput.close();
-  }
-  
 }
