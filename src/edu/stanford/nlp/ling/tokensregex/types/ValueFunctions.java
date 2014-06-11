da6558c5a42249e12763a2aea3b8657b55e1c42c
sonalg <sonalg@stanford.edu>
2014-06-10 17:05:13 -0700

    numday

--- src/edu/stanford/nlp/ling/tokensregex/types/ValueFunctions.java
+++ src/edu/stanford/nlp/ling/tokensregex/types/ValueFunctions.java
@@ -1184,10 +1184,6 @@ public class ValueFunctions {
       if (index < 0) {
         index = list.size() + index;
       }
-      if (index >= list.size() || index < 0) {
-        // index out of bounds
-        return null;
-      }
       if (in.size() >= 3) {
         Value fieldValue = in.get(2);
         if (fieldValue != null) {
