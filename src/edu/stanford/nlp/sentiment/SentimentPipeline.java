7be0ae2488691aa6690726b7cc64674899eedd35
Spence Green <spence@spencegreen.com>
2014-06-10 17:43:24 -0700

    Remove legacy featurizer stuff

--- src/edu/stanford/nlp/sentiment/SentimentPipeline.java
+++ src/edu/stanford/nlp/sentiment/SentimentPipeline.java
@@ -1,7 +1,5 @@
 package edu.stanford.nlp.sentiment;
 
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.util.Properties;
 
 import edu.stanford.nlp.io.IOUtils;
@@ -19,26 +17,33 @@ import edu.stanford.nlp.util.CoreMap;
  * @author John Bauer
  */
 public class SentimentPipeline {
-  public static void main(String[] args) throws IOException {
+  public static String sentimentString(int sentiment) {
+    switch(sentiment) {
+    case 0:
+      return "Very negative";
+    case 1:
+      return "Negative";
+    case 2:
+      return "Neutral";
+    case 3:
+      return "Positive";
+    case 4:
+      return "Very positive";
+    default:
+      return "Unknown sentiment label " + sentiment;
+    }
+  }
+
+  public static void main(String[] args) {
     String parserModel = null;
     String sentimentModel = null;
 
     String filename = null;
-    boolean stdin = false;
 
     for (int argIndex = 0; argIndex < args.length; ) {
-      if (args[argIndex].equalsIgnoreCase("-sentimentModel")) {
-        sentimentModel = args[argIndex + 1];
-        argIndex += 2;
-      } else if (args[argIndex].equalsIgnoreCase("-parserModel")) {
-        parserModel = args[argIndex + 1];
-        argIndex += 2;
-      } else if (args[argIndex].equalsIgnoreCase("-file")) {
+      if (args[argIndex].equalsIgnoreCase("-file")) {
         filename = args[argIndex + 1];
         argIndex += 2;
-      } else if (args[argIndex].equalsIgnoreCase("-stdin")) {
-        stdin = true;
-        argIndex++;
       } else {
         System.err.println("Unknown argument " + args[argIndex + 1]);
         throw new IllegalArgumentException("Unknown argument " + args[argIndex + 1]);
@@ -53,55 +58,17 @@ public class SentimentPipeline {
     if (parserModel != null) {
       props.setProperty("parse.model", parserModel);
     }
-
-    if (filename != null && stdin) {
-      throw new IllegalArgumentException("Please only specify one of -file or -stdin");
-    }
-    if (filename == null && !stdin) {
-      throw new IllegalArgumentException("Please specify either -file or -stdin");
-    }
-
-    if (stdin) {
-      props.setProperty("ssplit.eolonly", "true");
-    }
     StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
 
-    if (filename != null) {
-      String text = IOUtils.slurpFileNoExceptions(filename);
-      Annotation annotation = new Annotation(text);
-      pipeline.annotate(annotation);
+    String text = IOUtils.slurpFileNoExceptions(filename);
+    Annotation annotation = new Annotation(text);
+    pipeline.annotate(annotation);
 
-      for (CoreMap sentence : annotation.get(CoreAnnotations.SentencesAnnotation.class)) {
-        Tree tree = sentence.get(SentimentCoreAnnotations.AnnotatedTree.class);
-        int sentiment = RNNCoreAnnotations.getPredictedClass(tree);
-        System.err.println(sentence);
-        System.err.println("  Predicted sentiment: " + SentimentUtils.sentimentString(sentiment));
-      }
-    } else {
-      System.err.println("Reading in text from stdin.");
-      System.err.println("Please enter one sentence per line.");
-      System.err.println("Processing will end when EOF is reached.");
-      BufferedReader reader = new BufferedReader(IOUtils.encodedInputStreamReader(System.in, "utf-8"));
-      while (true) {
-        String line = reader.readLine();
-        if (line == null) {
-          break;
-        }
-        line = line.trim();
-        if (line.length() > 0) {
-          Annotation annotation = pipeline.process(line);
-          for (CoreMap sentence : annotation.get(CoreAnnotations.SentencesAnnotation.class)) {
-            Tree tree = sentence.get(SentimentCoreAnnotations.AnnotatedTree.class);
-            int sentiment = RNNCoreAnnotations.getPredictedClass(tree);
-            System.err.println("  " + SentimentUtils.sentimentString(sentiment));
-          }
-        } else {
-          // Output blank lines for blank lines so the tool can be
-          // used for line-by-line text processing
-          System.err.println("");
-        }
-      }
-      
+    for (CoreMap sentence : annotation.get(CoreAnnotations.SentencesAnnotation.class)) {
+      Tree tree = sentence.get(SentimentCoreAnnotations.AnnotatedTree.class);
+      int sentiment = RNNCoreAnnotations.getPredictedClass(tree);
+      System.err.println(sentence);
+      System.err.println("  Predicted sentiment: " + sentimentString(sentiment));
     }
   }
 }
