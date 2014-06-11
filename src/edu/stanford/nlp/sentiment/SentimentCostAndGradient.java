643e98fce4dce4c13799a4702955e8c2b7e2c70c
Angel Chang <angelx@cs.stanford.edu>
2014-06-10 18:06:28 -0700

    Add TokensRegex demo example.

--- src/edu/stanford/nlp/sentiment/SentimentCostAndGradient.java
+++ src/edu/stanford/nlp/sentiment/SentimentCostAndGradient.java
@@ -218,19 +218,13 @@ public class SentimentCostAndGradient extends AbstractCachingDiffFunction {
     // Build a vector that looks like 0,0,1,0,0 with an indicator for the correct class
     SimpleMatrix goldLabel = new SimpleMatrix(model.numClasses, 1);
     int goldClass = RNNCoreAnnotations.getGoldClass(tree);
-    if (goldClass >= 0) {
-      goldLabel.set(goldClass, 1.0);
-    }
+    goldLabel.set(goldClass, 1.0);
 
     double nodeWeight = model.op.trainOptions.getClassWeight(goldClass);
 
     SimpleMatrix predictions = RNNCoreAnnotations.getPredictions(tree);
 
-    // If this is an unlabeled class, set deltaClass to 0.  We could
-    // make this more efficient by eliminating various of the below
-    // calculations, but this would be the easiest way to handle the
-    // unlabeled class
-    SimpleMatrix deltaClass = goldClass >= 0 ? predictions.minus(goldLabel).scale(nodeWeight) : new SimpleMatrix(predictions.numRows(), predictions.numCols());
+    SimpleMatrix deltaClass = predictions.minus(goldLabel).scale(nodeWeight);
     SimpleMatrix localCD = deltaClass.mult(NeuralUtils.concatenateWithBias(currentVector).transpose());
 
     double error = -(NeuralUtils.elementwiseApplyLog(predictions).elementMult(goldLabel).elementSum());
