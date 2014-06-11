6358b0f5ae7a7da64a8aba41e2a465773aae0462
John Bauer <horatio@gmail.com>
2014-06-10 18:51:11 -0700

    Add a mechanism to combine unary transitions into one transition. Unfortunately, so far no improvement

--- src/edu/stanford/nlp/parser/dvparser/DVParser.java
+++ src/edu/stanford/nlp/parser/dvparser/DVParser.java
@@ -18,10 +18,10 @@ import edu.stanford.nlp.io.RuntimeIOException;
 import edu.stanford.nlp.ling.Word;
 import edu.stanford.nlp.math.ArrayMath;
 import edu.stanford.nlp.optimization.QNMinimizer;
-import edu.stanford.nlp.parser.lexparser.ArgUtils;
+import edu.stanford.nlp.parser.common.ArgUtils;
+import edu.stanford.nlp.parser.common.ParserQuery;
 import edu.stanford.nlp.parser.lexparser.EvaluateTreebank;
 import edu.stanford.nlp.parser.lexparser.LexicalizedParser;
-import edu.stanford.nlp.parser.lexparser.ParserQuery;
 import edu.stanford.nlp.parser.lexparser.Options;
 import edu.stanford.nlp.parser.lexparser.TrainOptions;
 import edu.stanford.nlp.trees.CompositeTreeTransformer;
@@ -304,11 +304,11 @@ public class DVParser {
     this.parser = parser;
     this.op = parser.getOp();
 
-    if (op.trainOptions.dvSeed == 0) {
-      op.trainOptions.dvSeed = (new Random()).nextLong();
-      System.err.println("Random seed not set, using randomly chosen seed of " + op.trainOptions.dvSeed);
+    if (op.trainOptions.randomSeed == 0) {
+      op.trainOptions.randomSeed = (new Random()).nextLong();
+      System.err.println("Random seed not set, using randomly chosen seed of " + op.trainOptions.randomSeed);
     } else {
-      System.err.println("Random seed set to " + op.trainOptions.dvSeed);
+      System.err.println("Random seed set to " + op.trainOptions.randomSeed);
     }
 
     System.err.println("Word vector file: " + op.lexOptions.wordVectorFile);
