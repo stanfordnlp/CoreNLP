a8cf3cf07073e2d1656b59760dde22b83eb1b31c
John Bauer <horatio@gmail.com>
2014-06-10 17:21:40 -0700

    Rename Options to RNNOptions

---
+++ src/edu/stanford/nlp/sentiment/RNNTrainOptions.java
@@ -0,0 +1,17 @@
+package edu.stanford.nlp.sentiment;
+
+import java.io.Serializable;
+
+public class RNNTrainOptions implements Serializable {
+  public int batchSize;
+
+  public int iterations;
+
+  public int debugOutputSeconds;
+
+  public int maxTrainTimeSeconds;
+
+  public double learningRate;
+
+  public double scalingForInit;
+}
--- src/edu/stanford/nlp/util/PropertiesUtils.java
+++ src/edu/stanford/nlp/util/PropertiesUtils.java
@@ -6,7 +6,6 @@ import java.io.StringReader;
 import java.io.StringWriter;
 import java.lang.reflect.Type;
 import java.util.Enumeration;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -82,7 +81,7 @@ public class PropertiesUtils {
   }
   
   /**
-   * Tired of Properties not behaving like Map<String,String>s?  This method will solve that problem for you.
+   * Tired of Properties not behaving like {@code Map<String,String>}s?  This method will solve that problem for you.
    */
   public static Map<String, String> asMap(Properties properties) {
     Map<String, String> map = Generics.newHashMap();
@@ -99,7 +98,8 @@ public class PropertiesUtils {
   /**
    * Checks to make sure that all properties specified in <code>properties</code>
    * are known to the program by checking that each simply overrides
-   * a default value
+   * a default value.
+   *
    * @param properties Current properties
    * @param defaults Default properties which lists all known keys
    */
