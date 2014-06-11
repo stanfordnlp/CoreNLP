aa48c9539b8db26c4b680f879c6f4616ddd61d86
Spence Green <spence@spencegreen.com>
2014-06-10 18:39:03 -0700

    Tweaking the n-best list extraction

--- src/edu/stanford/nlp/pipeline/TokensRegexNERAnnotator.java
+++ src/edu/stanford/nlp/pipeline/TokensRegexNERAnnotator.java
@@ -149,6 +149,10 @@ public class TokensRegexNERAnnotator implements Annotator {
           new PropertiesUtils.Property("verbose", "false", ""),
   };
 
+  /** Construct a new TokensRegexAnnotator.
+   *
+   *  @param mapping A comma-separated list of files, URLs, or classpath resources to load mappings from
+   */
   public TokensRegexNERAnnotator(String mapping) {
     this(mapping, false);
   }
