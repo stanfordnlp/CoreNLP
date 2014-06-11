eadb8710cbc43c0f81a25335fab1d3bc20a6a39a
sonalg <sonalg@stanford.edu>
2014-06-10 19:00:46 -0700

    debugging

--- src/edu/stanford/nlp/patterns/surface/ConstantsAndVariables.java
+++ src/edu/stanford/nlp/patterns/surface/ConstantsAndVariables.java
@@ -579,6 +579,8 @@ public class ConstantsAndVariables implements Serializable{
   @Option(name="saveSentencesSerDir")
   public String saveSentencesSerDir = null;
   
+  public boolean usingDirForSentsInIndex = false;
+  
   // @Option(name = "wekaOptions")
   // public String wekaOptions = "";
 
