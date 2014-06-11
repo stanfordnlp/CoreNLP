ab417a3d6e922319fffd209e19abfcdf8967d7b5
Spence Green <spence@spencegreen.com>
2014-06-10 18:42:46 -0700

    Fix ClassCastException

--- src/edu/stanford/nlp/sequences/DocumentReaderAndWriter.java
+++ src/edu/stanford/nlp/sequences/DocumentReaderAndWriter.java
@@ -9,10 +9,10 @@ import java.io.Serializable;
 
 /**
  * This interface is used for reading data and writing
- * output into and out of sequence classifiers.
+ * output into and out of {@link SequenceClassifier}s.
  * If you subclass this interface, all of the other
  * mechanisms necessary for getting your data into a
- * sequence classifier will be taken care of
+ * {@link SequenceClassifier} will be taken care of
  * for you.  Subclasses <b>MUST</b> have an empty constructor as
  * they can be instantiated by reflection, and
  * there is a promise that the init method will
