88b9580fa65d845924cb59115c7bd37d4d9a8bfc
John Bauer <horatio@gmail.com>
2014-06-10 18:55:37 -0700

    Merge branch 'srparser' of origin into srparser

--- test/src/edu/stanford/nlp/parser/shiftreduce/ShiftReduceParserTest.java
+++ test/src/edu/stanford/nlp/parser/shiftreduce/ShiftReduceParserTest.java
@@ -16,7 +16,7 @@ import edu.stanford.nlp.util.Function;
 import edu.stanford.nlp.parser.lexparser.TreeBinarizer;
 import edu.stanford.nlp.trees.PennTreebankLanguagePack;
 
-public class TrainParserTest extends TestCase {
+public class ShiftReduceParserTest extends TestCase {
   String commaTreeString = "(ROOT (FRAG (NP (DT A) (@NP (ADJP (JJ short) (@ADJP (, ,) (JJ simple))) (NN test)))))";
 
   String[] treeStrings = {
