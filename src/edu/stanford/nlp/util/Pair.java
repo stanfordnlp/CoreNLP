53c65db1b257a72aada632a44d05a0c64af55da6
malcdi <malcdi@stanford.edu>
2014-06-10 18:37:31 -0700

    testing tfidf filtering

--- src/edu/stanford/nlp/util/Pair.java
+++ src/edu/stanford/nlp/util/Pair.java
@@ -158,22 +158,12 @@ public class Pair <T1,T2> implements Comparable<Pair<T1,T2>>, Serializable, Pret
    */
   @SuppressWarnings("unchecked")
   public int compareTo(Pair<T1,T2> another) {
-    if (first() instanceof Comparable) {
-      int comp = ((Comparable<T1>) first()).compareTo(another.first());
-      if (comp != 0) {
-        return comp;
-      }
-    }
-
-    if (second() instanceof Comparable) {
+    int comp = ((Comparable<T1>) first()).compareTo(another.first());
+    if (comp != 0) {
+      return comp;
+    } else {
       return ((Comparable<T2>) second()).compareTo(another.second());
     }
-
-    if ((!(first() instanceof Comparable)) && (!(second() instanceof Comparable))) {
-      throw new AssertionError("Neither element of pair comparable");
-    }
-
-    return 0;
   }
 
   /**
