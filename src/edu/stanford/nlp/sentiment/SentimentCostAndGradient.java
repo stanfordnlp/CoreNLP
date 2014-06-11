fdaa79ba0657702c3bfc823596b179f5c61c04bc
Spence Green <spence@spencegreen.com>
2014-06-10 17:17:35 -0700

    Cleanup debug output

--- src/edu/stanford/nlp/sentiment/SentimentCostAndGradient.java
+++
@@ -1,7 +0,0 @@
-package edu.stanford.nlp.sentiment;
-
-import edu.stanford.nlp.optimization.AbstractCachingDiffFunction;
-
-public class SentimentCostAndGradient extends AbstractCachingDiffFunction {
-
-}
