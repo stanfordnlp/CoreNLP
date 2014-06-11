4c00d26347802c9f08f29213e6428c75639750d1
Sonal Gupta <sonalg@stanford.edu>
2014-06-10 17:04:17 -0700

    metamap

--- src/edu/stanford/nlp/util/PropertiesUtils.java
+++ src/edu/stanford/nlp/util/PropertiesUtils.java
@@ -1,9 +1,6 @@
 package edu.stanford.nlp.util;
 
-import java.io.IOException;
 import java.io.PrintStream;
-import java.io.StringReader;
-import java.io.StringWriter;
 import java.lang.reflect.Type;
 import java.util.Enumeration;
 import java.util.HashMap;
@@ -35,27 +32,6 @@ public class PropertiesUtils {
     return ! (value.equals("false") || value.equals("no") || value.equals("off"));
   }
 
-  // Convert from properties to string and from string to properties
-  public static String asString(Properties props) {
-    try {
-      StringWriter sw = new StringWriter();
-      props.store(sw, null);
-      return sw.toString();
-    } catch (IOException ex) {
-      throw new RuntimeException(ex);
-    }
-  }
-
-  public static Properties fromString(String str) {
-    try {
-      StringReader sr = new StringReader(str);
-      Properties props = new Properties();
-      props.load(sr);
-      return props;
-    } catch (IOException ex) {
-      throw new RuntimeException(ex);
-    }
-  }
 
   // printing -------------------------------------------------------------------
 
@@ -237,16 +213,9 @@ public class PropertiesUtils {
   public static String[] getStringArray(Properties props, String key) {
     String[] results = MetaClass.cast(props.getProperty(key), String [].class);
     if (results == null) {
-      results = new String[]{};
+      results =new String[] {};
     }
-    return results;
-  }
-
-  public static String[] getStringArray(Properties props, String key, String[] defaults) {
-    String[] results = MetaClass.cast(props.getProperty(key), String [].class);
-    if (results == null) {
-      results = defaults;
-    }
+    
     return results;
   }
 }
