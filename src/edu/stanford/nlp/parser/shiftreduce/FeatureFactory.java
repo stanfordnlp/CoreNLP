8e46d9c071411536aafa2861e7b7788840efa723
sonalg <sonalg@stanford.edu>
2014-06-10 18:50:17 -0700

    moved things around, split phrasescorer into three files

--- src/edu/stanford/nlp/parser/shiftreduce/FeatureFactory.java
+++
@@ -1,8 +0,0 @@
-package edu.stanford.nlp.parser.shiftreduce;
-
-import java.io.Serializable;
-import java.util.Set;
-
-public interface FeatureFactory extends Serializable {
-  Set<String> featurize(State state);
-}
