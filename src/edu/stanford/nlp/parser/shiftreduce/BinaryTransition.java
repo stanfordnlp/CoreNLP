a308903fd9c108b9d22470481ea9c18eeff2bea1
John Bauer <horatio@gmail.com>
2014-06-10 18:51:00 -0700

    Add a commented out example command line

---
+++ src/edu/stanford/nlp/parser/shiftreduce/BinaryTransition.java
@@ -0,0 +1,128 @@
+package edu.stanford.nlp.parser.shiftreduce;
+
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.trees.LabeledScoredTreeNode;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.TreeCoreAnnotations;
+import edu.stanford.nlp.util.TreeShapedStack;
+
+/**
+ * Transition that makes a binary parse node in a partially finished tree.
+ */
+public class BinaryTransition implements Transition {
+  public final String label;
+
+  /** Which side the head is on */
+  public final Side side;
+
+  public enum Side {
+    LEFT, RIGHT
+  }
+
+  public BinaryTransition(String label, Side side) {
+    this.label = label;
+    this.side = side;
+  }
+
+  /**
+   * Legal as long as there are at least two items on the state's stack.
+   *
+   * TODO: implement other measures of legality
+   */
+  public boolean isLegal(State state) {
+    if (state.finished) {
+      return false;
+    }
+    if (state.stack.size() <= 1) {
+      return false;
+    }
+    // don't allow binarized labels if it makes the state have a stack
+    // of size 1 and a queue of size 0
+    if (state.stack.size() == 2 && isBinarized() && state.endOfQueue()) {
+      return false;
+    }
+    return true;
+  }
+
+  public boolean isBinarized() {
+    return (label.charAt(0) == '@');
+  }
+
+  /**
+   * Add a binary node to the existing node on top of the stack
+   */
+  public State apply(State state) {
+    TreeShapedStack<Tree> stack = state.stack;
+    Tree right = stack.peek();
+    stack = stack.pop();
+    Tree left = stack.peek();
+    stack = stack.pop();
+    
+    Tree head;
+    switch(side) {
+    case LEFT:
+      head = left;
+      break;
+    case RIGHT:
+      head = right;
+      break;
+    default:
+      throw new IllegalArgumentException("Unknown side " + side);
+    }
+    
+    if (!(head.label() instanceof CoreLabel)) {
+      throw new IllegalArgumentException("Stack should have CoreLabel nodes");
+    }
+    CoreLabel headLabel = (CoreLabel) head.label();
+
+    CoreLabel production = new CoreLabel();
+    production.setValue(label);
+    production.set(TreeCoreAnnotations.HeadWordAnnotation.class, headLabel.get(TreeCoreAnnotations.HeadWordAnnotation.class));
+    production.set(TreeCoreAnnotations.HeadTagAnnotation.class, headLabel.get(TreeCoreAnnotations.HeadTagAnnotation.class));
+    Tree newTop = new LabeledScoredTreeNode(production);
+    newTop.addChild(left);
+    newTop.addChild(right);
+
+    stack = stack.push(newTop);
+    return new State(stack, state.transitions.push(this), state.sentence, state.tokenPosition, state.score, false);    
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == this) {
+      return true;
+    }
+    if (!(o instanceof BinaryTransition)) {
+      return false;
+    }
+    String otherLabel = ((BinaryTransition) o).label;
+    Side otherSide = ((BinaryTransition) o).side;
+    return otherSide.equals(side) && label.equals(otherLabel);
+  }
+
+  @Override
+  public int hashCode() {
+    switch(side) {
+    case LEFT:
+      return 97197711 ^ label.hashCode();
+    case RIGHT:
+      return 97197711 ^ label.hashCode();
+    default:
+      throw new IllegalArgumentException("Unknown side " + side);
+    }
+  }
+
+  @Override
+  public String toString() {
+    switch(side) {
+    case LEFT:
+      return "LeftBinary(" + label + ")";
+    case RIGHT:
+      return "RightBinary(" + label + ")";
+    default:
+      throw new IllegalArgumentException("Unknown side " + side);
+    }
+  }
+
+  private static final long serialVersionUID = 1;  
+}
