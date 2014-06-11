cd8c5c6676a5756a812616b2e38355cd9a10a774
heeyoung <heeyoung@stanford.edu>
2014-06-10 17:12:48 -0700

    Merged master fixed conflict.

--- src/edu/stanford/nlp/dcoref/CorefChain.java
+++ src/edu/stanford/nlp/dcoref/CorefChain.java
@@ -265,7 +265,7 @@ public class CorefChain implements Serializable {
     private static final long serialVersionUID = 3657691243504173L;
   }
 
-  protected static class MentionComparator implements Comparator<CorefMention> {
+  protected static class CorefMentionComparator implements Comparator<CorefMention> {
     @Override
     public int compare(CorefMention m1, CorefMention m2) {
       if(m1.sentNum < m2.sentNum) return -1;
@@ -281,6 +281,24 @@ public class CorefChain implements Serializable {
       }
     }
   }
+
+  protected static class MentionComparator implements Comparator<Mention> {
+    @Override
+    public int compare(Mention m1, Mention m2) {
+      if(m1.sentNum < m2.sentNum) return -1;
+      else if(m1.sentNum > m2.sentNum) return 1;
+      else{
+        if(m1.startIndex < m2.startIndex) return -1;
+        else if(m1.startIndex > m2.startIndex) return 1;
+        else {
+          if(m1.endIndex > m2.endIndex) return -1;
+          else if(m1.endIndex < m2.endIndex) return 1;
+          else return 0;
+        }
+      }
+    }
+  }
+
   public CorefChain(CorefCluster c, Map<Mention, IntTuple> positions){
     chainID = c.clusterID;
     // Collect mentions
@@ -291,7 +309,7 @@ public class CorefChain implements Serializable {
       CorefMention men = new CorefMention(m, positions.get(m));
       mentions.add(men);
     }
-    Collections.sort(mentions, new MentionComparator());
+    Collections.sort(mentions, new CorefMentionComparator());
     // Find representative mention
     for (CorefMention men : mentions) {
       IntPair position = new IntPair(men.sentNum, men.headIndex);
@@ -317,7 +335,7 @@ public class CorefChain implements Serializable {
         this.mentions.add(m);
       }
     }
-    Collections.sort(mentions, new MentionComparator());
+    Collections.sort(mentions, new CorefMentionComparator());
   }
 
   public String toString(){
