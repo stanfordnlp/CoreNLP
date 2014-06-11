35c440271401cdf6bf7de8c7f93dc3f5996282bf
John Bauer <horatio@gmail.com>
2014-06-10 17:38:05 -0700

    Merge branch 'master' of /u/nlp/git/javanlp

--- src/edu/stanford/nlp/sentiment/SentimentPipeline.java
+++ src/edu/stanford/nlp/sentiment/SentimentPipeline.java
@@ -17,23 +17,6 @@ import edu.stanford.nlp.util.CoreMap;
  * @author John Bauer
  */
 public class SentimentPipeline {
-  public static String sentimentString(int sentiment) {
-    switch(sentiment) {
-    case 0:
-      return "Very negative";
-    case 1:
-      return "Negative";
-    case 2:
-      return "Neutral";
-    case 3:
-      return "Positive";
-    case 4:
-      return "Very positive";
-    default:
-      return "Unknown sentiment label " + sentiment;
-    }
-  }
-
   public static void main(String[] args) {
     String parserModel = null;
     String sentimentModel = null;
@@ -74,7 +57,7 @@ public class SentimentPipeline {
       Tree tree = sentence.get(SentimentCoreAnnotations.AnnotatedTree.class);
       int sentiment = RNNCoreAnnotations.getPredictedClass(tree);
       System.err.println(sentence);
-      System.err.println("  Predicted sentiment: " + sentimentString(sentiment));
+      System.err.println("  Predicted sentiment: " + SentimentUtils.sentimentString(sentiment));
     }
   }
 }
