f5160f15d3d95a3b99bdca7d2730f6aa760a0f80
John Bauer <horatio@gmail.com>
2014-06-10 18:47:58 -0700

    Add a comment

--- src/edu/stanford/nlp/trees/tregex/tsurgeon/IfExistsNode.java
+++ src/edu/stanford/nlp/trees/tregex/tsurgeon/IfExistsNode.java
@@ -4,6 +4,10 @@ import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.trees.tregex.TregexMatcher;
 
 /**
+ * Executes the give children only if the named Tregex node exists in
+ * the TregexMatcher at match time (allows for OR relations or
+ * optional relations)
+ *
  * @author John Bauer (horatio@gmail.com)
  */
 class IfExistsNode extends TsurgeonPattern {
