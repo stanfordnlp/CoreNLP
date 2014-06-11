48904db7c3432e1fd9db014f345ac2037f58af63
sonalg <sonalg@stanford.edu>
2014-06-10 18:01:09 -0700

    evaluate getpatterns

--- src/edu/stanford/nlp/ie/crf/CRFClassifierNonlinear.java
+++ src/edu/stanford/nlp/ie/crf/CRFClassifierNonlinear.java
@@ -51,7 +51,7 @@ import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
 /**
- * Subclass of {@link edu.stanford.nlp.ie.crf.CRFClassifier} for implementing the nonlinear architecture in [Wang and Manning IJCNLP-2013 Effect of Nonlinear ...].
+ * Subclass of {@link edu.stanford.edu.ie.crf.CRFClassifier} for implementing the nonlinear architecture in [Wang and Manning IJCNLP-2013 Effect of Nonlinear ...].
  *
  * @author Mengqiu Wang 
  */
@@ -111,7 +111,7 @@ public class CRFClassifierNonlinear<IN extends CoreMap> extends CRFClassifier<IN
   }
 
   @Override
-  protected CliquePotentialFunction getCliquePotentialFunctionForTest() {
+  protected CliquePotentialFunction getCliquePotentialFunction() {
     if (cliquePotentialFunction == null) {
       if (flags.secondOrderNonLinear)
         cliquePotentialFunction = new NonLinearSecondOrderCliquePotentialFunction(inputLayerWeights4Edge, outputLayerWeights4Edge, inputLayerWeights, outputLayerWeights, flags);
