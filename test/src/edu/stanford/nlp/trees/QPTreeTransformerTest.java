c8aa290cbefe6b5d8784867b010e9f5f1c2f3e23
John Bauer <horatio@gmail.com>
2014-06-28 07:54:14 -0700

    Also fixed this case of internal QP structure

--- test/src/edu/stanford/nlp/trees/QPTreeTransformerTest.java
+++ test/src/edu/stanford/nlp/trees/QPTreeTransformerTest.java
@@ -11,9 +11,8 @@ public class QPTreeTransformerTest extends TestCase {
   public void testMoneyOrMore() {
     String input = "(ROOT (S (NP (DT This)) (VP (VBZ costs) (NP (QP ($ $) (CD 1) (CD million)) (QP (CC or) (JJR more)))) (. .)))";
     // First it gets flattened, then the CC gets broken up
-    // TODO: the end result of NP on the left side should be QP with internal structure
     // TODO: NP for the right?
-    String output = "(ROOT (S (NP (DT This)) (VP (VBZ costs) (NP (QP (NP ($ $) (CD 1) (CD million)) (CC or) (NP (JJR more))))) (. .)))";
+    String output = "(ROOT (S (NP (DT This)) (VP (VBZ costs) (NP (QP (QP ($ $) (QP (CD 1) (CD million))) (CC or) (NP (JJR more))))) (. .)))";
     runTest(input, output);
   }
 
