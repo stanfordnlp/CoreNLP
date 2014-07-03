19d0c80e2ccd7c2dad2e77608d46bb28c4e84d05
sonalg <sonalg@stanford.edu>
2014-07-02 18:23:30 -0700

    change things to _pipelineNER

--- src/edu/stanford/nlp/ie/machinereading/structure/Span.java
+++ src/edu/stanford/nlp/ie/machinereading/structure/Span.java
@@ -191,21 +191,4 @@ public class Span implements Serializable, Iterable<Integer> {
     }
     return false;
   }
-
-  /**
-   * Returns the smallest distance between two spans.
-   */
-  public static int distance(Span a, Span b) {
-    if (Span.overlaps(a, b)) {
-      return 0;
-    } else if (a.contains(b) || b.contains(a)) {
-      return 0;
-    } else if (a.isBefore(b)) {
-      return b.start - a.end;
-    } else if (b.isBefore(a)) {
-      return a.start - b.end;
-    } else {
-      throw new IllegalStateException("This should be impossible...");
-    }
-  }
 }
