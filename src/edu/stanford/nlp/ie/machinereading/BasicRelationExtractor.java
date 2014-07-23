878001ae9ba3cca68fbfd51dd8129e42ec3c9a5c
Spence Green <spence@spencegreen.com>
2014-07-22 23:53:00 -0700

    Merge branch 'master' of origin

--- src/edu/stanford/nlp/ie/machinereading/BasicRelationExtractor.java
+++ src/edu/stanford/nlp/ie/machinereading/BasicRelationExtractor.java
@@ -65,6 +65,7 @@ public class BasicRelationExtractor implements Extractor {
     featureFactory = featureFac;
     this.createUnrelatedRelations = createUnrelatedRelations;
     this.relationMentionFactory = factory;
+
     logger.setLevel(Level.INFO);
   }
 
@@ -136,7 +137,7 @@ public class BasicRelationExtractor implements Extractor {
       StringBuilder bos = new StringBuilder();
       bos.append("WEIGHTS FOR LABEL ").append(label).append(':');
       for (Pair<String, Double> feat: sorted) {
-        bos.append(' ').append(feat.first()).append(':').append(feat.second());
+        bos.append(' ').append(feat.first()).append(':').append(feat.second()+"\n");
       }
       logger.fine(bos.toString());
     }
