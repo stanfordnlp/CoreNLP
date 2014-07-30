0a2d5a27364854fd28983c89a6367cbba444ae42
John Bauer <horatio@gmail.com>
2014-07-30 15:03:21 -0700

    In general it seems objects make better deps for the xsubj than an nsubj does, so prefer the dobj if it is present

--- src/edu/stanford/nlp/trees/EnglishGrammaticalStructure.java
+++ src/edu/stanford/nlp/trees/EnglishGrammaticalStructure.java
@@ -861,6 +861,7 @@ public class EnglishGrammaticalStructure extends GrammaticalStructure {
       boolean hasSubjectDaughter = false;
       boolean hasAux = false;
       List<TreeGraphNode> subjects = new ArrayList<TreeGraphNode>();
+      List<TreeGraphNode> objects = new ArrayList<TreeGraphNode>();
       for (TypedDependency dep : list) {
         // already have a subject dependency
         if ((dep.reln() == NOMINAL_SUBJECT || dep.reln() == NOMINAL_PASSIVE_SUBJECT) && dep.gov() == modifier) {
@@ -876,6 +877,10 @@ public class EnglishGrammaticalStructure extends GrammaticalStructure {
         if ((dep.reln() == NOMINAL_SUBJECT || dep.reln() == NOMINAL_PASSIVE_SUBJECT) && dep.gov() == head) {
           subjects.add(dep.dep());
         }
+
+        if (dep.reln() == DIRECT_OBJECT && dep.gov() == head) {
+          objects.add(dep.dep());
+        }
       }
 
       // if we already have an nsubj dependency, no need to add an xsubj
@@ -888,9 +893,22 @@ public class EnglishGrammaticalStructure extends GrammaticalStructure {
         continue;
       }
 
-      for (TreeGraphNode subject : subjects) {
-        TypedDependency newDep = new TypedDependency(CONTROLLING_SUBJECT, modifier, subject);
-        newDeps.add(newDep);
+      // In general, we find that the objects of the verb are better
+      // for xsubj than the original nsubj of the verb.  For example,
+      // "Many investors wrote asking the SEC to require ..."
+      // There is no nsubj of asking, but the dobj, SEC, is the xsubj of require.
+      // Similarly, "The law tells them when to do so"
+      // Instead of xsubj(do, law) we want xsubj(do, them)
+      if (objects.size() > 0) {
+        for (TreeGraphNode object : objects) {
+          TypedDependency newDep = new TypedDependency(CONTROLLING_SUBJECT, modifier, object);
+          newDeps.add(newDep);
+        }
+      } else {
+        for (TreeGraphNode subject : subjects) {
+          TypedDependency newDep = new TypedDependency(CONTROLLING_SUBJECT, modifier, subject);
+          newDeps.add(newDep);
+        }
       }
     }
 
