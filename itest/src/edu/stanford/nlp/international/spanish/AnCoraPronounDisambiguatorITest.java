b190e8ed1cb82d7da068ec069cfee80244fa7665
John Bauer <horatio@gmail.com>
2014-08-27 12:37:37 -0700

    Move a test that loads models to the itest directory

--- itest/src/edu/stanford/nlp/international/spanish/AnCoraPronounDisambiguatorITest.java
+++ itest/src/edu/stanford/nlp/international/spanish/AnCoraPronounDisambiguatorITest.java
@@ -3,15 +3,14 @@ package edu.stanford.nlp.international.spanish;
 import edu.stanford.nlp.international.spanish.process.AnCoraPronounDisambiguator;
 import junit.framework.TestCase;
 
-public class AnCoraPronounDisambiguatorTest extends TestCase {
+public class AnCoraPronounDisambiguatorITest extends TestCase {
 
   private final SpanishVerbStripper verbStripper = SpanishVerbStripper.getInstance();
 
   private void runTest(AnCoraPronounDisambiguator.PersonalPronounType expected, String verb,
                        int i) {
     assertEquals(expected,
-      AnCoraPronounDisambiguator.disambiguatePersonalPronoun(verbStripper
-                                                               .separatePronouns(verb), i, ""));
+                 AnCoraPronounDisambiguator.disambiguatePersonalPronoun(verbStripper.separatePronouns(verb), i, ""));
   }
 
   public void testDisambiguation() {
