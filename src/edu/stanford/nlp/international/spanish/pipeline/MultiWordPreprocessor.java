bd6e4c18671aea3e75f9b68fb14ce0e0e0ba5329
Ishita Prasad <ishitap@john2.stanford.edu>
2014-08-27 12:03:32 -0700

    ConfusionMatrix for tsv

--- src/edu/stanford/nlp/international/spanish/pipeline/MultiWordPreprocessor.java
+++ src/edu/stanford/nlp/international/spanish/pipeline/MultiWordPreprocessor.java
@@ -58,8 +58,6 @@ public final class MultiWordPreprocessor {
   private static Map<String, String> phrasalCategoryMap = new HashMap<String, String>() {{
       put("ao0000", "grup.a");
       put("aq0000", "grup.a");
-      put("dn0000", "spec");
-      put("dt0000", "spec");
       put("rg", "grup.adv");
       put("rn", "grup.adv"); // no sólo
       put("vmg0000", "grup.verb");
@@ -276,6 +274,9 @@ public final class MultiWordPreprocessor {
       traverseAndFix(kid, t, pretermLabel, unigramTagger, retainNER);
 
     // Post-order visit
+    //
+    // TODO merge unnecessarily deep trees (maybe the job for a separate
+    // tree normalizer?)
     if(t.value().startsWith(SpanishTreeNormalizer.MW_PHRASE_TAG)) {
       nMissingPhrasal++;
 
