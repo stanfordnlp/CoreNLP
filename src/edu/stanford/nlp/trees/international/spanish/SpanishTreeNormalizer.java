c2fc552399eb569863da7b2c8dca86b5a0e69b3f
Jon Gauthier <jgauthie@stanford.edu>
2014-08-27 12:02:56 -0700

    Add Spanish tree normalizer routine which splits multi-word tokens into their constituent words

--- src/edu/stanford/nlp/trees/international/spanish/SpanishTreeNormalizer.java
+++ src/edu/stanford/nlp/trees/international/spanish/SpanishTreeNormalizer.java
@@ -1,6 +1,10 @@
 package edu.stanford.nlp.trees.international.spanish;
 
+import java.util.Arrays;
+
 import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.HasTag;
+import edu.stanford.nlp.ling.HasWord;
 import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.trees.TreeFactory;
 import edu.stanford.nlp.trees.TreeNormalizer;
@@ -9,6 +13,12 @@ import edu.stanford.nlp.trees.TreeNormalizer;
  * Normalize trees read from the AnCora Spanish corpus.
  */
 public class SpanishTreeNormalizer extends TreeNormalizer {
+
+  /**
+   * Tag provided to words which are extracted from a multi-word token
+   * into their own independent nodes
+   */
+  private static final String MW_TAG = "MW?";
 
   @Override
   public Tree normalizeWholeTree(Tree tree, TreeFactory tf) {
@@ -22,6 +32,8 @@ public class SpanishTreeNormalizer extends TreeNormalizer {
         pos = simplifyPOSTag(pos).intern();
         label.setValue(pos);
         label.setTag(pos);
+      } else if (t.isPrePreTerminal()) {
+        normalizeForMultiWord(t, tf);
       }
     }
 
@@ -79,6 +91,59 @@ public class SpanishTreeNormalizer extends TreeNormalizer {
       // conjunction
       //   retain all
       return pos;
+    }
+  }
+
+  /**
+   * Normalize a pre-pre-terminal tree node by accounting for multi-word
+   * tokens.
+   *
+   * Detects multi-word tokens in leaves below this pre-pre-terminal and
+   * expands their constituent words into separate leaves.
+   */
+  void normalizeForMultiWord(Tree t, TreeFactory tf) {
+    Tree[] preterminals = t.children();
+
+    // Loop accumulator: number of tokens which have been inserted to
+    // the left of the current preterminal thus far
+    int shift = 0;
+
+    for (int i = 0; i < preterminals.length; i++) {
+      Tree leaf = preterminals[i].firstChild();
+      String leafValue = ((CoreLabel) leaf.label()).value();
+
+      if (leafValue.indexOf('_') == -1)
+        continue;
+
+      // Leaf is a multi-word token; build new pre-terminal nodes for
+      // each of its constituent words
+      String[] words = leafValue.split("_");
+      Tree[] newPreterminals = new Tree[words.length];
+      for (int j = 0; j < words.length; j++) {
+        String word = words[j];
+
+        Tree newLeaf = tf.newLeaf(word);
+        if (newLeaf.label() instanceof HasWord)
+          ((HasWord) newLeaf.label()).setWord(word);
+
+        Tree newPreterminal = tf.newTreeNode(MW_TAG, Arrays.asList(newLeaf));
+        if (newPreterminal.label() instanceof HasTag)
+          ((HasTag) newPreterminal.label()).setTag(MW_TAG);
+
+        newPreterminals[j] = newPreterminal;
+      }
+
+      // Now insert each of the new preterminals and remove the old
+      // multi-word token
+      //
+      // Current preterminal is at index `i + shift` of the preterminals
+      t.removeChild(i + shift);
+      for (int k = 0; k < newPreterminals.length; k++)
+        t.addChild(i + shift + k, newPreterminals[k]);
+
+      // Later preterminals must take into account the (possibly) larger
+      // amount of preceding siblings
+      shift += words.length - 1;
     }
   }
 
---
+++ test/src/edu/stanford/nlp/trees/international/spanish/SpanishTreeNormalizerTest.java
@@ -0,0 +1,61 @@
+package edu.stanford.nlp.trees.international.spanish;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import junit.framework.TestCase;
+
+import edu.stanford.nlp.trees.LabeledScoredTreeFactory;
+import edu.stanford.nlp.trees.PennTreeReader;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.TreeFactory;
+import edu.stanford.nlp.util.Pair;
+
+/**
+ * @author Jon Gauthier
+ */
+public class SpanishTreeNormalizerTest extends TestCase {
+
+  private TreeFactory tf;
+  private SpanishTreeNormalizer tn;
+
+  public void setUp() {
+    tf = new LabeledScoredTreeFactory();
+    tn = new SpanishTreeNormalizer();
+  }
+
+  @SuppressWarnings("unchecked")
+  Pair<String, String>[] multiWordTestCases = new Pair[] {
+    new Pair("(grup.nom (np00000 Josep_Maria_Ollé))",
+             "(grup.nom (MW? Josep) (MW? Maria) (MW? Ollé))"),
+
+    new Pair("(grup.nom (grup.nom (nc0p000 productos)) (sp (prep (sp000 de)) (sn (grup.nom (np00000 American_Online)))))",
+             "(grup.nom (grup.nom (nc0p000 productos)) (sp (prep (sp000 de)) (sn (grup.nom (MW? American) (MW? Online)))))"),
+
+    // Two multi-word tokens as siblings
+    new Pair("(a (b c_d) (b e_f))",
+             "(a (MW? c) (MW? d) (MW? e) (MW? f))"),
+  };
+
+  public void testMultiWordNormalization() {
+    for (Pair<String, String> testCase : multiWordTestCases) {
+      Tree head = readTree(testCase.first());
+      for (Tree t : head) {
+        if (t.isPrePreTerminal())
+          tn.normalizeForMultiWord(t, tf);
+      }
+
+      assertEquals(testCase.second(), head.toString());
+    }
+  }
+
+  /**
+   * Read a tree from a PTB-style serialized form in the given string.
+   */
+  private Tree readTree(String treeRep) {
+    try {
+      return new PennTreeReader(new StringReader(treeRep), tf).readTree();
+    } catch (IOException e) { return null; }
+  }
+
+}
