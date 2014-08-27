a3c3777365281fc0ffa19a7e3179377dc444ea65
John Bauer <horatio@gmail.com>
2014-08-27 12:07:15 -0700

    Amp up the Language enum a bit, check for errors, add some test cases

--- src/edu/stanford/nlp/pipeline/PTBTokenizerAnnotator.java
+++ src/edu/stanford/nlp/pipeline/PTBTokenizerAnnotator.java
@@ -1,6 +1,8 @@
 package edu.stanford.nlp.pipeline;
 
 import java.io.Reader;
+import java.util.Collections;
+import java.util.Map;
 import java.util.Properties;
 
 import edu.stanford.nlp.ling.CoreLabel;
@@ -10,6 +12,7 @@ import edu.stanford.nlp.process.PTBTokenizer;
 import edu.stanford.nlp.process.Tokenizer;
 import edu.stanford.nlp.process.WhitespaceTokenizer;
 
+import edu.stanford.nlp.util.Generics;
 import edu.stanford.nlp.util.PropertiesUtils;
 import edu.stanford.nlp.international.spanish.process.SpanishTokenizer;
 import edu.stanford.nlp.international.french.process.FrenchTokenizer;
@@ -28,74 +31,75 @@ public class PTBTokenizerAnnotator extends TokenizerAnnotator {
 
   private final TokenizerFactory<CoreLabel> factory;
 
-	public static enum Language {Spanish, French, English, Whitespace}
+  public static enum Language {
+    Spanish    ("es"), 
+    French     ("fr"), 
+    English    ("en"), 
+    Whitespace ("wh");
 
-	public static final String SPANISH = "spanish";
-	public static final String ES = "es";
-	public static final String FRENCH = "french";
-	public static final String FR = "fr";
-	public static final String ENGLISH = "english";
-	public static final String EN = "en";
+    private final String abbreviation;
+    public String abbreviation() { return abbreviation; }
+
+    private static final Map<String, Language> nameToLanguageMap = initializeFromMap();
+
+    private Language(String abbreviation) {
+      this.abbreviation = abbreviation;
+    }
+
+    private static Map<String, Language> initializeFromMap() {
+      Map<String, Language> map = Generics.newHashMap();
+      for (Language language : Language.values()) {
+        map.put(language.abbreviation.toUpperCase(), language);
+        map.put(language.toString().toUpperCase(), language);
+      }
+      return Collections.unmodifiableMap(map);
+    }
+
+    /** Returns null for an unknown language */
+    public static Language fromName(String value) {
+      return nameToLanguageMap.get(value.toUpperCase());
+    }
+  }
 
   public static final String DEFAULT_OPTIONS_EN = "invertible,ptb3Escaping=true";
-	public static final String DEFAULT_OPTIONS_ES = "invertible,ptb3Escaping=true,splitAll=true" ;
-	public static final String DEFAULT_OPTIONS_FR = "";
+  public static final String DEFAULT_OPTIONS_ES = "invertible,ptb3Escaping=true,splitAll=true" ;
+  public static final String DEFAULT_OPTIONS_FR = "";
 
-	public static final String EOL_PROPERTY = "tokenize.keepeol";
-    
-	public PTBTokenizerAnnotator() {
-		this(true);
+  public static final String EOL_PROPERTY = "tokenize.keepeol";
+  
+  public PTBTokenizerAnnotator() {
+    this(true);
   }
     
-	public PTBTokenizerAnnotator(boolean verbose) {
-		this(verbose, new Properties());
+  public PTBTokenizerAnnotator(boolean verbose) {
+    this(verbose, new Properties());
   }
 
-	public PTBTokenizerAnnotator(boolean verbose, String options) {
-		super(verbose);
+  public PTBTokenizerAnnotator(boolean verbose, String options) {
+    super(verbose);
+    
+    Properties props = new Properties();
+    if (options != null)
+      props.setProperty("tokenize.options", options);
 
-		Properties props = new Properties();
-		if (options != null)
-			props.setProperty("tokenize.options", options);
+    factory = initFactory(Language.English, props);
+  }
 
-		factory = initFactory(Language.English, props);
-	}
+  public PTBTokenizerAnnotator(boolean verbose, Properties props) {
+    super(verbose);
 
-	public PTBTokenizerAnnotator(boolean verbose, Properties props) {
-		super(verbose);
+    if (props == null)
+      props = new Properties();
 
-		if (props == null)
-			props = new Properties();
+    Language type = getLangType(props);
+    factory = initFactory(type, props);
+  }
 
-		Language type = getLangType(props);
-		factory = initFactory(type, props);
-
-		/*		switch(type) {
-		case Spanish:			
-			options = (options == null) ? DEFAULT_OPTIONS_ES : options;
-			factory = SpanishTokenizer.factory(new CoreLabelTokenFactory(), options);
-			break;
-		case French:
-			options = (options == null) ? DEFAULT_OPTIONS_ES : options;
-			factory = FrenchTokenizer.factory(new CoreLabelTokenFactory(), options);
-			break;
-		case Whitespace:
-			boolean eolIsSignificant = Boolean.valueOf(props.getProperty(EOL_PROPERTY, "false"));
-			eolIsSignificant = eolIsSignificant || Boolean.valueOf(props.getProperty(StanfordCoreNLP.NEWLINE_SPLITTER_PROPERTY, "false"));
-			factory = new WhitespaceTokenizer.WhitespaceTokenizerFactory<CoreLabel> (new CoreLabelTokenFactory(), eolIsSignificant);
-			break;
-		default:
-      options = (options == null) ? DEFAULT_OPTIONS_EN : options;
-      factory = PTBTokenizer.factory(new CoreLabelTokenFactory(), options);
-			}*/
-	}
-
-	private TokenizerFactory<CoreLabel> initFactory(Language type, Properties props) {
-		String options = props.getProperty("tokenize.options", null);
-		TokenizerFactory<CoreLabel> factory;
-
-		switch(type) {
-
+  private TokenizerFactory<CoreLabel> initFactory(Language type, Properties props) {
+    String options = props.getProperty("tokenize.options", null);
+    TokenizerFactory<CoreLabel> factory;
+    
+    switch(type) {
     case Spanish:
       options = (options == null) ? DEFAULT_OPTIONS_ES : options;
       factory = SpanishTokenizer.factory(new CoreLabelTokenFactory(), options);
@@ -112,38 +116,43 @@ public class PTBTokenizerAnnotator extends TokenizerAnnotator {
       factory = new WhitespaceTokenizer.WhitespaceTokenizerFactory<CoreLabel> (new CoreLabelTokenFactory(), eolIsSignificant);
       break;
 
-    default:
+    case English:
       options = (options == null) ? DEFAULT_OPTIONS_EN : options;
       factory = PTBTokenizer.factory(new CoreLabelTokenFactory(), options);
+      break;
+
+    default:
+      throw new IllegalArgumentException("Unknown language " + type);
     }
-		return factory;
-	}
+    return factory;
+  }
 
-	/**
-	 * Returns the language or tokenizer type that the annotator should
-	 * use. Language/Tokenizer type is specified through tokenize.class
-	 * or tokenize.language.
-	 */
-	private Language getLangType(Properties props) {
-	  String tokClass = props.getProperty("tokenize.class", null);
-		if (tokClass != null) {
-			if (tokClass.equals("SpanishTokenizer")) 
-				return Language.Spanish;
-			else if (tokClass.equals("FrenchTokenizer"))
-				return Language.French;
-			else return Language.English;
-		}
+  /**
+   * Returns the language or tokenizer type that the annotator should
+   * use. Language/Tokenizer type is specified through tokenize.class
+   * or tokenize.language.
+   */
+  private Language getLangType(Properties props) {
+    String tokClass = props.getProperty("tokenize.class", null);
+    if (tokClass != null) {
+      if (tokClass.equals("SpanishTokenizer")) 
+        return Language.Spanish;
+      else if (tokClass.equals("FrenchTokenizer"))
+        return Language.French;
+      else
+        return Language.English;
+    }
 
-		if(Boolean.valueOf(props.getProperty("tokenize.whitespace", "false")))
-			return Language.Whitespace;
+    if (Boolean.valueOf(props.getProperty("tokenize.whitespace", "false")))
+      return Language.Whitespace;
 
-		String language = props.getProperty("tokenize.language", "english");
-		if (language.equalsIgnoreCase(SPANISH) || language.equalsIgnoreCase(ES))
-			return Language.Spanish;
-		if (language.equalsIgnoreCase(FRENCH) || language.equalsIgnoreCase(FR))
-			return Language.French;
-		else return Language.English;
-	}
+    String languageName = props.getProperty("tokenize.language", "english");
+    Language language = Language.fromName(languageName);
+    if (language == null) {
+      throw new IllegalArgumentException("Tokenizer does not support language " + languageName);
+    }
+    return language;
+  }
 	
   @Override
   public Tokenizer<CoreLabel> getTokenizer(Reader r) {
