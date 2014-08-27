ed4885d61047dad73df2af517124a6453af70f02
Jon Gauthier <jgauthie@stanford.edu>
2014-08-27 12:02:45 -0700

    Move Spanish POS code into proper directories -- resolve patch mistake

--- src/edu/stanford/nlp/tagger/maxent/TestSentence.java
+++ src/edu/stanford/nlp/tagger/maxent/TestSentence.java
@@ -21,6 +21,7 @@ import edu.stanford.nlp.sequences.ExactBestSequenceFinder;
 import edu.stanford.nlp.sequences.SequenceModel;
 import edu.stanford.nlp.tagger.common.Tagger;
 import edu.stanford.nlp.util.ArrayUtils;
+import edu.stanford.nlp.util.ConfusionMatrix;
 import edu.stanford.nlp.util.Generics;
 import edu.stanford.nlp.util.Pair;
 
@@ -287,6 +288,18 @@ public class TestSentence implements SequenceModel {
       }
       pw.println(sw);
     }
+  }
+
+  /**
+   * Update a confusion matrix with the errors from this sentence.
+   *
+   * @param finalTags Chosen tags for sentence
+   * @param confusionMatrix Confusion matrix to write to
+   */
+  protected void updateConfusionMatrix(String[] finalTags,
+                                       ConfusionMatrix<String> confusionMatrix) {
+    for (int i = 0; i < correctTags.length; i++)
+      confusionMatrix.add(finalTags[i], correctTags[i]);
   }
 
 
@@ -571,7 +584,8 @@ public class TestSentence implements SequenceModel {
     int numTags = maxentTagger.numTags();
     double[][][] probabilities = new double[size][kBestSize][numTags];
     calculateProbs(probabilities);
-    for (int current = 0; current < size; current++) {
+
+    for (int current = 0; current < correctTags.length; current++) {
       pfu.print(sent.get(current));
       double[] probs = new double[3];
       String[] tag3 = new String[3];
