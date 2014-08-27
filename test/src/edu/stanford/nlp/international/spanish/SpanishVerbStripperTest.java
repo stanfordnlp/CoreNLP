563d17b676e342d7cd50c409e3f87d77ac00e845
Jon Gauthier <jgauthie@stanford.edu>
2014-08-27 12:04:26 -0700

    Spanish verb stripper: handle icky cases related to second-person plural imperatives

--- test/src/edu/stanford/nlp/international/spanish/SpanishVerbStripperTest.java
+++ test/src/edu/stanford/nlp/international/spanish/SpanishVerbStripperTest.java
@@ -55,6 +55,13 @@ public class SpanishVerbStripperTest extends TestCase {
     assertEquals(new Pair("aplicar", pronouns),
       SpanishVerbStripper.separatePronouns("aplicárseles"));
 
+    // Don't treat plural past participles as 2nd-person commands!
+    assertNull(SpanishVerbStripper.separatePronouns("sentados"));
+    pronouns.clear();
+    pronouns.add("os");
+    assertEquals(new Pair("sentad", pronouns),
+      SpanishVerbStripper.separatePronouns("sentaos"));
+
     // Looks like a verb with a clitic pronoun.. but it's not! There are
     // a *lot* of these in Spanish.
     assertNull(SpanishVerbStripper.separatePronouns("címbalo"));
@@ -70,22 +77,18 @@ public class SpanishVerbStripperTest extends TestCase {
     assertEquals("leer", SpanishVerbStripper.stripVerb("leerlo"));
     assertEquals("jugar", SpanishVerbStripper.stripVerb("jugarla"));
     assertEquals("jugar", SpanishVerbStripper.stripVerb("jugárselos"));
+    assertEquals("jugar", SpanishVerbStripper.stripVerb("jugaros"));
     assertEquals("decir", SpanishVerbStripper.stripVerb("decírmelo"));
     assertEquals("contando", SpanishVerbStripper.stripVerb("contándolo"));
     assertEquals("yendo", SpanishVerbStripper.stripVerb("yéndole"));
     assertEquals("viviendo", SpanishVerbStripper.stripVerb("viviéndolo"));
     assertEquals("leyendo", SpanishVerbStripper.stripVerb("leyéndolo"));
     assertEquals("buscando", SpanishVerbStripper.stripVerb("buscándome"));
-    assertEquals("senta", SpanishVerbStripper.stripVerb("sentaos"));
-    assertEquals("vesti", SpanishVerbStripper.stripVerb("vestíos"));
+    assertEquals("sentad", SpanishVerbStripper.stripVerb("sentaos"));
+    assertEquals("vestid", SpanishVerbStripper.stripVerb("vestíos"));
     assertEquals("compre", SpanishVerbStripper.stripVerb("cómprelos"));
     assertEquals("haz", SpanishVerbStripper.stripVerb("házmelo"));
     assertEquals("oír", SpanishVerbStripper.stripVerb("oírse"));
-  }
-
-  public static void main(String[] args) {
-    testStrippable();
-    testStripVerb();
   }
 
 }
