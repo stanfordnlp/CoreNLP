67085edc7092c30f57a14a445776f8307b54510f
Christopher Manning <manning@stanford.edu>
2014-10-03 18:05:27 -0700

    Use @SafeVarargs and add a little more documentation.

--- src/edu/stanford/nlp/neural/NeuralUtils.java
+++ src/edu/stanford/nlp/neural/NeuralUtils.java
@@ -96,6 +96,7 @@ public class NeuralUtils {
    * the matrices with the entries in the theta vector.  Errors are
    * thrown if the theta vector does not exactly fill the matrices.
    */
+  @SafeVarargs
   public static void vectorToParams(double[] theta, Iterator<SimpleMatrix> ... matrices) {
     int index = 0;
     for (Iterator<SimpleMatrix> matrixIterator : matrices) {
@@ -119,6 +120,7 @@ public class NeuralUtils {
    * total size as a time savings.  AssertionError thrown if the
    * vector sizes do not exactly match.
    */
+  @SafeVarargs
   public static double[] paramsToVector(int totalSize, Iterator<SimpleMatrix> ... matrices) {
     double[] theta = new double[totalSize];
     int index = 0;
@@ -146,6 +148,7 @@ public class NeuralUtils {
    * expected total size as a time savings.  AssertionError thrown if
    * the vector sizes do not exactly match.
    */
+  @SafeVarargs
   public static double[] paramsToVector(double scale, int totalSize, Iterator<SimpleMatrix> ... matrices) {
     double[] theta = new double[totalSize];
     int index = 0;
