9922823867ca6e8dadf5cd564b708b830421df1a
Jon Gauthier <jgauthie@stanford.edu>
2014-10-26 12:28:51 -0700

    More of Danqi's code

--- src/edu/stanford/nlp/depparser/util/CONST.java
+++ src/edu/stanford/nlp/depparser/util/CONST.java
@@ -6,33 +6,12 @@
 * 	@Last Modified:  2014-09-30
 */
 
-package edu.stanford.nlp.depparser.util;
 
-public class CONST 
+public class CONST
 {
-	//TODO: write everything in a prop file, and call -prop
-	//TODO: change all variables to CAPS
-
-	public static final String UNKNOWN = "-UNKNOWN-";
-	public static final String ROOT = "-ROOT-";
-	public static final String NULL = "-NULL-";
-	public static final int NONEXIST = -1;
-	public static final String SEPARATOR = "###################";
-
-	public static final int wordCutOff = 1;
-	public static final double initRange = 0.01;
-	public static final int maxIter = 100000;
-	public static final int batchSize = 1000;
-
-	public static final double adaEps = 1e-6;
-	public static final double adaAlpha = 0.01;
-	public static final double regParameter = 1e-8;
-	public static final double dropProb = 0.5;
-
-	public static int hiddenSize = 200;
-	public static int embeddingSize = 50;
-	public static int numTokens = 48;
-
-	public static int numPreComputed = 100000;
-	public static int evalPerIter = 100;
+    public static final String UNKNOWN = "-UNKNOWN-";
+    public static final String ROOT = "-ROOT-";
+    public static final String NULL = "-NULL-";
+    public static final int NONEXIST = -1;
+    public static final String SEPARATOR = "###################";
 }
---
+++ src/edu/stanford/nlp/depparser/util/Counter.java
@@ -0,0 +1,46 @@
+
+import java.util.*;
+
+public class Counter<T> extends HashMap<T, Integer>
+{
+    public int getCount(T key)
+    {
+        return containsKey(key) ? get(key) : 0;
+    }
+
+    public void add(T key)
+    {
+        put(key, getCount(key) + 1);
+    }
+
+    public List<T> getSortedKeys()
+    {
+        List<T> ls = new ArrayList<T>(keySet());
+        Collections.sort( ls , new Comparator<T>() {
+            public int compare( T s1, T s2 )
+            {
+                return get(s2).compareTo(get(s1));
+            }
+        });
+        return ls;
+    }
+
+    public List<T> getSortedKeys(int K)
+    {
+        List<T> ls = getSortedKeys();
+        List<T> kls = new ArrayList<T>();
+        for (int i = 0; i < ls.size() && i < K; ++ i)
+            kls.add(ls.get(i));
+        return kls;
+    }
+
+    public void printTopKeys(int K)
+    {
+        List<T> ls = getSortedKeys(K);
+        for (int i = 0; i < ls.size(); ++ i)
+        {
+            T key = ls.get(i);
+            System.out.println(key.toString() + " " + getCount(key));
+        }
+    }
+}
