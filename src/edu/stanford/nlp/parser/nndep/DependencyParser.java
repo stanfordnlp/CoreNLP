c0d55ea6c6d29478b55deec439643e7b984c0739
Jon Gauthier <jgauthie@stanford.edu>
2014-10-26 12:30:08 -0700

    Better error handling for file opening

--- src/edu/stanford/nlp/parser/nndep/DependencyParser.java
+++ src/edu/stanford/nlp/parser/nndep/DependencyParser.java
@@ -995,17 +995,17 @@ public class DependencyParser {
                 ? new BufferedReader(new InputStreamReader(System.in, encoding))
                 : IOUtils.readerFromString(inputFilename, encoding);
       } catch (IOException e) {
-        throw new RuntimeIOException(e);
+        throw new RuntimeIOException("No input file provided (use -parseFile)", e);
       }
 
       String outputFilename = props.getProperty("outFile");
       PrintWriter output;
       try {
-        output = outputFilename.equals("-")
+        output = outputFilename == null || outputFilename.equals("-")
             ? IOUtils.encodedOutputStreamPrintWriter(System.out, encoding, true)
             : IOUtils.getPrintWriter(outputFilename, encoding);
       } catch (IOException e) {
-        throw new RuntimeIOException(e);
+        throw new RuntimeIOException("Error opening output file", e);
       }
 
       parser.parseTextFile(input, output);
