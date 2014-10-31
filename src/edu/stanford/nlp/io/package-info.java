671b0bd652f16f9603355b3abcef9bc2dc642ec0
Jon Gauthier <jgauthie@stanford.edu>
2014-10-31 12:34:08 -0700

    Merge branch 'master' of /u/nlp/git/javanlp

---
+++ src/edu/stanford/nlp/io/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * <p>
+ * A few classes that augment the <code>java.io</code> package in useful
+ * ways for our applications.
+ * </p>
+ * <ul>
+ * <li>FileFilters: <code>ExtensionFileFilter</code> and
+ * <code>NumberRangeFileFilter</code>
+ * <li>Buffered data streams: <code>InDataStreamFile</code>,
+ * <code>OutDataStreamFile</code>, <code>PrintFile</code>
+ * <li>Making collections of files in recursive descent of directory, with
+ * 	filtering: <code>FileArrayList</code>,
+ * 	<code>FileSequentialCollection</code>
+ * </ul>
+ */
+package edu.stanford.nlp.io;
---
+++ src/edu/stanford/nlp/stats/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * A set of tools for collecting, representing, and inferring with statistics. The two most useful bodies of code are for
+ * (1) representing distributions (e.g., Distribution and Counter) and (2) computing the performance of a classifier
+ * (e.g., PrecisionRecallStates and PRStatsManager).
+ * @author Teg Grenager
+ */
+package edu.stanford.nlp.stats;
