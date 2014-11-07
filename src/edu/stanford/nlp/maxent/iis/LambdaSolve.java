2f8e786c394ce41c71823bd74a4a678f85b9543a
Spence Green <spence@spencegreen.com>
2014-11-06 22:54:57 -0800

    Fix infinite recursion

--- src/edu/stanford/nlp/maxent/iis/LambdaSolve.java
+++ src/edu/stanford/nlp/maxent/iis/LambdaSolve.java
@@ -182,11 +182,16 @@ public class LambdaSolve {
       }
     }
 
+    if (VERBOSE) {
+      System.err.println(" pcond initialized ");
+    }
     // init zlambda
     for (int x = 0; x < p.data.xSize; x++) {
       zlambda[x] = probConds[x].length;
     }
-
+    if (VERBOSE) {
+      System.err.println(" zlambda initialized ");
+    }
     // init ftildeArr
     for (int i = 0; i < p.fSize; i++) {
       ftildeArr[i] = p.functions.get(i).ftilde();
@@ -226,7 +231,7 @@ public class LambdaSolve {
       }
     }//x
     if (VERBOSE) {
-      System.err.println(" pcond, zlamda, ftildeArr " + (fixedFnumXY ? "(fixed sum) " : "") + "initialized ");
+      System.err.println(" ftildeArr initialized " + (fixedFnumXY ? "fixed sum " : ""));
     }
   }
 
--- src/edu/stanford/nlp/trees/tregex/tsurgeon/package-info.java
+++ src/edu/stanford/nlp/trees/tregex/tsurgeon/package-info.java
@@ -13,100 +13,70 @@
  * <h3>Description of operations:</h3>
  * <pre>
  * delete name_1 name_2 ... name_m
- *
- *   For each name_i, deletes the node it names and everything below it.
- *
+ * For each name_i, deletes the node it names and everything below it.
  * prune name_1 name_2 ... name_m
- *
- *   For each name_i, prunes out the node it names.  Pruning differs from
- *   deletion in that if pruning a node causes its parent to have no
- *   children, then the parent is in turn pruned too.
- *
+ * For each name_i, prunes out the node it names.  Pruning differs from
+ * deletion in that if pruning a node causes its parent to have no
+ * children, then the parent is in turn pruned too.
  * excise name1 name2
- *
- *   The name1 node should either dominate or be the same as the name2
- *   node.  This excises out everything from name1 to name2.  All the
- *   children of name2 go into the parent of name1, where name1 was.
- *
+ * The name1 node should either dominate or be the same as the name2
+ * node.  This excises out everything from name1 to name2.  All the
+ * children of name2 go into the parent of name1, where name1 was.
  * relabel name new-label
- *
- *   Relabels the node to have the new label. There are three possible forms
- *   for the new-label:
- *
- *   relabel nodeX VP - for changing a node label to an alphanumeric
- *   string
- *
- *   relabel nodeX /''/ - for relabeling a node to something that
- *   isn't a valid identifier without quoting, and relabel nodeX
- *
- *   /^VB(.*)$/verb\/$1/ - for regular expression based relabeling. In the
- *   last case, all matches of the regular expression against the node
- *   label are replaced with the replacement String. This has the semantics
- *   of Java/Perl's replaceAll: you may use capturing groups and put them
- *   in replacements with $n. Also, as in the example, you can escape a
- *   slash in the middle of the second and third forms with \/ and \\.
- *   This last version lets you make a new label that is an arbitrary
- *   String function of the original label and additional characters that
- *   you supply.
- *
+ * Relabels the node to have the new label. There are three possible forms
+ * for the new-label:
+ * relabel nodeX VP - for changing a node label to an alphanumeric
+ * string, relabel nodeX /''/ - for relabeling a node to something that
+ * isn't a valid identifier without quoting, and relabel nodeX
+ * /^VB(.*)$/verb\/$1/ - for regular expression based relabeling. In the
+ * last case, all matches of the regular expression against the node
+ * label are replaced with the replacement String. This has the semantics
+ * of Java/Perl's replaceAll: you may use capturing groups and put them
+ * in replacements with $n. Also, as in the example, you can escape a
+ * slash in the middle of the second and third forms with \/ and \\.
+ * This last version lets you make a new label that is an arbitrary
+ * String function of the original label and additional characters that
+ * you supply.
  * relabel name new-label
- *
- *   Renames the node to have the new label.  If the new-label is not
- *   a valid tregex identifier, you can quote it by surrounding it by
- *   pipe characters (|new-label|).
- *
+ * Renames the node to have the new label.  If the new-label is not
+ * a valid tregex identifier, you can quote it by surrounding it by
+ * pipe characters (|new-label|).
  * relabel name regex groupNumber
- *
- *   matches the regex against the node's current label, and then renames
- *   the node to have a label that corresponds to the n-th group of the
- *   regex.
- *
+ * matches the regex against the node's current label, and then renames
+ * the node to have a label that corresponds to the n-th group of the
+ * regex.
  * insert name position
  * insert tree position
- *
- *   inserts the named node, or a manually specified tree (see below for
- *   syntax), into the position specified.  Right now the only ways to
- *   specify position are:
- *
- *   $+ name     to insert the left sister of the named node
- *   $- name     to insert  the right sister of the named node
- *   &gt;i name     the i_th daughter of the named node.
- *   &gt;-i name    the i_th daughter, counting from the right, of the named node.
- *
+ * inserts the named node, or a manually specified tree (see below for
+ * syntax), into the position specified.  Right now the only ways to
+ * specify position are:
+ * $+ name     to insert the left sister of the named node
+ * $- name     to insert  the right sister of the named node
+ * &gt;i name     the i_th daughter of the named node.
+ * &gt;-i name    the i_th daughter, counting from the right, of the named node.
  * move name position
- *
- *   moves the named node into the specified position.  To be precise, it
- *   deletes (*NOT* prunes) the node from the tree, and re-inserts it
- *   into the specified position.
- *
+ * moves the named node into the specified position.  To be precise, it
+ * deletes (*NOT* prunes) the node from the tree, and re-inserts it
+ * into the specified position.
  * replace name1 name2
- *
- *   deletes name1 and inserts a copy of name2 in its place.
- *
+ * deletes name1 and inserts a copy of name2 in its place.
  * adjoin tree target-node
- *
- *   adjoins the specified auxiliary tree (see below for syntax) into the
- *   target node specified.  The daughters of the target node will become
- *   the daughters of the foot of the auxiliary tree.
- *
+ * adjoins the specified auxiliary tree (see below for syntax) into the
+ * target node specified.  The daughters of the target node will become
+ * the daughters of the foot of the auxiliary tree.
  * adjoinH tree target-node
- *
- *   similar to adjoin, but preserves the target node and makes it the root
- *   of tree
- *
+ * similar to adjoin, but preserves the target node and makes it the root
+ * of tree
  * adjoinF tree target-node
- *
- *   similar to adjoin, but preserves the target node and makes it the foot
- *   of tree.  It thus retains its status as parent of its children, placed
- *   in the appropriate spot in tree.
- *
+ * similar to adjoin, but preserves the target node and makes it the foot
+ * of tree.  It thus retains its status as parent of its children, placed
+ * in the appropriate spot in tree.
  * coindex name_1 name_2 ... name_m
- *
- *   Puts a (Penn Treebank style) coindexation suffix of the form "-N" on
- *   each of nodes name_1 through name_m.  The value of N will be
- *   automatically generated in reference to the existing coindexations
- *   in the tree, so that there is never an accidental clash of
- *   indices across things that are not meant to be coindexed.
+ * Puts a (Penn Treebank style) coindexation suffix of the form "-N" on
+ * each of nodes name_1 through name_m.  The value of N will be
+ * automatically generated in reference to the existing coindexations
+ * in the tree, so that there is never an accidental clash of
+ * indices across things that are not meant to be coindexed.
  * </pre>
  * <h3>Comments: </h3>
  * <p>
@@ -160,7 +130,6 @@
  * || syntax.  Document adjoinH and adjoinF.  Provide a spliceIn(Above)
  * operation that lets you insert a node above a given node.
  * </p>
- *
  * @author Roger Levy
  * @version 21 July 2005.
  */
