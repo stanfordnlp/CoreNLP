69f1cb3ed066a222dc1fcadc6f29ccf5845bfa71
Gabor Angeli <angeli@stanford.edu>
2014-12-10 22:29:57 -0800

    Not quite sure how this was passing before; I hope my fix is ok

--- src/edu/stanford/nlp/ling/CoreLabel.java
+++ src/edu/stanford/nlp/ling/CoreLabel.java
@@ -148,10 +148,11 @@ public class CoreLabel extends ArrayCoreMap implements AbstractCoreLabel, HasCat
 
   @SuppressWarnings("unchecked")
   private void initFromStrings(String[] keys, String[] values) {
-    // values too long is okay in the case of reading NER data where
-    // we don't care about some fields, for example.  keys too long
-    // means some keys will be null.
-    for (int i = 0; i < Math.min(keys.length, values.length); i++) {
+    if (keys.length != values.length) {
+      throw new UnsupportedOperationException("Argument array lengths differ: " +
+              Arrays.toString(keys) + " vs. " + Arrays.toString(values));
+    }
+    for (int i = 0; i < keys.length; i++) {
       String key = keys[i];
       String value = values[i];
       KeyLookup lookup = AnnotationLookup.getCoreKey(key);
