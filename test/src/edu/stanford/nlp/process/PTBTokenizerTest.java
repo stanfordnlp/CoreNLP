7afccc2da2e9bc6ec141eb064102407a48b6f26c
Christopher Manning <manning@stanford.edu>
2015-01-09 11:52:46 -0800

    Add Yahoo and lowercase v. for legal cases.

--- test/src/edu/stanford/nlp/process/PTBTokenizerTest.java
+++ test/src/edu/stanford/nlp/process/PTBTokenizerTest.java
@@ -37,7 +37,8 @@ public class PTBTokenizerTest extends TestCase {
     "You can get a B.S. or a B. A. or a Ph.D (sometimes a Ph. D) from Stanford.",
     "@Harry_Styles didn`t like Mu`ammar al-Qaddafi",
     "Kenneth liked Windows 3.1, Windows 3.x, and Mesa A.B as I remember things.",
-    "I like programming in F# more than C#."
+    "I like programming in F# more than C#.",
+    "NBC Live will be available free through the Yahoo! Chat Web site",
   };
 
   private final String[][] ptbGold = {
@@ -69,6 +70,7 @@ public class PTBTokenizerTest extends TestCase {
     { "@Harry_Styles", "did", "n`t", "like", "Mu`ammar", "al-Qaddafi" },
     { "Kenneth", "liked", "Windows", "3.1", ",", "Windows", "3.x", ",", "and", "Mesa", "A.B", "as", "I", "remember", "things", ".", },
     { "I", "like", "programming", "in", "F#", "more", "than", "C#", "." },
+    { "NBC", "Live", "will", "be", "available", "free", "through", "the", "Yahoo!", "Chat", "Web", "site" },
   };
 
   public void testPTBTokenizerWord() {
