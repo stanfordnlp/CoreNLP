fa3048a2753fe27eb8e589d4a3dbe0e63ab1e719
sonalg <sonalg@stanford.edu>
2015-01-23 13:06:57 -0800

    debugging

--- src/edu/stanford/nlp/patterns/surface/ApplyPatterns.java
+++ src/edu/stanford/nlp/patterns/surface/ApplyPatterns.java
@@ -65,6 +65,8 @@ public class ApplyPatterns<E extends Pattern>  implements Callable<Pair<TwoDimen
           int s = m.start("$term");
           int e = m.end("$term");
 
+          assert e-s <= PatternFactory.numWordsCompound : "How come the pattern is extracting phrases longer than numWordsCompound";
+
           String phrase = "";
           String phraseLemma = "";
           boolean useWordNotLabeled = false;
