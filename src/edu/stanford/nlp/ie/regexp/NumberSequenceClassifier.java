81c675165f5ff7e243a22a427af6bfe98c300f6d
Christopher Manning <manning@stanford.edu>
2015-02-16 06:42:32 -0800

    Return test to working, but really should update PTBTokenizer and these files for 2015-style Unicode.

--- src/edu/stanford/nlp/ie/regexp/NumberSequenceClassifier.java
+++ src/edu/stanford/nlp/ie/regexp/NumberSequenceClassifier.java
@@ -584,7 +584,13 @@ public class NumberSequenceClassifier extends AbstractSequenceClassifier<CoreLab
   private static final Pattern AM_PM = Pattern.compile("(a\\.?m\\.?)|(p\\.?m\\.?)", Pattern.CASE_INSENSITIVE);
 
   public static final Pattern CURRENCY_WORD_PATTERN = Pattern.compile("(?:dollar|cent|euro|pound)s?|penny|pence|yen|yuan|won", Pattern.CASE_INSENSITIVE);
-  public static final Pattern CURRENCY_SYMBOL_PATTERN = Pattern.compile("\\$|&#163;|\u00A3|\u00A5|\u20AC|US\\$|HK\\$|A\\$", Pattern.CASE_INSENSITIVE);  // No longer include archaic # for pound as messes up anytime someone says #1
+
+  // pattern matches: dollar, pound sign XML escapes; pound sign, yen sign, euro, won; other country dollars; now omit # for pound
+  // TODO: Delete # as currency.  But doing this involves changing PTBTokenizer currency normalization rules
+  // Code \u0023 '#' was used for pound '£' in the ISO version of ASCII (ISO 646), and this is found in very old materials
+  // e.g., the 1999 Penn Treebank, but we now don't recognize this, as it now doesn't occur and wrongly recognizes
+  // currency whenever someone refers to the #4 country etc.
+  public static final Pattern CURRENCY_SYMBOL_PATTERN = Pattern.compile("\\$|#|&#163;|&pound;|\u00A3|\u00A5|\u20AC|\u20A9|(?:US|HK|A|C|NT|S|NZ)\\$", Pattern.CASE_INSENSITIVE);  // TODO: No longer include archaic # for pound
 
   public static final Pattern ORDINAL_PATTERN = Pattern.compile("(?i)[2-9]?1st|[2-9]?2nd|[2-9]?3rd|1[0-9]th|[2-9]?[04-9]th|100+th|zeroth|first|second|third|fourth|fifth|sixth|seventh|eighth|ninth|tenth|eleventh|twelfth|thirteenth|fourteenth|fifteenth|sixteenth|seventeenth|eighteenth|nineteenth|twentieth|twenty-first|twenty-second|twenty-third|twenty-fourth|twenty-fifth|twenty-sixth|twenty-seventh|twenty-eighth|twenty-ninth|thirtieth|thirty-first|fortieth|fiftieth|sixtieth|seventieth|eightieth|ninetieth|hundredth|thousandth|millionth");
 
