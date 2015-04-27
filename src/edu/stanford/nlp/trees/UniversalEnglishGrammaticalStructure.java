56d59f7251c3a4a5e8acfca11c572cb151610a19
Sebastian Schuster <sebschu@gmail.com>
2015-04-27 09:09:58 -0700

    fix bug in converter for weird nested and conjoined PP trees caused by parsing errors.

--- src/edu/stanford/nlp/trees/UniversalEnglishGrammaticalStructure.java
+++ src/edu/stanford/nlp/trees/UniversalEnglishGrammaticalStructure.java
@@ -550,8 +550,10 @@ public class UniversalEnglishGrammaticalStructure extends GrammaticalStructure {
     
     /* Attach CC node to conjGov */ 
     SemanticGraphEdge edge = sg.getEdge(gov, ccDep);
-    sg.removeEdge(edge);
-    sg.addEdge(conjGov, ccDep, COORDINATION, Double.NEGATIVE_INFINITY, false);
+    if (edge != null) {
+      sg.removeEdge(edge);
+      sg.addEdge(conjGov, ccDep, COORDINATION, Double.NEGATIVE_INFINITY, false);
+    }
     
     /* Add conjunction information for these relations already at this point.
      * It could be that we add several coordinating conjunctions while collapsing
