6af54e6e87ec2298df1e3e1426399dbafecd99e5
Gabor Angeli <angeli@stanford.edu>
2015-07-22 16:13:49 -0700

    Improve the acronym matcher a bit

--- src/edu/stanford/nlp/util/AcronymMatcher.java
+++ src/edu/stanford/nlp/util/AcronymMatcher.java
@@ -3,18 +3,179 @@ package edu.stanford.nlp.util;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 /**
  * A simple class with a variety of acronym matching utilities.
  *
+ * You're probably looking for the method {@link AcronymMatcher#isAcronym(String, List)}.
+ *
  * @author Gabor Angeli
  */
+@SuppressWarnings("Convert2Diamond")
 public class AcronymMatcher {
   private static final Pattern discardPattern = Pattern.compile("[-._]");
+
+  /** A set of words that should be considered stopwords for the acronym matcher */
+  private static final Set<String> STOPWORDS = Collections.unmodifiableSet(new HashSet<String>(){{
+    add("'d");
+    add("'ll");
+    add("'re");
+    add("'s");
+    add("'t");
+    add("'ve");
+    add("n't");
+    add("a");
+    add("about");
+    add("above");
+    add("after");
+    add("again");
+    add("against");
+    add("all");
+    add("am");
+    add("an");
+    add("and");
+    add("any");
+    add("are");
+    add("as");
+    add("at");
+    add("be");
+    add("because");
+    add("been");
+    add("before");
+    add("being");
+    add("below");
+    add("between");
+    add("both");
+    add("but");
+    add("by");
+    add("cannot");
+    add("could");
+    add("did");
+    add("do");
+    add("does");
+    add("doing");
+    add("down");
+    add("during");
+    add("each");
+    add("few");
+    add("for");
+    add("from");
+    add("further");
+    add("had");
+    add("has");
+    add("have");
+    add("having");
+    add("he");
+    add("her");
+    add("here");
+    add("hers");
+    add("herself");
+    add("him");
+    add("himself");
+    add("his");
+    add("how");
+    add("i");
+    add("if");
+    add("in");
+    add("into");
+    add("is");
+    add("it");
+    add("its");
+    add("itself");
+    add("me");
+    add("more");
+    add("most");
+    add("my");
+    add("myself");
+    add("no");
+    add("nor");
+    add("not");
+    add("of");
+    add("off");
+    add("on");
+    add("once");
+    add("only");
+    add("or");
+    add("other");
+    add("ought");
+    add("our");
+    add("ours");
+    add("ourselves");
+    add("out");
+    add("over");
+    add("own");
+    add("same");
+    add("she");
+    add("should");
+    add("so");
+    add("some");
+    add("such");
+    add("than");
+    add("their");
+    add("theirs");
+    add("them");
+    add("themselves");
+    add("the");
+    add("then");
+    add("there");
+    add("these");
+    add("they");
+    add("this");
+    add("those");
+    add("through");
+    add("to");
+    add("too");
+    add("under");
+    add("until");
+    add("up");
+    add("very");
+    add("was");
+    add("we");
+    add("were");
+    add("what");
+    add("when");
+    add("where");
+    add("which");
+    add("while");
+    add("who");
+    add("whom");
+    add("why");
+    add("with");
+    add("would");
+    add("you");
+    add("your");
+    add("yours");
+    add("yourself");
+    add("yourselves");
+
+
+    add("de");
+    add("del");
+    add("di");
+    add("y");
+
+    add("corporation");
+    add("corp");
+    add("corp.");
+    add("co");
+    add("llc");
+    add("inc");
+    add("inc.");
+    add("ltd");
+    add("ltd.");
+    add("llp");
+    add("llp.");
+    add("plc");
+    add("plc.");
+
+    add("&");
+    add(",");
+    add("-");
+  }});
+
 
   private static List<String> getTokenStrs(List<CoreLabel> tokens)
   {
@@ -67,7 +228,13 @@ public class AcronymMatcher {
   // Public static utility methods
   public static boolean isAcronymImpl(String str, List<String> tokens)
   {
+    // Remove some words from the candidate acronym
     str = discardPattern.matcher(str).replaceAll("");
+    // Remove stopwords if we need to
+    if (str.length() != tokens.size()) {
+      tokens = tokens.stream().filter(x -> !STOPWORDS.contains(x.toLowerCase())).collect(Collectors.toList());
+    }
+    // Run the matcher
     if (str.length() == tokens.size()) {
       for (int i = 0; i < str.length(); i++) {
         char ch = Character.toUpperCase(str.charAt(i));
@@ -132,42 +299,6 @@ public class AcronymMatcher {
       isAcro = isAcronymImpl(text1, tokenStrs2) || isAcronymImpl(text2, tokenStrs1);
     }
     return isAcro;
-  }
-
-
-  /** @see AcronymMatcher#isAcronym(edu.stanford.nlp.util.CoreMap, edu.stanford.nlp.util.CoreMap) */
-  public static boolean isPartialAcronym(String[] chunk1, String[] chunk2)
-  {
-    String text1 = StringUtils.join(chunk1);
-    String text2 = StringUtils.join(chunk2);
-    if (text1.length() <= 1 || text2.length() <= 1) { return false; }
-    List<String> tokenStrs1 = Arrays.asList(chunk1);
-    List<String> tokenStrs2 = Arrays.asList(chunk2);
-    boolean isAcro = isPartialAcronymImpl(text1, tokenStrs2) || isPartialAcronymImpl(text2, tokenStrs1);
-    if (!isAcro) {
-      tokenStrs1 = getMainTokenStrs(chunk1);
-      tokenStrs2 = getMainTokenStrs(chunk2);
-      isAcro = isAcronymImpl(text1, tokenStrs2) || isAcronymImpl(text2, tokenStrs1);
-    }
-    return isAcro;
-  }
-
-  // Public static utility methods
-  public static boolean isPartialAcronymImpl(String str, List<String> tokens)
-  {
-    str = discardPattern.matcher(str).replaceAll("");
-    if (str.length() == tokens.size()) {
-      for (int i = 0; i < str.length(); i++) {
-        char ch = Character.toUpperCase(str.charAt(i));
-        if ( !tokens.get(i).isEmpty() &&
-                Character.toUpperCase(tokens.get(i).charAt(0)) != ch ) {
-          return false;
-        }
-      }
-      return true;
-    } else {
-      return false;
-    }
   }
 
   public static boolean isFancyAcronym(String[] chunk1, String[] chunk2) {
---
+++ test/src/edu/stanford/nlp/util/AcronymMatcherTest.java
@@ -0,0 +1,32 @@
+package edu.stanford.nlp.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Test some acronyms. Taken mostly from the 2013 KBP results.
+ *
+ * @author Gabor Angeli
+ */
+public class AcronymMatcherTest {
+
+  @Test
+  public void testBasic() {
+    assertTrue(AcronymMatcher.isAcronym("IBM", "International Business Machines".split("\\s+")));
+    assertTrue(AcronymMatcher.isAcronym("SIWI", "Stockholm International Water Institute".split("\\s+")));
+    assertTrue(AcronymMatcher.isAcronym("CBRC", "China Banking Regulatory Commission".split("\\s+")));
+    assertTrue(AcronymMatcher.isAcronym("ECC", "Election Complaints Commission".split("\\s+")));
+  }
+
+  @Test
+  public void testFilterStopWords() {
+    assertTrue(AcronymMatcher.isAcronym("CML", "Council of Mortgage Lenders".split("\\s+")));
+    assertTrue(AcronymMatcher.isAcronym("AAAS", "American Association for the Advancement of Science".split("\\s+")));
+  }
+
+  @Test
+  public void testStripCorp() {
+    assertTrue(AcronymMatcher.isAcronym("FCI", "Fake Company International Corp.".split("\\s+")));
+  }
+}
