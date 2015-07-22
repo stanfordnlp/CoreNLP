2b9b33ed9e39cd6fdf1cb3a23aa07319793a01aa
Keenon Werling <keenon@stanford.edu>
2015-07-22 16:11:44 -0700

    Committing the simple benchmarks I used to evaluate multithreading of objective functions.

---
+++ test/src/edu/stanford/nlp/benchmarks/Benchmarks.java
@@ -0,0 +1,157 @@
+package edu.stanford.nlp.benchmarks;
+
+import edu.stanford.nlp.classify.Dataset;
+import edu.stanford.nlp.classify.LinearClassifierFactory;
+import edu.stanford.nlp.classify.RVFDataset;
+import edu.stanford.nlp.ie.crf.CRFClassifier;
+import edu.stanford.nlp.ie.machinereading.structure.AnnotationUtils;
+import edu.stanford.nlp.ling.*;
+import edu.stanford.nlp.stats.ClassicCounter;
+import edu.stanford.nlp.stats.Counter;
+
+import java.util.*;
+
+/**
+ * Created by keenon on 6/19/15.
+ *
+ * Down and dirty (and not entirely representative) benchmarks to quickly judge improvement as we optimize stuff
+ */
+public class Benchmarks {
+    /**
+     * 67% of time spent in LogConditionalObjectiveFunction.rvfcalculate()
+     * 29% of time spent in dataset construction (11% in RVFDataset.addFeatures(), 7% rvf incrementCount(), 11% rest)
+     *
+     * Single threaded, 4700 ms
+     * Multi threaded, 700 ms
+     *
+     * with 8 cpus, a 6.7x speedup -- almost, but not quite linear, pretty good
+     */
+    public static void benchmarkRVFLogisticRegression() {
+        Random r = new Random(42);
+
+        RVFDataset<String, String> data = new RVFDataset<>();
+        for (int i = 0; i < 10000; i++) {
+            Counter<String> features = new ClassicCounter<>();
+
+            boolean cl = r.nextBoolean();
+
+            for (int j = 0; j < 1000; j++) {
+                double value;
+                if (cl && i % 2 == 0) {
+                    value = (r.nextDouble()*2.0)-0.6;
+                }
+                else {
+                    value = (r.nextDouble()*2.0)-1.4;
+                }
+                features.incrementCount("f" + j, value);
+            }
+
+            data.add(new RVFDatum<>(features, "target:" + cl));
+        }
+
+        LinearClassifierFactory<String, String> factory = new LinearClassifierFactory<>();
+
+        long msStart = System.currentTimeMillis();
+        factory.trainClassifier(data);
+        long delay = System.currentTimeMillis() - msStart;
+        System.out.println("Training took "+delay+" ms");
+    }
+
+    /**
+     * 57% of time spent in LogConditionalObjectiveFunction.calculateCLBatch()
+     * 22% spent in constructing datums (expensive)
+     *
+     * Single threaded, 4100 ms
+     * Multi threaded, 600 ms
+     *
+     * with 8 cpus, a 6.8x speedup -- basically the same as with RVFDatum
+     */
+    public static void benchmarkLogisticRegression() {
+        Random r = new Random(42);
+
+        Dataset<String, String> data = new Dataset<>();
+        for (int i = 0; i < 10000; i++) {
+            Set<String> features = new HashSet<>();
+
+            boolean cl = r.nextBoolean();
+
+            for (int j = 0; j < 1000; j++) {
+                if (cl && i % 2 == 0) {
+                    if (r.nextDouble() > 0.3) {
+                        features.add("f:"+j+":true");
+                    }
+                    else {
+                        features.add("f:"+j+":false");
+                    }
+                }
+                else {
+                    if (r.nextDouble() > 0.3) {
+                        features.add("f:" + j + ":false");
+                    }
+                    else {
+                        features.add("f:"+j+":false");
+                    }
+                }
+            }
+
+            data.add(new BasicDatum<String, String>(features, "target:" + cl));
+        }
+
+        LinearClassifierFactory<String, String> factory = new LinearClassifierFactory<>();
+
+        long msStart = System.currentTimeMillis();
+        factory.trainClassifier(data);
+        long delay = System.currentTimeMillis() - msStart;
+        System.out.println("Training took "+delay+" ms");
+    }
+
+    /**
+     * 29% in FactorTable.getValue()
+     * 28% in CRFCliqueTree.getCalibratedCliqueTree()
+     * 12.6% waiting for threads
+     *
+     * Single threaded: 15000 ms - 26000 ms
+     * Multi threaded: 4500 ms - 7000 ms
+     *
+     * with 8 cpus, 3.3x - 3.7x speedup, around 800% utilization
+     */
+    public static void benchmarkCRF() {
+        Properties props = new Properties();
+        props.setProperty("macro", "true"); // use a generic CRF configuration
+        props.setProperty("useIfInteger", "true");
+        props.setProperty("featureFactory", "edu.stanford.nlp.benchmarks.BenchmarkFeatureFactory");
+        props.setProperty("saveFeatureIndexToDisk", "false");
+
+        CRFClassifier<CoreLabel> crf = new CRFClassifier<CoreLabel>(props);
+
+        Random r = new Random(42);
+
+        List<List<CoreLabel>> data = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            List<CoreLabel> sentence = new ArrayList<>();
+            for (int j = 0; j < 20; j++) {
+                CoreLabel l = new CoreLabel();
+
+                l.setWord("j:"+j);
+
+                boolean tag = j % 2 == 0 ^ (r.nextDouble() > 0.7);
+                l.set(CoreAnnotations.AnswerAnnotation.class, "target:"+tag);
+                sentence.add(l);
+            }
+            data.add(sentence);
+        }
+
+        long msStart = System.currentTimeMillis();
+        crf.train(data);
+        long delay = System.currentTimeMillis() - msStart;
+        System.out.println("Training took "+delay+" ms");
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            // benchmarkRVFLogisticRegression();
+            // benchmarkLogisticRegression();
+            benchmarkCRF();
+        }
+    }
+}
