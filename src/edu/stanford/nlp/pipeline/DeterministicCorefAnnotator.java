0559645fcc1a5e39948cfc2ae24b03e38a6d6ed9
Christopher Manning <manning@stanford.edu>
2015-08-14 12:46:19 -0700

    Merged

--- src/edu/stanford/nlp/pipeline/DeterministicCorefAnnotator.java
+++ src/edu/stanford/nlp/pipeline/DeterministicCorefAnnotator.java
@@ -86,7 +86,7 @@ public class DeterministicCorefAnnotator implements Annotator {
           Tree tree = sentence.get(TreeCoreAnnotations.TreeAnnotation.class);
           trees.add(tree);
 
-          SemanticGraph dependencies = SemanticGraphFactory.makeFromTree(tree, Mode.COLLAPSED, Extras.NONE, false, null, true); // todo: Needs to lock!
+          SemanticGraph dependencies = SemanticGraphFactory.makeFromTree(tree, Mode.COLLAPSED, Extras.NONE, true, null, true); // locking here is crucial for correct threading!
           sentence.set(SemanticGraphCoreAnnotations.AlternativeDependenciesAnnotation.class, dependencies);
 
           if (!hasSpeakerAnnotations) {
