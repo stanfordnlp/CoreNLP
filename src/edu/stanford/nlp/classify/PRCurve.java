61d86d819fff86cc70ee166cd991e24d14e3d350
Sebastian Schuster <sebschu@gmail.com>
2015-09-01 17:00:37 -0700

    Add basic CoNLL-U writer.

--- src/edu/stanford/nlp/classify/PRCurve.java
+++ src/edu/stanford/nlp/classify/PRCurve.java
@@ -10,7 +10,6 @@ import edu.stanford.nlp.util.Pair;
 import edu.stanford.nlp.util.PriorityQueue;
 import edu.stanford.nlp.util.StringUtils;
 import edu.stanford.nlp.util.Triple;
-import edu.stanford.nlp.util.logging.Logging;
 
 /** A class to create recall-precision curves given scores
  *  used to fit the best monotonic function for logistic regression and SVMs.
@@ -92,7 +91,7 @@ public class PRCurve {
     List<Pair<Integer, Pair<Double, Integer>>> sorted = q.toSortedList();
     scores = new double[sorted.size()];
     classes = new int[sorted.size()];
-    Logging.logger(this.getClass()).info("incoming size " + dataScores.size() + " resulting " + sorted.size());
+    System.err.println("incoming size " + dataScores.size() + " resulting " + sorted.size());
 
     for (int i = 0; i < sorted.size(); i++) {
       Pair<Double, Integer> next = sorted.get(i).second();
@@ -112,7 +111,7 @@ public class PRCurve {
     scores = new double[sorted.size()];
     classes = new int[sorted.size()];
     guesses = new int[sorted.size()];
-    Logging.logger(this.getClass()).info("incoming size " + dataScores.size() + " resulting " + sorted.size());
+    System.err.println("incoming size " + dataScores.size() + " resulting " + sorted.size());
 
     for (int i = 0; i < sorted.size(); i++) {
       Triple<Double, Integer, Integer> next = sorted.get(i).second();
@@ -139,7 +138,7 @@ public class PRCurve {
     for (int i = 1; i <= num; i++) {
       numpositive[i] = numpositive[i - 1] + (classes[num - i] == 0 ? 0 : 1);
     }
-    Logging.logger(this.getClass()).info("total positive " + numpositive[num] + " total negative " + numnegative[num] + " total " + num);
+    System.err.println("total positive " + numpositive[num] + " total negative " + numnegative[num] + " total " + num);
     for (int i = 1; i < numpositive.length; i++) {
       //System.out.println(i + " positive " + numpositive[i] + " negative " + numnegative[i] + " classes " + classes[i - 1] + " " + classes[num - i]);
     }
@@ -209,7 +208,7 @@ public class PRCurve {
       } else {
         leftIndex++;
       }
-      //Logging.logger(this.getClass()).info("chose "+chosen+" score "+scores[chosen]+" class "+classes[chosen]+" correct "+correct(scores[chosen],classes[chosen]));
+      //System.err.println("chose "+chosen+" score "+scores[chosen]+" class "+classes[chosen]+" correct "+correct(scores[chosen],classes[chosen]));
       if ((scores[chosen] >= .5) && (classes[chosen] == 1)) {
         totalcorrect++;
       }
@@ -263,7 +262,7 @@ public class PRCurve {
       } else {
         leftIndex++;
       }
-      //Logging.logger(this.getClass()).info("chose "+chosen+" score "+scores[chosen]+" class "+classes[chosen]+" correct "+correct(scores[chosen],classes[chosen]));
+      //System.err.println("chose "+chosen+" score "+scores[chosen]+" class "+classes[chosen]+" correct "+correct(scores[chosen],classes[chosen]));
       if ((scores[chosen] >= .5)) {
         if (classes[chosen] == 1) {
           tp++;
@@ -354,13 +353,13 @@ public class PRCurve {
     PriorityQueue<String> q = new BinaryHeapPriorityQueue<String>();
     q.add("bla", 2);
     q.add("bla3", 2);
-    Logging.logger(PRCurve.class).info("size of q " + q.size());
+    System.err.println("size of q " + q.size());
 
     PRCurve pr = new PRCurve("c:/data0204/precsvm", true);
-    Logging.logger(PRCurve.class).info("acc " + pr.accuracy() + " opt " + pr.optimalAccuracy() + " cwa " + pr.cwa() + " optcwa " + pr.optimalCwa());
+    System.err.println("acc " + pr.accuracy() + " opt " + pr.optimalAccuracy() + " cwa " + pr.cwa() + " optcwa " + pr.optimalCwa());
     for (int r = 1; r <= pr.numSamples(); r++) {
-      Logging.logger(PRCurve.class).info("optimal precision at recall " + r + " " + pr.precision(r));
-      Logging.logger(PRCurve.class).info("model precision at recall " + r + " " + pr.logPrecision(r));
+      System.err.println("optimal precision at recall " + r + " " + pr.precision(r));
+      System.err.println("model precision at recall " + r + " " + pr.logPrecision(r));
     }
   }
 
--- src/edu/stanford/nlp/classify/SVMLightClassifierFactory.java
+++ src/edu/stanford/nlp/classify/SVMLightClassifierFactory.java
@@ -6,7 +6,6 @@ import edu.stanford.nlp.util.*;
 import edu.stanford.nlp.ling.Datum;
 import edu.stanford.nlp.ling.RVFDatum;
 import edu.stanford.nlp.optimization.LineSearcher;
-import edu.stanford.nlp.util.logging.Logging;
 
 import java.io.*;
 import java.text.NumberFormat;
@@ -318,6 +317,12 @@ public class SVMLightClassifierFactory<L, F> implements ClassifierFactory<L, F, 
     useSigmoid = oldUseSigmoid;
   }
 
+  @Deprecated
+  public SVMLightClassifier<L, F> trainClassifier(List<RVFDatum<L, F>> examples) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
   private boolean tuneHeldOut = false;
   private boolean tuneCV = false;
   private Scorer<L> scorer = new MultiClassAccuracyStats<L>();
@@ -440,7 +445,7 @@ public class SVMLightClassifierFactory<L, F> implements ClassifierFactory<L, F, 
       // File and Model Data
       cmd = cmd + " " + dataFile.getAbsolutePath() + " " + modelFile.getAbsolutePath();
 
-      if (verbose) Logging.logger(this.getClass()).info("<< "+cmd+" >>");
+      if (verbose) System.err.println("<< "+cmd+" >>");
 
       /*Process p = Runtime.getRuntime().exec(cmd);
 
@@ -458,7 +463,7 @@ public class SVMLightClassifierFactory<L, F> implements ClassifierFactory<L, F, 
         }
         String evalCmd = (multiclass ? svmStructClassify : (useSVMPerf ? svmPerfClassify : svmLightClassify)) + " "
                 + dataFile.getAbsolutePath() + " " + modelFile.getAbsolutePath() + " " + predictFile.getAbsolutePath();
-        if (verbose) Logging.logger(this.getClass()).info("<< " + evalCmd + " >>");
+        if (verbose) System.err.println("<< "+evalCmd+" >>");
         SystemUtils.run(new ProcessBuilder(whitespacePattern.split(evalCmd)),
                 new PrintWriter(System.err), new PrintWriter(System.err));
       }
