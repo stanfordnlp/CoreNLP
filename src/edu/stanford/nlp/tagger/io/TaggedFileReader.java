7a174538225ed8894df067d1da4756332844c59d
Christopher Manning <manning@cs.stanford.edu>
2015-10-10 16:42:23 -0700

    Remove or deprecate a few duplicate IOUtils methods.

--- src/edu/stanford/nlp/tagger/io/TaggedFileReader.java
+++ src/edu/stanford/nlp/tagger/io/TaggedFileReader.java
@@ -4,8 +4,8 @@ import java.util.Iterator;
 import java.util.List;
 import edu.stanford.nlp.ling.TaggedWord;
 
-public interface TaggedFileReader 
-  extends Iterable<List<TaggedWord>>, Iterator<List<TaggedWord>>
-{
-  public String filename();
+public interface TaggedFileReader extends Iterable<List<TaggedWord>>, Iterator<List<TaggedWord>> {
+
+  String filename();
+
 }
