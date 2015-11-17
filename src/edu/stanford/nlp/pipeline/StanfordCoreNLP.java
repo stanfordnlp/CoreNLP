659f36ef61a8a9950006dbc352f197ed528e81cf
Christopher Manning <manning@cs.stanford.edu>
2015-11-16 20:46:54 -0800

    Correct position of break statement.

--- src/edu/stanford/nlp/pipeline/StanfordCoreNLP.java
+++ src/edu/stanford/nlp/pipeline/StanfordCoreNLP.java
@@ -96,7 +96,7 @@ public class StanfordCoreNLP extends AnnotationPipeline {
 
   public static final String DEFAULT_OUTPUT_FORMAT = isXMLOutputPresent() ? "xml" : "text";
 
-  private static Logger logger = LoggerFactory.getLogger(StanfordCoreNLP.class);
+  private static final Logger logger = LoggerFactory.getLogger(StanfordCoreNLP.class);
 
   /** Formats the constituent parse trees for display */
   private TreePrint constituentTreePrinter;
@@ -478,7 +478,7 @@ public class StanfordCoreNLP extends AnnotationPipeline {
       Annotator a =  pool.get(name);
       return a;
     } catch(IllegalArgumentException e) {
-      logger.error("ERROR: attempted to fetch annotator \"" + name + 
+      logger.error("ERROR: attempted to fetch annotator \"" + name +
         "\" but the annotator pool does not store any such type!");
       return null;
     }
@@ -873,8 +873,8 @@ public class StanfordCoreNLP extends AnnotationPipeline {
             if (outputSerializerClass != null) {
               AnnotationSerializer outputSerializer = loadSerializer(outputSerializerClass, outputSerializerName, properties);
               outputSerializer.write(annotation, fos);
-              break;
             }
+            break;
           }
           case CONLLU:
             new CoNLLUOutputter().print(annotation, fos, outputOptions);
@@ -905,7 +905,7 @@ public class StanfordCoreNLP extends AnnotationPipeline {
                                      Properties properties, BiConsumer<Annotation, Consumer<Annotation>> annotate,
                                      BiConsumer<Annotation, OutputStream> print,
                                      OutputFormat outputFormat) throws IOException {
-    List<Runnable> toRun = new LinkedList<>();
+    // List<Runnable> toRun = new LinkedList<>();
 
     // Process properties here
     final String baseOutputDir = properties.getProperty("outputDirectory", ".");
