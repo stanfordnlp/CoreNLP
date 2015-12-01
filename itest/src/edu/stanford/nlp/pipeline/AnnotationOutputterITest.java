dc6710c8938eafb85b2135934861288bd32f7999
Jason Bolton <jebolton@stanford.edu>
2015-12-01 05:10:54 -0800

    fix AnnotationOutputterITest issue with expected output

--- itest/src/edu/stanford/nlp/pipeline/AnnotationOutputterITest.java
+++ itest/src/edu/stanford/nlp/pipeline/AnnotationOutputterITest.java
@@ -30,7 +30,7 @@ public class AnnotationOutputterITest extends TestCase {
             "2\tdog\tdog\tNN\tO\t4\tnsubj\n" +
             "3\tis\tbe\tVBZ\tO\t4\tcop\n" +
             "4\tlazy\tlazy\tJJ\tO\t0\tROOT\n" +
-            "5\t.\t.\t.\tO\t_\t_\n" +
+            "5\t.\t.\t.\tO\t4\tpunct\n" +
             "\n";
     assertEquals(expected, actual);
   }
