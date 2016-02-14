5167712309231f46b7ef7aac56f05e07ccb346a0
Christopher Manning <manning@cs.stanford.edu>
2016-02-13 21:03:56 -0800

    Add missing serialVersionUID.

--- src/edu/stanford/nlp/parser/lexparser/SpanishUnknownWordModel.java
+++ src/edu/stanford/nlp/parser/lexparser/SpanishUnknownWordModel.java
@@ -1,4 +1,5 @@
-package edu.stanford.nlp.parser.lexparser; 
+package edu.stanford.nlp.parser.lexparser;
+
 import edu.stanford.nlp.util.logging.Redwood;
 
 import edu.stanford.nlp.international.spanish.SpanishUnknownWordSignatures;
@@ -10,7 +11,7 @@ import edu.stanford.nlp.util.StringUtils;
 public class SpanishUnknownWordModel extends BaseUnknownWordModel  {
 
   /** A logger for this class */
-  private static Redwood.RedwoodChannels log = Redwood.channels(SpanishUnknownWordModel.class);
+  private static final Redwood.RedwoodChannels log = Redwood.channels(SpanishUnknownWordModel.class);
 
   protected final boolean smartMutation;
 
@@ -136,18 +137,20 @@ public class SpanishUnknownWordModel extends BaseUnknownWordModel  {
         break;
 
       default:
-        System.err.printf("%s: Invalid unknown word signature! (%d)%n", this.getClass().getName(),unknownLevel);
+        log.error(String.format("%s: Invalid unknown word signature! (%d)%n", this.getClass().getName(),unknownLevel));
     }
 
     return sb.toString();
   }
 
-  private boolean isUpperCase(String s) {
+  private static boolean isUpperCase(String s) {
     for (int i = 0; i < s.length(); i++) {
       if (Character.isLowerCase(s.charAt(i)))
         return false;
     }
-
     return true;
   }
+
+  private static final long serialVersionUID = 5370429530690606644L;
+
 }
