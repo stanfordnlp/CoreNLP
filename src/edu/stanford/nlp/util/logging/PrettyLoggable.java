c178e980db99f796c6ecfee95fa62426880b9f98
Christopher Manning <manning@cs.stanford.edu>
2016-02-15 21:52:21 -0800

    Remove code warnings.

--- src/edu/stanford/nlp/util/logging/PrettyLoggable.java
+++ src/edu/stanford/nlp/util/logging/PrettyLoggable.java
@@ -6,21 +6,22 @@ import edu.stanford.nlp.util.logging.Redwood.RedwoodChannels;
 /**
  * Indicates that a class supports "pretty logging". Pretty logging is a type of
  * pretty-printing that uses the Redwood logging system to structure itself.
- * 
+ *
  * When pretty logging the contents of your object, you should check to see if
  * each object (call it <code>obj</code>) is dispatchable with
  * <code>PrettyLogger.dispatchable(obj)</code> if you don't know their type. If
  * true, you should call <code>channels.prettyLog(obj)</code> to pretty log it.
  * Otherwise, use its <code>toString()</code> method.
- * 
+ *
  * @see PrettyLogger
  * @author David McClosky
- * 
+ *
  */
 public interface PrettyLoggable {
+
   /**
    * Pretty logs the current object to specific Redwood channels.
-   * 
+   *
    * @param channels
    *          the channels which should be logged to -- all logging calls should
    *          use logging methods on the channels (e.g. channels.log(), etc.)
@@ -30,5 +31,6 @@ public interface PrettyLoggable {
    *          typically used as a track name surrounding the contents of this
    *          object.
    */
-  public void prettyLog(RedwoodChannels channels, String description);
+  void prettyLog(RedwoodChannels channels, String description);
+
 }
