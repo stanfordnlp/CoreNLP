69627ddd834b8fa6b3e8b7a5204477d51e54c942
Sebastian Schuster <sebschu@gmail.com>
2016-05-26 00:58:59 -0700

    ProtobufAnnotationSerializerSlowITest: correct uncompressed number

--- itest/src/edu/stanford/nlp/pipeline/ProtobufAnnotationSerializerSlowITest.java
+++ itest/src/edu/stanford/nlp/pipeline/ProtobufAnnotationSerializerSlowITest.java
@@ -307,7 +307,7 @@ public class ProtobufAnnotationSerializerSlowITest {
 
     // Check size
     assertTrue("" + compressedProto.length, compressedProto.length < 390000);
-    assertTrue("" + uncompressedProto.length, uncompressedProto.length < 2000000);
+    assertTrue("" + uncompressedProto.length, uncompressedProto.length < 2100000);
   }
 
   @Test
