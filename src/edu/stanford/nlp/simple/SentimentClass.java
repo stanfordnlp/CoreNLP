ddf4525da80464fd7f4aa62cac2e04c13816b9ba
Gabor Angeli <gabor@eloquent.ai>
2016-06-06 20:08:18 -0700

    Change my email to angeli@stanford.edu

--- src/edu/stanford/nlp/simple/SentimentClass.java
+++ src/edu/stanford/nlp/simple/SentimentClass.java
@@ -3,7 +3,7 @@ package edu.stanford.nlp.simple;
 /**
  * An enum for the Simple CoreNLP API to represent a sentiment value.
  *
- * @author <a href="mailto:gabor@eloquent.ai">Gabor Angeli</a>
+ * @author <a href="mailto:angeli@stanford.edu">Gabor Angeli</a>
  */
 public enum SentimentClass {
   POSITIVE,
