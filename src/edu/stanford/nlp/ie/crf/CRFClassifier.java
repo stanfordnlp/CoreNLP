1be9f5a12bd158593dfadea6de57f7e4c7e74287
Christopher Manning <manning@cs.stanford.edu>
2016-06-11 12:40:20 -0700

    Improve printing of NER performance in logging environment.

--- src/edu/stanford/nlp/ie/crf/CRFClassifier.java
+++ src/edu/stanford/nlp/ie/crf/CRFClassifier.java
@@ -1181,7 +1181,7 @@ public class CRFClassifier<IN extends CoreMap> extends AbstractSequenceClassifie
     if (!flags.suppressTestDebug)
       log.info(StringUtils.getShortClassName(this) + " tagged " + numWords + " words in " + numDocs
         + " documents at " + nf.format(wordspersec) + " words per second.");
-    if (resultsCounted && !flags.suppressTestDebug) {
+    if (resultsCounted && ! flags.suppressTestDebug) {
       printResults(entityTP, entityFP, entityFN);
     }
   }
@@ -1659,8 +1659,7 @@ public class CRFClassifier<IN extends CoreMap> extends AbstractSequenceClassifie
         if (flags.testFiles != null) {
           String[] testFiles = flags.testFiles.split(",");
           for (String testFile : testFiles) {
-            CRFClassifierEvaluator<IN> crfEvaluator = new CRFClassifierEvaluator<>("Test set ("
-                    + testFile + ")", this);
+            CRFClassifierEvaluator<IN> crfEvaluator = new CRFClassifierEvaluator<>("Test set (" + testFile + ')', this);
             ObjectBank<List<IN>> testObjBank = makeObjectBankFromFile(testFile, readerAndWriter);
             List<Triple<int[][][], int[], double[][][]>> testDataAndLabels = documentsToDataAndLabelsList(testObjBank);
             crfEvaluator.setTestData(testObjBank, testDataAndLabels);
