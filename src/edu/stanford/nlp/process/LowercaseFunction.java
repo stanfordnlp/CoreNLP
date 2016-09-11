9c6f01f7702223a05905be62800c5fe2202cbefa
Christopher Manning <manning@cs.stanford.edu>
2016-09-11 12:59:19 -0700

    Tiny cleanups

--- src/edu/stanford/nlp/process/LowercaseFunction.java
+++ src/edu/stanford/nlp/process/LowercaseFunction.java
@@ -5,6 +5,8 @@ import java.io.Serializable;
 import java.util.function.Function;
 
 public class LowercaseFunction implements Function<String, String>, Serializable {
+
+  @Override
   public String apply(String input) {
     if (input == null) {
       return null;
@@ -12,5 +14,5 @@ public class LowercaseFunction implements Function<String, String>, Serializable
     return input.toLowerCase();
   }
 
-  private static final long serialVersionUID = 1L;  
+  private static final long serialVersionUID = 1L;
 }
