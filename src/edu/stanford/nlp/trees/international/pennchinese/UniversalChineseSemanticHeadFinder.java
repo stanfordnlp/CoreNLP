5daf354682aa084a41450a349f484a393d3d19fb
Jason Bolton <jebolton@stanford.edu>
2016-09-17 10:43:23 -0700

    latest

--- src/edu/stanford/nlp/trees/international/pennchinese/UniversalChineseSemanticHeadFinder.java
+++ src/edu/stanford/nlp/trees/international/pennchinese/UniversalChineseSemanticHeadFinder.java
@@ -24,23 +24,15 @@ public class UniversalChineseSemanticHeadFinder extends ChineseHeadFinder {
   /** Makes modifications of head finder rules to better fit with semantic notions of heads. */
   private void ruleChanges() {
     // Note: removed VC and added NP; copula should not be the head.
-    // todo [pengqi 2016]: prioritizing VP over VV works in most cases, but this actually interferes
-    //   with xcomps(?) like
-    // (VP (VV 继续)
-    //     (VP (VC 是)
-    //         (NP 重要 的 国际 新闻)
-    //     )
-    // )
-    nonTerminalInfo.put("VP", new String[][]{{"left", "VP", "VCD", "VSB", "VPT", "VV", "VCP", "VA", "VE", "IP", "VRD", "VNV", "NP"}, leftExceptPunct});
+    nonTerminalInfo.put("VP", new String[][]{{"left", "VP", "VCD", "VPT", "VV", "VCP", "VA", "VE", "IP", "VSB", "VCP", "VRD", "VNV", "NP"}, leftExceptPunct});
 
-    //nonTerminalInfo.put("CP", new String[][]{{"right", "CP", "IP", "VP"}, rightExceptPunct});
-    nonTerminalInfo.put("CP", new String[][]{{"rightexcept", "DEC", "WHNP", "WHPP", "SP"}, rightExceptPunct});
+    nonTerminalInfo.put("CP", new String[][]{{"right", "CP", "IP", "VP"}, rightExceptPunct});
     nonTerminalInfo.put("DVP", new String[][]{{"leftdis", "VP" ,"ADVP"}});
     nonTerminalInfo.put("LST", new String[][]{{"right", "CD", "NP", "QP", "PU"}});
 
     nonTerminalInfo.put("PP", new String[][]{{"leftexcept", "P"}});  // Preposition
     nonTerminalInfo.put("LCP", new String[][]{{"leftexcept", "LC"}});  // Localizer
-    nonTerminalInfo.put("DNP", new String[][]{{"rightexcept", "DEG"}});  // Associative
+    nonTerminalInfo.put("DNP", new String[][]{{"leftexcept", "DEG"}});  // Associative
   }
 
   private static final long serialVersionUID = 2L;
