451fdcaad414743a4d29212b5d3a36df87cc31dd
Danqi Chen <danqi@cs.stanford.edu>
2016-09-27 00:40:50 -0700

    Skip the useless lines in loadCoNLLFile

--- src/edu/stanford/nlp/parser/nndep/Util.java
+++ src/edu/stanford/nlp/parser/nndep/Util.java
@@ -1,12 +1,5 @@
 
-/*
-* 	@Author:  Danqi Chen
-* 	@Email:  danqi@cs.stanford.edu
-*	@Created:  2014-08-25
-* 	@Last Modified:  2014-10-05
-*/
-
-package edu.stanford.nlp.parser.nndep; 
+package edu.stanford.nlp.parser.nndep;
 import edu.stanford.nlp.util.logging.Redwood;
 
 import edu.stanford.nlp.io.IOUtils;
@@ -150,26 +143,32 @@ public class Util  {
     try {
       reader = IOUtils.readerFromString(inFile);
 
-      CoreMap sentence = new CoreLabel();
       List<CoreLabel> sentenceTokens = new ArrayList<>();
-
       DependencyTree tree = new DependencyTree();
 
       for (String line : IOUtils.getLineIterable(reader, false)) {
         String[] splits = line.split("\t");
         if (splits.length < 10) {
-          trees.add(tree);
-          sentence.set(CoreAnnotations.TokensAnnotation.class, sentenceTokens);
-          sents.add(sentence);
-
-          tree = new DependencyTree();
-          sentence = new CoreLabel();
-          sentenceTokens = new ArrayList<>();
+          if (sentenceTokens.size() > 0) {
+            trees.add(tree);
+            CoreMap sentence = new CoreLabel();
+            sentence.set(CoreAnnotations.TokensAnnotation.class, sentenceTokens);
+            sents.add(sentence);
+            tree = new DependencyTree();
+            sentenceTokens = new ArrayList<>();
+          }
         } else {
           String word = splits[1],
                   pos = cPOS ? splits[3] : splits[4],
                   depType = splits[7];
-          int head = Integer.parseInt(splits[6]);
+
+          int head = -1;
+          try {
+            head = Integer.parseInt(splits[6]);
+          }
+          catch (NumberFormatException e) {
+            continue;
+          }
 
           CoreLabel token = tf.makeToken(word, 0, 0);
           token.setTag(pos);
@@ -182,7 +181,7 @@ public class Util  {
           else
             tree.add(head, Config.UNKNOWN);
         }
-      }    
+      }
     } catch (IOException e) {
       throw new RuntimeIOException(e);
     } finally {
