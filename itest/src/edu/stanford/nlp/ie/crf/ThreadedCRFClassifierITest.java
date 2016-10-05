7b6921110867e4b82a808d5ce74e63e1ef5f93c9
Christopher Manning <manning@cs.stanford.edu>
2016-10-05 06:37:42 -0700

    Updates for new German models (will hopefully fix tests).

--- itest/src/edu/stanford/nlp/ie/crf/ThreadedCRFClassifierITest.java
+++ itest/src/edu/stanford/nlp/ie/crf/ThreadedCRFClassifierITest.java
@@ -15,15 +15,17 @@ public class ThreadedCRFClassifierITest extends TestCase {
 
   private String german1 = 
     "/u/nlp/data/ner/goodClassifiers/german.hgc_175m_600.crf.ser.gz";
+  /** -- We're no longer supporting this one
   private String german2 = 
     "/u/nlp/data/ner/goodClassifiers/german.dewac_175m_600.crf.ser.gz";
-  private String germanTestFile = "/u/nlp/data/german/ner/deu.testa";
+  */
+  private String germanTestFile = "/u/nlp/data/german/ner/2016/deu.testa";
 
   private String english1 = 
     "/u/nlp/data/ner/goodClassifiers/english.all.3class.nodistsim.crf.ser.gz";
   private String english2 = 
-    "/u/nlp/data/ner/goodClassifiers/english.all.3class.distsim.crf.ser.gz";
-  private String englishTestFile = "/u/nlp/data/ner/column_data/conll.testa";
+    "/u/nlp/data/ner/goodClassifiers/english.conll.4class.distsim.crf.ser.gz";
+  private String englishTestFile = "/u/nlp/data/ner/column_data/conll.4class.testa";
 
   private String germanEncoding = "iso-8859-1";
   private String englishEncoding = "utf-8";
@@ -47,11 +49,11 @@ public class ThreadedCRFClassifierITest extends TestCase {
     TestThreadedCRFClassifier.runTest(props);
   }
 
-  public void testTwoGermanCRFs() {
-    props.setProperty("crf1", german1);
-    props.setProperty("crf2", german2);
-    props.setProperty("testFile", germanTestFile);
-    props.setProperty("inputEncoding", germanEncoding);
+  public void testTwoEnglishCRFs() {
+    props.setProperty("crf1", english1);
+    props.setProperty("crf2", english2);
+    props.setProperty("testFile", englishTestFile);
+    props.setProperty("inputEncoding", englishEncoding);
     TestThreadedCRFClassifier.runTest(props);
   }
 }
