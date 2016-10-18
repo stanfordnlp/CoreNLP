0a0fa6cfcb52b27ef3205f994f1b72b3d085b082
Jason Bolton <jebolton@stanford.edu>
2016-10-17 22:40:46 -0700

    bug fix

--- src/edu/stanford/nlp/pipeline/AnnotatorImplementations.java
+++ src/edu/stanford/nlp/pipeline/AnnotatorImplementations.java
@@ -220,6 +220,12 @@ public class AnnotatorImplementations  {
     Properties mentionProperties = PropertiesUtils.extractPrefixedProperties(properties,
             Annotator.STANFORD_MENTION + ".",
             true);
+    // handle default for coref.useConstituencyParse based on whether parse is used or not
+    String[] annotatorsList = properties.getProperty("annotators","").replaceAll("\\s+","").split(",");
+    if (Arrays.asList(annotatorsList).contains(Annotator.STANFORD_PARSE) &&
+            !corefProperties.containsKey(Annotator.STANFORD_COREF+".useConstituencyParse")) {
+      corefProperties.setProperty(Annotator.STANFORD_COREF+".useConstituencyParse", "true");
+    }
     Properties allPropsForCoref = new Properties();
     allPropsForCoref.putAll(corefProperties);
     allPropsForCoref.putAll(mentionProperties);
