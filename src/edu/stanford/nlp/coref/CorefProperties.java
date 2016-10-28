33cad1d33df445cf74a6f592b1904952a41b5c7d
Jason Bolton <jebolton@stanford.edu>
2016-10-27 18:54:53 -0700

    fix path issue

--- src/edu/stanford/nlp/coref/CorefProperties.java
+++ src/edu/stanford/nlp/coref/CorefProperties.java
@@ -101,7 +101,10 @@ public class CorefProperties {
 
   public static final String OUTPUT_PATH_PROP = "coref.conllOutputPath";
   public static String conllOutputPath(Properties props) {
-    return props.getProperty("coref.conllOutputPath", "/scr/nlp/coref/logs/");
+    String returnPath = props.getProperty("coref.conllOutputPath", "/scr/nlp/coref/logs/");
+    if (!returnPath.substring(returnPath.length()-1).equals("/"))
+      returnPath += "/";
+    return returnPath;
   }
 
   public enum Dataset {TRAIN, DEV, TEST}
