d4b398013fc708fc9d480712b8433f5e44f1ad8f
J38 <jebolton@stanford.edu>
2016-11-10 13:16:24 -0800

    fix null pointer issue

--- src/edu/stanford/nlp/pipeline/EntityMentionsAnnotator.java
+++ src/edu/stanford/nlp/pipeline/EntityMentionsAnnotator.java
@@ -202,9 +202,11 @@ public class EntityMentionsAnnotator implements Annotator {
             // ... and actually are an acronym
             if (AcronymMatcher.isAcronym(token.word(), org)) {
               // ... and add them.
+              System.out.println("found ACRONYM ORG");
               token.setNER("ORGANIZATION");
               CoreMap chunk = ChunkAnnotationUtils.getAnnotatedChunk(tokens,
                   i, i + 1, totalTokensOffset, null, null, null);
+              chunk.set(CoreAnnotations.NamedEntityTagAnnotation.class,"ORGANIZATION");
               mentions.add(chunk);
 
             }
