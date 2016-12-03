f3fce7e68554541b466d633fa487e9286a5470c0
Christopher Manning <manning@cs.stanford.edu>
2016-12-03 11:51:00 -0800

    Improve documentation.

--- src/edu/stanford/nlp/ling/CoreAnnotations.java
+++ src/edu/stanford/nlp/ling/CoreAnnotations.java
@@ -166,7 +166,10 @@ public class CoreAnnotations {
   }
 
   /**
-   * The CoreMap key for getting the sentences contained by an annotation.
+   * The CoreMap key for getting the sentences contained in an annotation.
+   * The sentences are represented as a {@code List<CoreMap>}.
+   * Each sentence might typically have annotations such as {@code TextAnnotation},
+   * {@code TokensAnnotation}, {@code SentenceIndexAnnotation}, and {@code BasicDependenciesAnnotation}.
    *
    * This key is typically set only on document annotations.
    */
