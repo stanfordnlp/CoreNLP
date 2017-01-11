452711204e1412dd522738ca5ced6ed4c7ca47cb
Gabor Angeli <gabor@eloquent.ai>
2017-01-11 11:41:52 -0800

    Add ability to listen to Redwood channels

--- src/edu/stanford/nlp/util/logging/Redwood.java
+++ src/edu/stanford/nlp/util/logging/Redwood.java
@@ -1312,9 +1312,11 @@ public class Redwood  {
   @SuppressWarnings("deprecation")
   public static void main(String[] args){
 
-    Redwood.log(Redwood.DBG, "hello world!");
-    Redwood.hideChannelsEverywhere(Redwood.DBG);
-    Redwood.log(Redwood.DBG, "hello debug!");
+    RedwoodConfiguration.current().listenOnChannels(record -> {
+      System.out.println(">>> " + record.content.toString());
+    }, Redwood.ERR).apply();
+    Redwood.log("hello world!");
+    Redwood.log(Redwood.ERR, "an error!");
 
     System.exit(1);
 
