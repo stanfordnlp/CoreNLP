e4046afdfda1bf433107c1e0c3f0415f66cb6943
Jason Bolton <jebolton@stanford.edu>
2017-03-26 19:35:55 -0700

    Merge branch 'master' of origin

--- src/edu/stanford/nlp/pipeline/AnnotatorPool.java
+++ src/edu/stanford/nlp/pipeline/AnnotatorPool.java
@@ -61,66 +61,6 @@ public class AnnotatorPool  {
   }
 
 
-  /**
-   * The cache of Annotators already created at some point.
-   * This is keyed on a pair: (annotator_name, signature), and returns a soft reference to an annotator.
-   */
-  private static final Map<Pair<String, String>, SoftReference<Annotator>> cache = Generics.newHashMap();
-
-  /**
-   * A set of annotators that we want to keep hard references to.
-   * These are cleaned up in {@link AnnotatorPool#gc}'s timer task.
-   */
-  @SuppressWarnings("MismatchedQueryAndUpdateOfCollection")  // Updated with Iterator#remove()
-  private static final IdentityHashMap<Annotator, Long> annotatorsForcedAlive = new IdentityHashMap<>();
-
-
-  /** A timer for cleaning up old annotators */
-  @SuppressWarnings("unused")  // Unused, but still runs
-  private static final Timer gc = new Timer(true) {{
-    scheduleAtFixedRate(new TimerTask() {
-      @Override
-      public void run() {
-        int actionsTaken = 0;
-        long initialMemory = (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (1024 * 1024);
-        // 1. Allow cleaning up any annotator that hasn't been called in the last 10 minutes
-        synchronized (annotatorsForcedAlive) {
-          Iterator<Map.Entry<Annotator, Long>> iter = annotatorsForcedAlive.entrySet().iterator();
-          while (iter.hasNext()) {
-            if (iter.next().getValue() < (System.currentTimeMillis() - 1000 * 60 * 10)) {  // older than 10 minutes old
-              actionsTaken += 1;
-              iter.remove();
-            }
-          }
-        }
-        // 2. Clean up stale keys in the cache
-        synchronized (cache) {
-          Iterator<Map.Entry<Pair<String, String>, SoftReference<Annotator>>> iter = cache.entrySet().iterator();
-          while (iter.hasNext()) {
-            Map.Entry<Pair<String, String>, SoftReference<Annotator>> entry = iter.next();
-            if (entry.getValue().get() == null) {  // this reference has been garbage collected
-              actionsTaken += 1;
-              iter.remove();
-            }
-          }
-        }
-        // 3. Print stats
-        long finalMemory = (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (1024 * 1024);
-        if (actionsTaken > 0) {
-          Runtime.getRuntime().gc();
-          log.info("Annotator GC run. References cleaned=" + actionsTaken + "; Memory: " + initialMemory + " MB  ->  " + finalMemory + " MB");
-          synchronized (cache) {
-            log.info("Annotators in cache: " + cache.size());
-          }
-          synchronized (annotatorsForcedAlive) {
-            log.info("Annotators forced alive: " + annotatorsForcedAlive.size());
-          }
-        }
-      }
-    }, 0, 30000);
-  }};
-
-
   /** The set of factories we know about defining how we should create new annotators of each name */
   private final Map<String, CachedAnnotator> factories;
 
@@ -200,30 +140,11 @@ public class AnnotatorPool  {
    * @throws IllegalArgumentException If the annotator cannot be created
    */
   public synchronized Annotator get(String name) {
-    CachedAnnotator factory;
-    synchronized (factories) {
-      factory = this.factories.get(name);
-    }
-    if (factory == null) {
-      throw new IllegalArgumentException("No annotator named " + name);
-    }
-    Pair<String, String> key = Pair.makePair(name, factory.signature);
-    Optional<Annotator> annotator;
-    synchronized (cache) {
-      annotator = Optional.ofNullable(cache.get(key)).flatMap(x -> Optional.ofNullable(x.get()));
-    }
-    if (!annotator.isPresent()) {
-      annotator = Optional.of(factory.annotator.get());
-    }
-    synchronized (cache) {
-      cache.put(key, new SoftReference<>(annotator.orElse(null)));  // will never be null though
-    }
-
-    // Return
-    if (annotator.isPresent()) {
-      return annotator.get();
+    CachedAnnotator factory =  this.factories.get(name);
+    if (factory != null) {
+      return factory.annotator.get();
     } else {
-      throw new IllegalStateException("Logic error in AnnotatorPool#get()");  // should be impossible
+      throw new IllegalArgumentException("No annotator named " + name);
     }
   }
 
