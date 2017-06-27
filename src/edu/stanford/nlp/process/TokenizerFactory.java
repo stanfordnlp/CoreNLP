82b6233aeb04dd67b045bf745d6f74465542f2e4
Christopher Manning <manning@cs.stanford.edu>
2017-06-27 15:30:01 -0700

    Ever better

--- src/edu/stanford/nlp/process/TokenizerFactory.java
+++ src/edu/stanford/nlp/process/TokenizerFactory.java
@@ -8,12 +8,13 @@ import java.io.Reader;
  * A TokenizerFactory is a factory that can build a Tokenizer (an extension of Iterator)
  * from a java.io.Reader.
  *
- * <i>IMPORTANT NOTE:</i><br/>
+ * <i>IMPORTANT NOTE:</i>
  *
- * A TokenizerFactory should also provide two static methods: <br>
+ * A TokenizerFactory should also provide two static methods:
+ *
  * {@code public static TokenizerFactory<? extends HasWord> newTokenizerFactory(); }
  * {@code public static TokenizerFactory<Word> newWordTokenizerFactory(String options); }
- * <br/>
+ *
  * These are expected by certain JavaNLP code (e.g., LexicalizedParser),
  * which wants to produce a TokenizerFactory by reflection.
  *
@@ -23,10 +24,25 @@ import java.io.Reader;
  */
 public interface TokenizerFactory<T> extends IteratorFromReaderFactory<T> {
 
+  /** Get a tokenizer for this reader.
+   *
+   *  @param r A Reader (which is assumed to already by buffered, if appropriate)
+   *  @return A Tokenizer
+   */
   Tokenizer<T> getTokenizer(Reader r);
 
+  /** Get a tokenizer for this reader.
+   *
+   *  @param r A Reader (which is assumed to already by buffered, if appropriate)
+   *  @param extraOptions Options for how this tokenizer should behave
+   *  @return A Tokenizer
+   */
   Tokenizer<T> getTokenizer(Reader r, String extraOptions);
 
+  /** Sets default options for how tokenizers built from this factory should behave.
+   *
+   *  @param options Options for how this tokenizer should behave
+   */
   void setOptions(String options);
 
 }
