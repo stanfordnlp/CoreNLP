606f360c1902639d8467d325f443c4c87327fd3e
Christopher Manning <manning@cs.stanford.edu>
2017-07-27 20:59:27 -0700

    More diamond operator; close #479

--- test/src/edu/stanford/nlp/ie/regexp/RegexNERSequenceClassifierTest.java
+++ test/src/edu/stanford/nlp/ie/regexp/RegexNERSequenceClassifierTest.java
@@ -1,9 +1,12 @@
 package edu.stanford.nlp.ie.regexp;
 
-import junit.framework.TestCase;
-
 import java.io.*;
 import java.util.*;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.ling.SentenceUtils;
@@ -17,29 +20,29 @@ import edu.stanford.nlp.util.StringUtils;
  *
  * @author John Bauer
  */
-public class RegexNERSequenceClassifierTest extends TestCase {
+public class RegexNERSequenceClassifierTest {
 
-  private static File tempFile = null;
+  private static File tempFile; // = null;
 
-  static final String[] words =
+  private static final String[] words =
   { "My dog likes to eat sausage : turkey , pork , beef , etc .",
     "I went to Shoreline Park and saw an avocet and some curlews ( shorebirds ) ." };
-  static final String[] tags =
+  private static final String[] tags =
   { "PRP$ NN RB VBZ VBG NN : NN , NN , NN , FW .",
     "PRP VBD TO NNP NNP CC VBD DT NN CC DT NNS -LRB- NNP -RRB- ." };
-  static final String[] ner =
+  private static final String[] ner =
   { "O O O O O O O O O O O O O O O",
     "O O O LOCATION LOCATION O O O O O O O O O O O"};
 
-  static final String[] expectedUncased =
+  private static final String[] expectedUncased =
   { "- - - - - food - - - - - - - - -",
     "- - - park park - - - shorebird - - shorebird - - - -" };
 
-  static final String[] expectedCased =
+  private static final String[] expectedCased =
   { "- - - - - food - - - - - - - - -",
     "- - - - - - - - shorebird - - shorebird - - - -" };
 
-  static final String[] nerPatterns = {
+  private static final String[] nerPatterns = {
           "Shoreline Park\tPARK\n",
           "Shoreline Park\tPARK\tLOCATION\n",
           "Shoreline\tPARK\n",
@@ -52,7 +55,7 @@ public class RegexNERSequenceClassifierTest extends TestCase {
           "\\(|\\)\tPAREN\n",
   };
 
-  static final String[][] expectedNER =
+  private static final String[][] expectedNER =
   {
     { "- - - - - - - - - - - - - - -",
       "- - - - - - - - - - - - - - - -" },
@@ -77,9 +80,9 @@ public class RegexNERSequenceClassifierTest extends TestCase {
   };
 
   public List<List<CoreLabel>> sentences;
-  public List<List<CoreLabel>> NERsentences;
+  private List<List<CoreLabel>> NERsentences;
 
-  @Override
+  @Before
   public void setUp()
     throws IOException
   {
@@ -96,16 +99,16 @@ public class RegexNERSequenceClassifierTest extends TestCase {
       }
     }
 
-    sentences = new ArrayList<List<CoreLabel>>();
-    NERsentences = new ArrayList<List<CoreLabel>>();
-    assertEquals(words.length, tags.length);
-    assertEquals(words.length, ner.length);
+    sentences = new ArrayList<>();
+    NERsentences = new ArrayList<>();
+    Assert.assertEquals(words.length, tags.length);
+    Assert.assertEquals(words.length, ner.length);
     for (int snum = 0; snum < words.length; ++snum) {
       String[] wordPieces = words[snum].split(" ");
       String[] tagPieces = tags[snum].split(" ");
       String[] nerPieces = ner[snum].split(" ");
-      assertEquals(wordPieces.length, tagPieces.length);
-      assertEquals("Input " + snum + " " + words[snum] + " of different length than " + ner[snum], wordPieces.length, nerPieces.length);
+      Assert.assertEquals(wordPieces.length, tagPieces.length);
+      Assert.assertEquals("Input " + snum + " " + words[snum] + " of different length than " + ner[snum], wordPieces.length, nerPieces.length);
       List<CoreLabel> sentence = new ArrayList<>();
       List<CoreLabel> NERsentence = new ArrayList<>();
       for (int wnum = 0; wnum < wordPieces.length; ++wnum) {
@@ -150,53 +153,56 @@ public class RegexNERSequenceClassifierTest extends TestCase {
   }
 
   private static void compareAnswers(String[] expected, List<CoreLabel> sentence) {
-    assertEquals("Lengths different for " + StringUtils.join(expected) + " and " + SentenceUtils.listToString(sentence), expected.length, sentence.size());
+    Assert.assertEquals("Lengths different for " + StringUtils.join(expected) + " and " + SentenceUtils.listToString(sentence), expected.length, sentence.size());
     String str = "Comparing " + Arrays.toString(expected) + " and " + listToString(sentence);
     for (int i = 0; i < expected.length; ++i) {
       if (expected[i].equals("-")) {
-        assertEquals(str, null, sentence.get(i).get(CoreAnnotations.AnswerAnnotation.class));
+        Assert.assertEquals(str, null, sentence.get(i).get(CoreAnnotations.AnswerAnnotation.class));
       } else {
-        assertEquals(str, expected[i],
-                     sentence.get(i).get(CoreAnnotations.AnswerAnnotation.class));
+        Assert.assertEquals(str, expected[i],
+                sentence.get(i).get(CoreAnnotations.AnswerAnnotation.class));
       }
     }
   }
 
+  @Test
   public void testUncased() {
     String tempFilename = tempFile.getPath();
     RegexNERSequenceClassifier uncased =
       new RegexNERSequenceClassifier(tempFilename, true, false);
 
-    assertEquals(sentences.size(), expectedUncased.length);
+    checkSentences(sentences, uncased, expectedUncased);
+  }
+
+  private static void checkSentences(List<List<CoreLabel>> sentences,
+                                     RegexNERSequenceClassifier uncased,
+                                     String[] expectedOutput) {
+    Assert.assertEquals(expectedOutput.length, sentences.size());
     for (int i = 0; i < sentences.size(); ++i) {
       List<CoreLabel> sentence = deepCopy(sentences.get(i));
       uncased.classify(sentence);
-      String[] answers = expectedUncased[i].split(" ");
+      String[] answers = expectedOutput[i].split(" ");
       compareAnswers(answers, sentence);
     }
   }
 
+  @Test
   public void testCased() {
     String tempFilename = tempFile.getPath();
     RegexNERSequenceClassifier cased =
       new RegexNERSequenceClassifier(tempFilename, false, false);
 
-    assertEquals(sentences.size(), expectedCased.length);
-    for (int i = 0; i < sentences.size(); ++i) {
-      List<CoreLabel> sentence = deepCopy(sentences.get(i));
-      cased.classify(sentence);
-      String[] answers = expectedCased[i].split(" ");
-      compareAnswers(answers, sentence);
-    }
+    checkSentences(sentences, cased, expectedCased);
   }
 
+  @Test
   public void testNEROverlaps() {
-    assertEquals(nerPatterns.length, expectedNER.length);
+    Assert.assertEquals(nerPatterns.length, expectedNER.length);
     for (int k = 0; k < nerPatterns.length; k++) {
       BufferedReader r1 = new BufferedReader(new StringReader(nerPatterns[k]));
       RegexNERSequenceClassifier cased =
         new RegexNERSequenceClassifier(r1, false, false, null);
-      assertEquals(NERsentences.size(), expectedNER[k].length);
+      Assert.assertEquals(NERsentences.size(), expectedNER[k].length);
       for (int i = 0; i < NERsentences.size(); ++i) {
         List<CoreLabel> sentence = deepCopy(NERsentences.get(i));
         cased.classify(sentence);
