fe261c845c3951dbfe9de204f040682bf6a27eb3
Christopher Manning <manning@cs.stanford.edu>
2017-07-27 20:59:25 -0700

    Use startsWith, collection argument constructor

--- src/edu/stanford/nlp/tagger/maxent/ExtractorFrames.java
+++ src/edu/stanford/nlp/tagger/maxent/ExtractorFrames.java
@@ -27,10 +27,10 @@
 
 
 package edu.stanford.nlp.tagger.maxent;
-import edu.stanford.nlp.util.logging.Redwood;
 
 import edu.stanford.nlp.process.WordShapeClassifier;
 import edu.stanford.nlp.util.StringUtils;
+import edu.stanford.nlp.util.logging.Redwood;
 
 import java.util.*;
 
@@ -40,6 +40,7 @@ import java.util.*;
  * tag sequences (and interaction terms) for the MaxentTagger, but not the
  * feature extractors explicitly targeting generalization for rare or unknown
  * words.
+ *
  * The following options are supported:
  * <table>
  * <tr><td>Name</td><td>Args</td><td>Effect</td></tr>
@@ -796,9 +797,7 @@ class ExtractorSpanishAuxiliaryTag extends Extractor {
   @Override
   String extract(History h, PairsHolder pH) {
     String tag = super.extract(h, pH);
-    boolean isAux = tag.length() >= 2 && tag.substring(0, 2).equals("va");
-
-    return isAux ? "1" : "0";
+    return tag.startsWith("va") ? "1" : "0";
   }
 
   @Override
@@ -824,9 +823,7 @@ class ExtractorSpanishSemiauxiliaryTag extends Extractor {
   @Override
   String extract(History h, PairsHolder pH) {
     String tag = super.extract(h, pH);
-    boolean isSemiAux = tag.length() >= 2 && tag.substring(0, 2).equals("vs");
-
-    return isSemiAux ? "1" : "0";
+    return tag.startsWith("vs") ? "1" : "0";
   }
 
   @Override
