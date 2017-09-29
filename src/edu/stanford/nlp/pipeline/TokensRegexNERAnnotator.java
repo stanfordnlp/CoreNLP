c9eec64ac5922af9a9b7ad82a85dcbf972e2db04
Jason Bolton <jebolton@stanford.edu>
2017-09-28 20:02:50 -0700

    convert to a SentenceAnnotator

--- src/edu/stanford/nlp/pipeline/TokensRegexNERAnnotator.java
+++ src/edu/stanford/nlp/pipeline/TokensRegexNERAnnotator.java
@@ -126,7 +126,7 @@ import java.util.regex.Pattern;
  *
  * @author Angel Chang
  */
-public class TokensRegexNERAnnotator implements Annotator  {
+public class TokensRegexNERAnnotator extends SentenceAnnotator {
 
   /** A logger for this class */
   protected static final Redwood.RedwoodChannels logger = Redwood.channels(TokensRegexNERAnnotator.class);
@@ -157,6 +157,10 @@ public class TokensRegexNERAnnotator implements Annotator  {
 
   // Labels for which we don't use the default overwrite types (mylabels)
   private final Set<String> noDefaultOverwriteLabels;
+
+  // variables for SentenceAnnotator
+  private final int nThreads;
+  private final long maxParseTime = 0;
 
   enum PosMatchType {
     // all tokens must match the pos pattern
@@ -303,33 +307,10 @@ public class TokensRegexNERAnnotator implements Annotator  {
       Collections.addAll(myLabels, entry.types);
     }
     this.myLabels = Collections.unmodifiableSet(myLabels);
+    this.nThreads = PropertiesUtils.getInt(properties, name + ".nthreads",
+        PropertiesUtils.getInt(properties, "nthreads", 1));
   }
-
-  @Override
-  public void annotate(Annotation annotation) {
-    if (verbose) {
-      logger.info("Adding TokensRegexNER annotations ... ");
-    }
-
-    List<CoreMap> sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);
-    if (sentences != null) {
-      for (CoreMap sentence : sentences) {
-        List<CoreLabel> tokens = sentence.get(CoreAnnotations.TokensAnnotation.class);
-        annotateMatched(tokens);
-      }
-    } else {
-      List<CoreLabel> tokens = annotation.get(CoreAnnotations.TokensAnnotation.class);
-      if (tokens != null){
-        annotateMatched(tokens);
-      } else {
-        throw new RuntimeException("Unable to find sentences or tokens in " + annotation);
-      }
-    }
-
-    if (verbose)
-      logger.info("done.");
-  }
-
+  
   private MultiPatternMatcher<CoreMap> createPatternMatcher(Map<SequencePattern<CoreMap>, Entry> patternToEntry) {
     // Convert to tokensregex pattern
 
@@ -870,6 +851,28 @@ public class TokensRegexNERAnnotator implements Annotator  {
       if (pattern != null) return true;
     }
     return false;
+  }
+
+  @Override
+  protected void doOneSentence(Annotation annotation, CoreMap sentence) {
+    List<CoreLabel> tokens = sentence.get(CoreAnnotations.TokensAnnotation.class);
+    annotateMatched(tokens);
+  }
+
+  @Override
+  protected void doOneFailedSentence(Annotation annotation, CoreMap sentence) {
+    // TODO
+    System.err.println("fail");
+  }
+
+  @Override
+  protected int nThreads() {
+    return nThreads;
+  }
+
+  @Override
+  protected long maxTime() {
+    return maxParseTime;
   }
 
   @Override
