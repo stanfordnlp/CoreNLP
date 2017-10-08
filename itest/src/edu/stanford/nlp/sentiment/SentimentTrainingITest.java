7b775c986148a2957af0b102b43309857d04dc71
Christopher Manning <manning@cs.stanford.edu>
2017-10-07 17:35:53 -0700

    Change to JUnit4

--- itest/src/edu/stanford/nlp/sentiment/SentimentTrainingITest.java
+++ itest/src/edu/stanford/nlp/sentiment/SentimentTrainingITest.java
@@ -1,23 +1,27 @@
 package edu.stanford.nlp.sentiment;
 
-import junit.framework.TestCase;
-
 import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 import edu.stanford.nlp.trees.Tree;
 
 /**
  * Tests that the gradient check for the sentiment tool passes.
  */
-public class SentimentTrainingITest extends TestCase {
-  static final String TRAIN_PATH = "projects/core/data/edu/stanford/nlp/sentiment/onesent.txt";
+public class SentimentTrainingITest {
 
+  private static final String TRAIN_PATH = "projects/core/data/edu/stanford/nlp/sentiment/onesent.txt";
+
+  @Test
   public void testGradientCheck() {
     List<Tree> trainingTrees = SentimentUtils.readTreesWithGoldLabels(TRAIN_PATH);
     RNNOptions op = new RNNOptions();
     op.numHid = 5;
     SentimentModel model = new SentimentModel(op, trainingTrees);
-    assertTrue("Gradient check failed with random seed of " + op.randomSeed, 
-               SentimentTraining.runGradientCheck(model, trainingTrees));
+    Assert.assertTrue("Gradient check failed with random seed of " + op.randomSeed,
+            SentimentTraining.runGradientCheck(model, trainingTrees));
   }
 
   /**
@@ -25,6 +29,7 @@ public class SentimentTrainingITest extends TestCase {
    * total cost, it is important to test those gradients with the reg
    * values turned up a lot.
    */
+  @Test
   public void testRegularizationGradientCheck() {
     List<Tree> trainingTrees = SentimentUtils.readTreesWithGoldLabels(TRAIN_PATH);
     RNNOptions op = new RNNOptions();
@@ -34,8 +39,9 @@ public class SentimentTrainingITest extends TestCase {
     op.trainOptions.regClassification = 10.0;
     op.trainOptions.regWordVector = 10.0;
     SentimentModel model = new SentimentModel(op, trainingTrees);
-    assertTrue("Gradient check failed with random seed of " + op.randomSeed, 
-               SentimentTraining.runGradientCheck(model, trainingTrees));
+    Assert.assertTrue("Gradient check failed with random seed of " + op.randomSeed,
+            SentimentTraining.runGradientCheck(model, trainingTrees));
   }
+
 }
 
