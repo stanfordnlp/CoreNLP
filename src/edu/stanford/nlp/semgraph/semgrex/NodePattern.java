fa272e8d28f829496bce999b9cbecf54a1dc6f39
Christopher Manning <manning@cs.stanford.edu>
2017-10-30 21:42:05 -0700

    Fix a few more similar issues with UNICODE_CASE; closes #533.

--- src/edu/stanford/nlp/semgraph/semgrex/NodePattern.java
+++ src/edu/stanford/nlp/semgraph/semgrex/NodePattern.java
@@ -1,5 +1,4 @@
-package edu.stanford.nlp.semgraph.semgrex; 
-import edu.stanford.nlp.util.logging.Redwood;
+package edu.stanford.nlp.semgraph.semgrex;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -13,11 +12,12 @@ import edu.stanford.nlp.ling.IndexedWord;
 import edu.stanford.nlp.semgraph.SemanticGraph;
 import edu.stanford.nlp.util.Generics;
 import edu.stanford.nlp.util.Pair;
+import edu.stanford.nlp.util.logging.Redwood;
 
 public class NodePattern extends SemgrexPattern  {
 
   /** A logger for this class */
-  private static Redwood.RedwoodChannels log = Redwood.channels(NodePattern.class);
+  private static final Redwood.RedwoodChannels log = Redwood.channels(NodePattern.class);
 
   private static final long serialVersionUID = -5981133879119233896L;
   private GraphRelation reln;
@@ -78,7 +78,7 @@ public class NodePattern extends SemgrexPattern  {
         if (isRegexp) {
           attributes.put(key, Pair.makePair(
               Pattern.compile(patternContent),
-              Pattern.compile(patternContent, Pattern.CASE_INSENSITIVE))
+              Pattern.compile(patternContent, Pattern.CASE_INSENSITIVE|Pattern.UNICODE_CASE))
           );
         } else {
           attributes.put(key, Pair.makePair(patternContent, patternContent));
@@ -214,7 +214,7 @@ public class NodePattern extends SemgrexPattern  {
     }
     sb.append(' ');
     if (reln != null) {
-      sb.append(reln.toString());
+      sb.append(reln);
       sb.append(' ');
     }
     if (!hasPrecedence && addChild && child != null) {
