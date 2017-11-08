aba56814b317e8c519e833afd3f74d4e93d2b4c4
Jason Bolton <jebolton@stanford.edu>
2017-11-08 01:40:14 -0800

    bug fix

--- src/edu/stanford/nlp/pipeline/WordsToSentencesAnnotator.java
+++ src/edu/stanford/nlp/pipeline/WordsToSentencesAnnotator.java
@@ -233,7 +233,6 @@ public class WordsToSentencesAnnotator implements Annotator  {
       // update tokenOffset to first token of next sentence (if there is a next sentence)
       while (tokenOffset < tokens.size()-1 && Boolean.TRUE.equals(tokens.get(tokenOffset).isNewline()))
         tokenOffset++;
-      sentence.set(CoreAnnotations.TokenEndAnnotation.class, tokenOffset);
       sentence.set(CoreAnnotations.SentenceIndexAnnotation.class, sentences.size());
 
       if (countLineNumbers) {
