6704917be41a9dd107ff0d18aeaeb30165809817
Jason Bolton <jebolton@stanford.edu>
2017-11-16 14:06:06 -0800

    attempt to hide Redwood for this test

--- itest/src/edu/stanford/nlp/pipeline/ProtobufAnnotationSerializerSlowITest.java
+++ itest/src/edu/stanford/nlp/pipeline/ProtobufAnnotationSerializerSlowITest.java
@@ -10,7 +10,10 @@ import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations;
 import edu.stanford.nlp.trees.TreeCoreAnnotations;
 import edu.stanford.nlp.util.CoreMap;
 import edu.stanford.nlp.util.Pair;
+import edu.stanford.nlp.util.logging.Redwood;
 import edu.stanford.nlp.util.StringUtils;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.*;
@@ -47,6 +50,11 @@ public class ProtobufAnnotationSerializerSlowITest {
       "It is a truth universally acknowledged, that a single man in possession  of a good fortune, must be in want of a wife. However little known the feelings or views of such a man may be on his  first entering a neighbourhood, this truth is so well fixed in the minds  of the surrounding families, that he is considered the rightful property  of some one or other of their daughters";
   public static final String prideAndPrejudiceFirstSentence =
       "It is a truth universally acknowledged, that a single man in possession  of a good fortune, must be in want of a wife.";
+
+  @Before
+  public void setUp() {
+    Redwood.hideChannelsEverywhere(StanfordCoreNLP.class);
+  }
 
 
   protected Annotation mkAnnotation() {
@@ -513,7 +521,7 @@ public class ProtobufAnnotationSerializerSlowITest {
 
       // Create pipeline
       if (!annotators.contains("dcoref") && !annotators.contains("entitymentions")) {  // TODO(gabor) eventually, don't ignore entitymentions!
-        //System.err.println(">>TESTING " + StringUtils.join(annotators, ","));
+        System.err.println(">>TESTING " + StringUtils.join(annotators, ","));
         testAnnotators(StringUtils.join(annotators, ","));
       }
     }
