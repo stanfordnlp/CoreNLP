06f01ba8437e5f83f78e1e6e498905cd25ad8f71
J38 <jebolton@stanford.edu>
2017-12-16 16:00:50 -0800

    bug fix

--- itest/src/edu/stanford/nlp/ie/crf/TrainCRFClassifierSlowITest.java
+++ itest/src/edu/stanford/nlp/ie/crf/TrainCRFClassifierSlowITest.java
@@ -4,10 +4,14 @@ import junit.framework.TestCase;
 
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.sequences.SeqClassifierFlags;
+import edu.stanford.nlp.util.*;
+import edu.stanford.nlp.util.logging.*;
+import edu.stanford.nlp.io.IOUtils;
 import edu.stanford.nlp.util.StringUtils;
 
 import java.io.*;
 import java.nio.file.*;
+import java.util.List;
 import java.util.Properties;
 
 
@@ -16,9 +20,11 @@ public class TrainCRFClassifierSlowITest extends TestCase {
   public static String crfTrainingWorkingDir =
       "/scr/nlp/data/stanford-corenlp-testing/crf-classifier-training";
 
-  public static String expectedGermanPerformanceLine = "Totals\t0.8408\t0.6752\t0.7489\t3263\t618\t1570";
+  public static String expectedGermanPerformanceLine = "Totals\t0.8416\t0.6760\t0.7497\t3267\t615\t1566";
 
+  
   public void testCRFClassifierTraining() throws Exception {
+    StanfordRedwoodConfiguration.apply(PropertiesUtils.asProperties("log.file", "/scr/nlp/data/stanford-corenlp-testing/german-crf.results"));
     // delete the model if present
     String originalModelPath =
         "/scr/nlp/data/stanford-corenlp-testing/crf-classifier-training/german.hgc_175m_600.crf.ser.gz";
@@ -28,23 +34,13 @@ public class TrainCRFClassifierSlowITest extends TestCase {
     CRFClassifier.main(new String[]{"-props",
         "/scr/nlp/data/stanford-corenlp-testing/crf-classifier-training/german-crf-example-train.prop"});
     // check for lack of quality drop
-    ByteArrayOutputStream germanCRFTestBAOS = new ByteArrayOutputStream();
-    PrintStream germanCRFPrintStream = new PrintStream(germanCRFTestBAOS);
-    PrintStream originalSystemErr = System.err;
-    System.setErr(germanCRFPrintStream);
     CRFClassifier.main(new String[]{"-props",
         "/scr/nlp/data/stanford-corenlp-testing/crf-classifier-training/german-crf-example-test.prop"});
-    System.err.flush();
-    System.setErr(originalSystemErr);
-    String germanTrainingResults = germanCRFTestBAOS.toString();
-    System.out.println("---");
-    System.out.println("German CRF Training Results");
-    System.out.println(germanTrainingResults);
-    String[] resultLines = germanTrainingResults.split("\n");
-    int finalLineLength = resultLines[10].length();
-    System.out.println("last line: "+resultLines[10].substring(70,finalLineLength));
+    List<String> germanTrainingResults = IOUtils.linesFromFile("/scr/nlp/data/stanford-corenlp-testing/german-crf.results");
+    String lastLineOfResults = germanTrainingResults.get(10);
+    System.out.println("last line: "+lastLineOfResults.trim());
     System.out.println("last line: "+expectedGermanPerformanceLine);
-    assertEquals(resultLines[10].substring(70,finalLineLength), expectedGermanPerformanceLine);
+    assertEquals(lastLineOfResults.trim(), expectedGermanPerformanceLine);
   }
 
 }
