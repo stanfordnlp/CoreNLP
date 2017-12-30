2374951c3a525d741d377cc2c0bc57d786be4858
Christopher Manning <manning@cs.stanford.edu>
2017-12-29 16:14:10 -0800

    Remove Javadoc errors

--- src/edu/stanford/nlp/ie/ner/package-info.java
+++ src/edu/stanford/nlp/ie/ner/package-info.java
@@ -1,19 +1,15 @@
 /**
- * <body>
- * <p>
- * This package contains the older version of Stanford NER tagger that uses a
+ * This package contains the older version of the Stanford NER tagger that uses a
  * Conditional Markov Model (a.k.a., Maximum Entropy Markov Model or MEMM)
- * designed for Named Entity Recognition, and various suppport code.
- * For newer version of the Stanford NER tagger that is based on Conditional
- * Random Fields (CRF) models, please see <code>edu.stanford.nlp.ie.crf.CRFClassifier</code>.
+ * designed for Named Entity Recognition, and various support code.
+ * For a newer version of the Stanford NER tagger that is based on Conditional
+ * Random Fields (CRF) models, please see {@code edu.stanford.nlp.ie.crf.CRFClassifier}.
  * The original version was written for the CoNLL 2003 Shared Task competition.
  * The design was considerably generalized, and many more features added for the
  * BioCreative 2003 evaluation.  For basic NER tagging, you should use
- * <code>CMMClassifier</code>.  (Earlier versions still exist as
- * <code>NGramStringClassifier</code> and <code>BioNGramStringClassifier</code>.)
+ * {@code CMMClassifier}.  (Earlier versions still exist as
+ * {@code NGramStringClassifier} and {@code BioNGramStringClassifier}.)
  * The rest of the code in this directory is various remnants of our
  * BioCreative system and various support utilities for NER or CMMClassifier.
- * </p>
- * </body>
  */
 package edu.stanford.nlp.ie.ner;
