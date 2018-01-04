f4a8bff9132aec71396f768a83191613cd9be5dd
Erich Schubert <schubert@informatik.uni-heidelberg.de>
2018-01-03 20:38:28 -0800

    Sometimes, Hotspot benefits a lot from such tiny changes.

--- src/edu/stanford/nlp/tagger/maxent/PairsHolder.java
+++ src/edu/stanford/nlp/tagger/maxent/PairsHolder.java
@@ -117,18 +117,12 @@ public class PairsHolder {
   }
 
   String getWord(History h, int position) {
-    if (((h.current + position) >= h.start) && (h.current + position <= h.end)) {
-      return arr.get(h.current + position).word();
-    } else {
-      return "NA";
-    }
+    final int p = h.current + position;
+    return (p >= h.start && p <= h.end) ? arr.get(p).word() : "NA";
   }
 
   String getTag(History h, int position) {
-    if (((h.current + position) >= h.start) && (h.current + position <= h.end)) {
-      return arr.get(h.current + position).tag();
-    } else {
-      return "NA";
-    }
+    final int p = h.current + position;
+    return (p >= h.start && p <= h.end) ? arr.get(p).tag() : "NA";
   }
 }
