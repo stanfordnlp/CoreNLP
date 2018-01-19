780b3fa5c395b16f3e104aef796fabfac601dcfe
Jason Bolton <jebolton@stanford.edu>
2018-01-19 13:43:54 -0800

    quote needs coref by default now

--- itest/src/edu/stanford/nlp/pipeline/ProtobufAnnotationSerializerSlowITest.java
+++ itest/src/edu/stanford/nlp/pipeline/ProtobufAnnotationSerializerSlowITest.java
@@ -291,7 +291,7 @@ public class ProtobufAnnotationSerializerSlowITest {
     testAnnotators("tokenize,quote");
     testAnnotators("tokenize,ssplit,quote");
     testAnnotators("tokenize,ssplit,quote");
-    testAnnotators("tokenize,ssplit,pos,lemma,ner,quote");
+    testAnnotators("tokenize,ssplit,pos,lemma,ner,depparse,coref,quote");
   }
 
   @Test
