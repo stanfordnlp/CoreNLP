e6808830530a65c847b6075452a1e43d40867042
J38 <jebolton@stanford.edu>
2018-02-08 18:52:07 -0800

    bug fix

--- src/edu/stanford/nlp/util/Pointer.java
+++ src/edu/stanford/nlp/util/Pointer.java
@@ -1,6 +1,5 @@
 package edu.stanford.nlp.util;
 
-import java.io.Serializable;
 import java.util.Optional;
 
 /**
@@ -9,58 +8,22 @@ import java.util.Optional;
  *
  * @author Gabor Angeli
  */
-public class Pointer<T> implements Serializable {
-  /** The serial version uid to ensure stable serialization. */
-  private static final long serialVersionUID = 1L;
+public class Pointer<T> {
 
-  /**
-   * The value the pointer is set to, if it is set.
-   */
-  private T impl;
+  private Optional<T> impl;
 
-  /**
-   * Create a pointer pointing nowhere.
-   */
   public Pointer() {
-    this.impl = null;
+    this.impl = Optional.empty();
   }
 
-  /**
-   * Create a pointer pointing at the given object.
-   *
-   * @param impl The object the pointer is pointing at.
-   */
+  @SuppressWarnings("UnusedDeclaration")
   public Pointer(T impl) {
-    this.impl = impl;
+    this.impl = Optional.of(impl);
   }
 
+  public Optional<T> dereference() { return impl; }
 
-  /**
-   * Dereference the pointer.
-   * If the pointer is pointing somewhere, the {@linkplain Optional optional} will be set.
-   * Otherwise, the optional will be {@linkplain Optional#empty() empty}.
-   */
-  public Optional<T> dereference() {
-    return Optional.ofNullable(impl);
-  }
+  public void set(T impl) { this.impl = Optional.of(impl); }
 
-
-  /**
-   * Set the pointer.
-   *
-   * @param impl The value to set the pointer to. If this is null, the pointer is unset.
-   */
-  public void set(T impl) {
-    this.impl = impl;
-  }
-
-
-  /**
-   * Set the pointer to a possible value.
-   *
-   * @param impl The value to set the pointer to. If this is {@linkplain Optional#empty empty}, the pointer is unset.
-   */
-  public void set(Optional<T> impl) {
-    this.impl = impl.orElse(null);
-  }
+  public void set(Optional<T> impl) { this.impl = impl.isPresent() ? impl : this.impl; }
 }
