a13c4373a5e57d322b143516b493e84f14909fec
Jason Bolton <jebolton@stanford.edu>
2018-02-18 19:12:20 -0800

    fix test

--- test/src/edu/stanford/nlp/pipeline/WordsToSentencesAnnotatorTest.java
+++ test/src/edu/stanford/nlp/pipeline/WordsToSentencesAnnotatorTest.java
@@ -92,7 +92,7 @@ public class WordsToSentencesAnnotatorTest {
     // make sure that there are the correct # of tokens
     // (does NOT contain NL tokens)
     List<CoreLabel> tokens = document1.get(CoreAnnotations.TokensAnnotation.class);
-    assertEquals(15, tokens.size());
+    assertEquals(13, tokens.size());
   }
 
   @Test
@@ -128,7 +128,7 @@ public class WordsToSentencesAnnotatorTest {
 
     // make sure that there are the correct # of tokens (does contain NL tokens)
     List<CoreLabel> tokens = document1.get(CoreAnnotations.TokensAnnotation.class);
-    assertEquals(12, tokens.size());
+    assertEquals(9, tokens.size());
   }
 
   @Test
@@ -149,7 +149,7 @@ public class WordsToSentencesAnnotatorTest {
 
     // make sure that there are the correct # of tokens (does contain NL tokens)
     List<CoreLabel> tokens = document1.get(CoreAnnotations.TokensAnnotation.class);
-    assertEquals(12, tokens.size());
+    assertEquals(9, tokens.size());
 
     List<CoreLabel> sentenceTwoTokens = sentences.get(1).get(CoreAnnotations.TokensAnnotation.class);
     String sentenceTwo = SentenceUtils.listToString(sentenceTwoTokens);
@@ -173,7 +173,7 @@ public class WordsToSentencesAnnotatorTest {
 
     // make sure that there are the correct # of tokens (count does contain NL tokens)
     List<CoreLabel> tokens = document1.get(CoreAnnotations.TokensAnnotation.class);
-    assertEquals(12, tokens.size());
+    assertEquals(9, tokens.size());
 
     for (int i = 0; i < Math.min(sents.length, sentences.size()); i++) {
       CoreMap sentence = sentences.get(i);
