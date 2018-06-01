0d407e47773c422a242d7babcc0b4723d5d8213e
Jason Bolton <jebolton@stanford.edu>
2018-06-01 02:01:36 -0700

    fix path issue

--- itest/src/edu/stanford/nlp/time/HeidelTimeKBPAnnotatorITest.java
+++ itest/src/edu/stanford/nlp/time/HeidelTimeKBPAnnotatorITest.java
@@ -10,7 +10,7 @@ import junit.framework.TestCase;
 public class HeidelTimeKBPAnnotatorITest extends TestCase {
 
   public StanfordCoreNLP pipeline;
-  public String WORKING_DIR = "/scr/nlp/data/stanford-corenlp-testing/spanish-heideltime";
+  public String WORKING_DIR = "/u/scr/nlp/data/stanford-corenlp-testing/spanish-heideltime";
   public Set<String> GOLD_RESULTS = new HashSet<>(
       Arrays.asList("Rusia\tCOUNTRY", "Japón\tCOUNTRY", "hoy\tDATE","rusa\tLOCATION", "Vicente Fox\tPERSON",
           "el 2 de julio de 1942\tDATE", "Esta semana\tDATE", "ING\tORGANIZATION",
