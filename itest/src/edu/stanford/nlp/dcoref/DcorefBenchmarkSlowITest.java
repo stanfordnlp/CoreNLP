74fce336b1e6ddf937f16681e519bf6339e48584
J38 <jebolton@stanford.edu>
2019-01-31 04:36:07 -0800

    update value

--- itest/src/edu/stanford/nlp/dcoref/DcorefBenchmarkSlowITest.java
+++ itest/src/edu/stanford/nlp/dcoref/DcorefBenchmarkSlowITest.java
@@ -149,7 +149,7 @@ public class DcorefBenchmarkSlowITest extends TestCase {
     setLowHighExpected(lowResults, highResults, expectedResults, MUC_TP, 6245, 6255, 6250);
     setLowHighExpected(lowResults, highResults, expectedResults, MUC_F1, 60.65, 60.7, 60.66);
 
-    setLowHighExpected(lowResults, highResults, expectedResults, BCUBED_TP, 12440, 12450, 12445.8);
+    setLowHighExpected(lowResults, highResults, expectedResults, BCUBED_TP, 12440, 12450, 12451.87);
     setLowHighExpected(lowResults, highResults, expectedResults, BCUBED_F1, 70.75, 70.85, 70.80);
 
     setLowHighExpected(lowResults, highResults, expectedResults, CEAFM_TP, 10915, 10930, 10920);
