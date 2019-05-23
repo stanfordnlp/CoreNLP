7e47f574a96f1ff5107826ae1e64222166f912d4
John Bauer <horatio@gmail.com>
2019-05-23 16:31:47 -0700

    Add a small tokens regex example

---
+++ src/edu/stanford/nlp/examples/TokensRegexExample.java
@@ -0,0 +1,30 @@
+package edu.stanford.nlp.examples;
+
+import edu.stanford.nlp.ling.*;
+import edu.stanford.nlp.pipeline.*;
+
+import java.io.File;
+import java.util.Properties;
+
+public class TokensRegexExample {
+
+  public static void main(String[] args) {
+    Properties props = new Properties();
+    props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,tokensregex");
+    // The .../ling/tokensregex/demo directory has a larger example
+    File rules = new File("projects/core/src/edu/stanford/nlp/ling/tokensregex/demo/rules/colors.rules.txt");
+    if (!rules.isFile()) {
+      rules = new File("src/edu/stanford/nlp/ling/tokensregex/demo/rules/colors.rules.txt");
+    }
+    if (!rules.isFile()) {
+      throw new RuntimeException("Unable to find colors.rules.txt");
+    }
+    props.setProperty("tokensregex.rules", rules.getPath());
+    StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+    Annotation ann = new Annotation("My favorite color is green.");
+    pipeline.annotate(ann);
+    for (CoreLabel token : ann.get(CoreAnnotations.TokensAnnotation.class)) {
+      System.out.println(token.word() + " " + token.ner());
+    }
+  }
+}
