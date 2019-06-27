4def95e035677cbf9be142ca97ca0214e9c19b4f
John Bauer <horatio@gmail.com>
2019-06-27 12:46:08 -0700

    Merge branch 'master' of ssh://origin

--- itest/src/edu/stanford/nlp/pipeline/EntityMentionCorefITest.java
+++ itest/src/edu/stanford/nlp/pipeline/EntityMentionCorefITest.java
@@ -3,38 +3,47 @@ package edu.stanford.nlp.pipeline;
 import edu.stanford.nlp.ling.*;
 import edu.stanford.nlp.util.*;
 import java.util.*;
-import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
-public class EntityMentionCorefITest extends TestCase {
+public class EntityMentionCorefITest {
 
-  public String apostropheText = "President Barack Obama's presidency started in 2009. " +
+  private static final String apostropheText = "President Barack Obama's presidency started in 2009. " +
       "Before that Obama defeated John McCain in the 2008 presidential election. " +
       "Barack Obama served two terms.";
 
-  public StanfordCoreNLP pipeline;
+  private StanfordCoreNLP pipeline;
 
-  AnnotationSerializer serializer;
+  // private AnnotationSerializer serializer;
 
-  @Override
+  @Before
   public void setUp() {
     // set up pipeline and serializer
     Properties props = new Properties();
     props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,depparse,coref");
     pipeline = new StanfordCoreNLP(props);
-    serializer = new ProtobufAnnotationSerializer();
+    // serializer = new ProtobufAnnotationSerializer();
   }
 
+  @Test
   public void testApostropheMatch() {
     // build annotation
     Annotation sampleAnnotation = new Annotation(apostropheText);
     // annotate
     pipeline.annotate(sampleAnnotation);
-    // check there is a link
+    // check there is an "Obama" coref link in one direction or the other but be agnostic as to which mention is canonical
+    // System.err.println(sampleAnnotation.get(CoreAnnotations.MentionsAnnotation.class));
     CoreMap entityMention =
-        sampleAnnotation.get(CoreAnnotations.MentionsAnnotation.class).get(3);
+        sampleAnnotation.get(CoreAnnotations.MentionsAnnotation.class).get(3); // gets "Obama" in second (index 1) sentence
     int matchingEntityMentionIndex =
         entityMention.get(CoreAnnotations.CanonicalEntityMentionIndexAnnotation.class);
-    assertEquals(1, matchingEntityMentionIndex);
+    Assert.assertTrue(matchingEntityMentionIndex == 1 || matchingEntityMentionIndex == 3);
+    CoreMap entityMention2 =
+            sampleAnnotation.get(CoreAnnotations.MentionsAnnotation.class).get(1); // gets "Obama" in first (index 0) sentence
+    int matchingEntityMentionIndex2 =
+            entityMention2.get(CoreAnnotations.CanonicalEntityMentionIndexAnnotation.class);
+    Assert.assertTrue(matchingEntityMentionIndex2 == 1 || matchingEntityMentionIndex2 == 3);
   }
 
 }
--- src/edu/stanford/nlp/sequences/package-info.java
+++ src/edu/stanford/nlp/sequences/package-info.java
@@ -1,6 +1,7 @@
 /**
- * <p>This package contains classes that support sequence modeling and
- * inference (CMMs and CRFs).</p>
+ * This package contains classes that support sequence modeling and
+ * inference (CMMs and CRFs).
+ *
  * @author Teg Grenager (grenager@stanford.edu)
  * @author Jenny Finkel
  */
