cfba52b1fe1e51b5f8a48258f4904209a7fde192
John Bauer <horatio@gmail.com>
2019-06-27 12:46:07 -0700

    Fix a CC attachment

--- itest/src/edu/stanford/nlp/pipeline/EntityMentionCorefITest.java
+++ itest/src/edu/stanford/nlp/pipeline/EntityMentionCorefITest.java
@@ -3,47 +3,38 @@ package edu.stanford.nlp.pipeline;
 import edu.stanford.nlp.ling.*;
 import edu.stanford.nlp.util.*;
 import java.util.*;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import junit.framework.TestCase;
 
-public class EntityMentionCorefITest {
+public class EntityMentionCorefITest extends TestCase {
 
-  private static final String apostropheText = "President Barack Obama's presidency started in 2009. " +
+  public String apostropheText = "President Barack Obama's presidency started in 2009. " +
       "Before that Obama defeated John McCain in the 2008 presidential election. " +
       "Barack Obama served two terms.";
 
-  private StanfordCoreNLP pipeline;
+  public StanfordCoreNLP pipeline;
 
-  // private AnnotationSerializer serializer;
+  AnnotationSerializer serializer;
 
-  @Before
+  @Override
   public void setUp() {
     // set up pipeline and serializer
     Properties props = new Properties();
     props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,depparse,coref");
     pipeline = new StanfordCoreNLP(props);
-    // serializer = new ProtobufAnnotationSerializer();
+    serializer = new ProtobufAnnotationSerializer();
   }
 
-  @Test
   public void testApostropheMatch() {
     // build annotation
     Annotation sampleAnnotation = new Annotation(apostropheText);
     // annotate
     pipeline.annotate(sampleAnnotation);
-    // check there is an "Obama" coref link in one direction or the other but be agnostic as to which mention is canonical
-    // System.err.println(sampleAnnotation.get(CoreAnnotations.MentionsAnnotation.class));
+    // check there is a link
     CoreMap entityMention =
-        sampleAnnotation.get(CoreAnnotations.MentionsAnnotation.class).get(3); // gets "Obama" in second (index 1) sentence
+        sampleAnnotation.get(CoreAnnotations.MentionsAnnotation.class).get(3);
     int matchingEntityMentionIndex =
         entityMention.get(CoreAnnotations.CanonicalEntityMentionIndexAnnotation.class);
-    Assert.assertTrue(matchingEntityMentionIndex == 1 || matchingEntityMentionIndex == 3);
-    CoreMap entityMention2 =
-            sampleAnnotation.get(CoreAnnotations.MentionsAnnotation.class).get(1); // gets "Obama" in first (index 0) sentence
-    int matchingEntityMentionIndex2 =
-            entityMention2.get(CoreAnnotations.CanonicalEntityMentionIndexAnnotation.class);
-    Assert.assertTrue(matchingEntityMentionIndex2 == 1 || matchingEntityMentionIndex2 == 3);
+    assertEquals(1, matchingEntityMentionIndex);
   }
 
 }
