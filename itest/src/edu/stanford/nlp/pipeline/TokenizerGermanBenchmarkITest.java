e45f8ce366c6f3a14e5613411107f31fb3d400c8
J38 <jebolton@stanford.edu>
2019-07-08 22:26:55 -0700

    update German numbers

--- itest/src/edu/stanford/nlp/pipeline/TokenizerGermanBenchmarkITest.java
+++ itest/src/edu/stanford/nlp/pipeline/TokenizerGermanBenchmarkITest.java
@@ -18,12 +18,12 @@ public class TokenizerGermanBenchmarkITest extends TokenizerBenchmarkTestCase {
 
     public void testOnDev() {
         goldFilePath = "/u/nlp/data/stanford-corenlp/test/data/tokenize/de_gsd-ud-dev.conllu";
-        runTest("dev", "de", 0.95);
+        runTest("dev", "de", 0.986);
     }
 
     public void testOnTest() {
         goldFilePath = "/u/nlp/data/stanford-corenlp/test/data/tokenize/de_gsd-ud-test.conllu";
-        runTest("test", "de", 0.95);
+        runTest("test", "de", 0.977);
     }
 
 }
