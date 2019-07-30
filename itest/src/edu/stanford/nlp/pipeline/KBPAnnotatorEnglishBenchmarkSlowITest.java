82b69a773252bdecdba84ff3ef8e0314e7d3ac4f
J38 <jebolton@stanford.edu>
2019-07-29 18:30:53 -0700

    fix English KBP score in test

--- itest/src/edu/stanford/nlp/pipeline/KBPAnnotatorEnglishBenchmarkSlowITest.java
+++ itest/src/edu/stanford/nlp/pipeline/KBPAnnotatorEnglishBenchmarkSlowITest.java
@@ -12,7 +12,7 @@ public class KBPAnnotatorEnglishBenchmarkSlowITest extends KBPAnnotatorBenchmark
     // set the English specific settings
     KBP_DOCS_DIR = "/u/scr/nlp/data/kbp-benchmark/kbp-docs";
     GOLD_RELATIONS_PATH = "/u/scr/nlp/data/kbp-benchmark/kbp-gold-relations.txt";
-    KBP_MINIMUM_SCORE = .445;
+    KBP_MINIMUM_SCORE = .456;
     docIDToText = new HashMap<String,String>();
     docIDToRelations = new HashMap<String,Set<String>>();
     // load the gold relations from gold relations file
