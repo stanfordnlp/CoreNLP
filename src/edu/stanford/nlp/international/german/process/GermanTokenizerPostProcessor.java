1d5474b7260d64998972890e17dcf6553c339160
J38 <jebolton@stanford.edu>
2019-09-14 15:09:07 -0700

    add number range handling, refactor some

--- src/edu/stanford/nlp/international/german/process/GermanTokenizerPostProcessor.java
+++ src/edu/stanford/nlp/international/german/process/GermanTokenizerPostProcessor.java
@@ -7,9 +7,18 @@ import java.util.*;
 import java.util.function.*;
 
 /**
- * German tokenization is handled by PTBLexer.  This module handles ordinals.
+ * German tokenization is handled by PTBLexer.  This module handles special cases for German.
+ * Currently the German tokenization in Stanford CoreNLP should match the UD standard, as
+ * seen in the CoNLL-2018 dependency parsing data for example.
+ *
+ * Two special cases for now:
+ *   - rebuild ordinals
+ *   - rebuild numeric ranges
+ *
  * For instance in the sentence "Der Vertrag läuft offiziell bis zum 31. Dezember 1992."
  * the "31." should be a token.  This module will merge "31" and "." back together.
+ *
+ * Also "1989" "-" "1990" should be merged back to "1989-1990"
  */
 
 public class GermanTokenizerPostProcessor extends CoreLabelProcessor {
@@ -35,20 +44,28 @@ public class GermanTokenizerPostProcessor extends CoreLabelProcessor {
   @Override
   public List<CoreLabel> process(List<CoreLabel> tokens) {
     List<CoreLabel> processedTokens = new ArrayList<CoreLabel>();
+    // process the first token
     if (tokens.size() > 0)
       processedTokens.add(tokens.get(0));
+    // now always guaranteed one token on list of processed tokens
     for (int i = 1 ; i < tokens.size() ; i++) {
       CoreLabel currToken = tokens.get(i);
-      CoreLabel processedToken = new CoreLabel(currToken);
-      CoreLabel lastProcessedToken =
-          processedTokens.size() > 0 ? processedTokens.get(processedTokens.size() - 1) : null;
-      boolean nextTokenPredictsOrdinal =
-          i+1 < tokens.size() ? ordinalPredictingWords.contains(tokens.get(i+1).word()) : false ;
-      if (lastProcessedToken != null && afterIsEmpty.apply(lastProcessedToken) && currToken.word().equals(".") &&
-          nextTokenPredictsOrdinal) {
+      CoreLabel lastProcessedToken = processedTokens.get(processedTokens.size()-1);
+      // check last processed token has "" as after()
+      boolean emptyAfter = afterIsEmpty.apply(lastProcessedToken);
+      // perform ordinal merge (e.g. "21" "." "Dezember" -> "21." "Dezember")
+      boolean ordinalMerge = lastProcessedToken.word().matches("[0-9]+") && currToken.word().equals(".")
+          && i+1 < tokens.size() && ordinalPredictingWords.contains(tokens.get(i+1).word());
+      // perform number to hyphen merge (e.g. "1989" "-" "1990" -> "1989-" "1990")
+      boolean numberToHyphenMerge = lastProcessedToken.word().matches("[0-9]+") &&
+          currToken.word().equals("-") && i+1 < tokens.size() && tokens.get(i+1).word().matches("[0-9]+");
+      // perform hyphen to number merge (e.g. "1989-" "1990" -> "1989-1990")
+      boolean hyphenToNumberMerge = lastProcessedToken.word().matches("[0-9]+-") &&
+          currToken.word().matches("[0-9]+");
+      if (emptyAfter && (ordinalMerge || numberToHyphenMerge || hyphenToNumberMerge)) {
         mergeTokens(lastProcessedToken, currToken);
       } else {
-        processedTokens.add(processedToken);
+        processedTokens.add(currToken);
       }
     }
     return processedTokens;
