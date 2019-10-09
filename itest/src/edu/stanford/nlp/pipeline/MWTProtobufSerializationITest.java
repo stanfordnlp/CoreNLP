14badd7c525378e298cb51f3e279134921e5a857
J38 <jebolton@stanford.edu>
2019-10-08 19:53:41 -0700

    cleanup

--- itest/src/edu/stanford/nlp/pipeline/MWTProtobufSerializationITest.java
+++ itest/src/edu/stanford/nlp/pipeline/MWTProtobufSerializationITest.java
@@ -19,19 +19,6 @@ public class MWTProtobufSerializationITest extends TestCase {
 
   AnnotationSerializer serializer;
 
-  public void findCoreMapDifference(CoreMap originalCoreMap, CoreMap readCoreMap) {
-    if (!originalCoreMap.get(CoreAnnotations.TokensAnnotation.class).equals(
-        readCoreMap.get(CoreAnnotations.TokensAnnotation.class)))
-      System.err.println("tokens annotation difference detected!");
-    if (!originalCoreMap.get(CoreAnnotations.TextAnnotation.class).equals(
-        readCoreMap.get(CoreAnnotations.TextAnnotation.class)))
-      System.err.println("text annotation difference detected!");
-    if (!originalCoreMap.get(CoreAnnotations.SentencesAnnotation.class).equals(
-        readCoreMap.get(CoreAnnotations.SentencesAnnotation.class)))
-      System.err.println("sentences annotation difference detected!");
-  }
-
-
   @Override
   public void setUp() {
     // set up pipeline and serializer
@@ -53,13 +40,6 @@ public class MWTProtobufSerializationITest extends TestCase {
     pair.second.close();
     Annotation readAnnotation = pair.first;
     kis.close();
-    //findCoreMapDifference(sampleDocument.annotation(), readAnnotation);
-    /*for (int i = 0 ; i < readAnnotation.get(CoreAnnotations.TokensAnnotation.class).size() ; i++) {
-      if (!readAnnotation.get(CoreAnnotations.TokensAnnotation.class).get(i).equals(
-          sampleDocument.tokens().get(i))) {
-        System.out.println(sampleDocument.tokens().get(i));
-      }
-    }*/
     ProtobufAnnotationSerializerSlowITest.sameAsRead(sampleDocument.annotation(), readAnnotation);
   }
 
