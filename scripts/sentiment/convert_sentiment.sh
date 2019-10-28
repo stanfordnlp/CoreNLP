d6c917029e9e85626d01a9a9c38c98945524d2ac
John Bauer <horatio@gmail.com>
2019-10-28 16:29:05 -0700

    Add a script for converting sentiment trees to text files

---
+++ scripts/sentiment/convert_sentiment.sh
@@ -0,0 +1,44 @@
+# Convert five class sentiment data into text files of the type you
+# might use a sentence classifier for.
+
+# The default directories are what I set up on my local machine.
+# -i and -o change the input and output dirs.
+
+INPUT_DIR=extern_data/sentiment/sentiment-treebank
+OUTPUT_DIR=extern_data/sentiment/sst-processed
+
+while getopts "i:o:" OPTION
+do
+  case $OPTION in 
+  i)
+    INPUT_DIR=$OPTARG	    
+    ;;
+  o)
+    OUTPUT_DIR=$OPTARG	    
+    ;;
+  esac
+done
+
+  
+echo INPUT DIR: $INPUT_DIR
+echo OUTPUT DIR: $OUTPUT_DIR
+
+mkdir -p $OUTPUT_DIR/fiveclass
+
+echo $OUTPUT_DIR/fiveclass/train-phrases.txt
+java edu.stanford.nlp.trees.OutputSubtrees -input $INPUT_DIR/fiveclass/train.txt > $OUTPUT_DIR/fiveclass/train-phrases.txt
+
+echo $OUTPUT_DIR/fiveclass/dev-phrases.txt
+java edu.stanford.nlp.trees.OutputSubtrees -input $INPUT_DIR/fiveclass/dev.txt > $OUTPUT_DIR/fiveclass/dev-phrases.txt
+
+echo $OUTPUT_DIR/fiveclass/test-phrases.txt
+java edu.stanford.nlp.trees.OutputSubtrees -input $INPUT_DIR/fiveclass/test.txt > $OUTPUT_DIR/fiveclass/test-phrases.txt
+
+
+echo $OUTPUT_DIR/fiveclass/dev-roots.txt
+java edu.stanford.nlp.trees.OutputSubtrees -input $INPUT_DIR/fiveclass/dev.txt  -root_only > $OUTPUT_DIR/fiveclass/dev-roots.txt
+
+echo $OUTPUT_DIR/fiveclass/test-roots.txt
+java edu.stanford.nlp.trees.OutputSubtrees -input $INPUT_DIR/fiveclass/test.txt -root_only > $OUTPUT_DIR/fiveclass/test-roots.txt
+
+
