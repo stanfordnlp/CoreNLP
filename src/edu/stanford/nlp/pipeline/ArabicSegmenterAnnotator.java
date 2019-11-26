037ede23e64503e17aac3b831bc24bc38c073ad9
John Bauer <horatio@gmail.com>
2019-11-26 12:05:41 -0800

    Add the capability to ArabicSegmenterAnnotator to preserve newlines or double newlines for the sentence splitter

--- src/edu/stanford/nlp/pipeline/ArabicSegmenterAnnotator.java
+++ src/edu/stanford/nlp/pipeline/ArabicSegmenterAnnotator.java
@@ -1,13 +1,16 @@
 package edu.stanford.nlp.pipeline;
 
 import java.util.*;
+import java.util.regex.Pattern;
 
 import edu.stanford.nlp.international.arabic.process.ArabicSegmenter;
 import edu.stanford.nlp.ling.CoreAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.process.AbstractTokenizer;
 import edu.stanford.nlp.util.CoreMap;
 import edu.stanford.nlp.util.PropertiesUtils;
+import edu.stanford.nlp.util.StringUtils;
 import edu.stanford.nlp.util.logging.Redwood;
 
 /**
@@ -30,6 +33,8 @@ public class ArabicSegmenterAnnotator implements Annotator  {
 
   private ArabicSegmenter segmenter;
   private final boolean VERBOSE;
+  private final boolean tokenizeNewline;
+  private final boolean sentenceSplitOnTwoNewlines;
 
   private static final String DEFAULT_SEG_LOC =
     "/u/nlp/data/arabic-segmenter/arabic-segmenter-atb+bn+arztrain.ser.gz";
@@ -46,6 +51,8 @@ public class ArabicSegmenterAnnotator implements Annotator  {
     VERBOSE = verbose;
     Properties props = new Properties();
     loadModel(segLoc, props);
+    tokenizeNewline = false;
+    sentenceSplitOnTwoNewlines = false;
   }
 
   public ArabicSegmenterAnnotator(String name, Properties props) {
@@ -69,6 +76,21 @@ public class ArabicSegmenterAnnotator implements Annotator  {
       throw new RuntimeException("Expected a property " + name + ".model");
     }
     loadModel(model, modelProps);
+
+    // TODO: unify with ChineseSegmenterAnnotator somehow?
+    // The issue here is the Chinese segmenter returns text chunks and
+    // the Arabic segmenter has a method which returns CoreLabels, so
+    // the project of unifying the two into one ur-SegmenterAnnotator
+    // is larger than simply ripping some code into a superclass and
+    // calling it a day
+
+    // If newlines are treated as sentence split, we need to retain them in tokenization for ssplit to make use of them
+    tokenizeNewline = (!props.getProperty(StanfordCoreNLP.NEWLINE_IS_SENTENCE_BREAK_PROPERTY, "never").equals("never"))
+            || Boolean.valueOf(props.getProperty(StanfordCoreNLP.NEWLINE_SPLITTER_PROPERTY, "false"));
+
+    // record whether or not sentence splitting on two newlines ; if so, need to remove single newlines
+    sentenceSplitOnTwoNewlines =
+        props.getProperty(StanfordCoreNLP.NEWLINE_IS_SENTENCE_BREAK_PROPERTY, "never").equals("two");
   }
 
   @SuppressWarnings("unused")
@@ -114,9 +136,62 @@ public class ArabicSegmenterAnnotator implements Annotator  {
     }
   }
 
+  private static final String NEWLINE_REGEX = "\\R";
+  private static final Pattern NEWLINE_PATTERN = Pattern.compile(NEWLINE_REGEX);
+
+  private CoreLabel makeNewlineCoreLabel(String piece, int stringConsumed) {
+    // TODO: do we need to set other stuff like PositionAnnotations, etc?
+    CoreLabel newline = new CoreLabel();
+    newline.setWord(AbstractTokenizer.NEWLINE_TOKEN);
+    newline.setValue(AbstractTokenizer.NEWLINE_TOKEN);
+    newline.set(CoreAnnotations.OriginalTextAnnotation.class, piece);
+    newline.set(CoreAnnotations.CharacterOffsetBeginAnnotation.class, stringConsumed);
+    newline.set(CoreAnnotations.CharacterOffsetEndAnnotation.class, stringConsumed + piece.length());
+    return newline;
+  }
+
   private void doOneSentence(CoreMap annotation) {
     String text = annotation.get(CoreAnnotations.TextAnnotation.class);
-    List<CoreLabel> tokens = segmenter.segmentStringToTokenList(text);
+    List<CoreLabel> tokens;
+    if (!tokenizeNewline) {
+      tokens = segmenter.segmentStringToTokenList(text);
+    } else {
+      tokens = new ArrayList<>();
+      List<String> pieces = StringUtils.splitLinesKeepNewlines(text);
+      int stringConsumed = 0;
+      boolean sawNewline = false;
+      boolean sawTwoNewlines = false;
+      for (String piece : pieces) {
+        if (NEWLINE_PATTERN.matcher(piece).matches()) {
+          if (!sawNewline) {
+            // Found a newline we care about
+            tokens.add(makeNewlineCoreLabel(piece, stringConsumed));
+            sawNewline = true;
+          } else if (sentenceSplitOnTwoNewlines && !sawTwoNewlines) {
+            // Guess we care about this one two
+            // Others we skip
+            tokens.add(makeNewlineCoreLabel(piece, stringConsumed));
+            sawTwoNewlines = true;
+          }
+        } else {
+          sawNewline = false;
+          sawTwoNewlines = false;
+          List<CoreLabel> pieceTokens = segmenter.segmentStringToTokenList(piece);
+          for (CoreLabel label : pieceTokens) {
+            // TODO: what about updating other annotations?
+            // The javadoc for TokenBeginAnnotation is incredibly useless.
+            // PositionAnnotation doesn't have any documentation at all.
+            // It's not clear which is worse
+            label.set(CoreAnnotations.CharacterOffsetBeginAnnotation.class,
+                      label.get(CoreAnnotations.CharacterOffsetBeginAnnotation.class) + stringConsumed);
+            label.set(CoreAnnotations.CharacterOffsetEndAnnotation.class,
+                      label.get(CoreAnnotations.CharacterOffsetEndAnnotation.class) + stringConsumed);
+          }
+          tokens.addAll(pieceTokens);
+        }
+        stringConsumed = stringConsumed + piece.length();
+      }
+    }
     annotation.set(CoreAnnotations.TokensAnnotation.class, tokens);
   }
 
