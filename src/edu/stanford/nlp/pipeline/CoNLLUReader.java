a16933dacf25356453624c2932bd5160367ce27b
J38 <jebolton@stanford.edu>
2020-01-22 22:39:51 -0800

    revert to older version so test passes

--- src/edu/stanford/nlp/pipeline/CoNLLUReader.java
+++ src/edu/stanford/nlp/pipeline/CoNLLUReader.java
@@ -13,8 +13,6 @@ import java.util.regex.*;
 
 /**
  * A class for reading in CoNLL-U data and creating Annotations.
- *
- * @author Jason Bolton
  */
 
 public class CoNLLUReader {
@@ -38,7 +36,7 @@ public class CoNLLUReader {
    **/
   public static Pattern COMMENT_LINE = Pattern.compile("^#.*");
   public static Pattern DOCUMENT_LINE = Pattern.compile("^# newdoc");
-  public static Pattern MWT_LINE = Pattern.compile("^[0-9]+-[0-9]+.*");
+  public static Pattern MWT_LINE = Pattern.compile("^[0-9]+\\-[0-9]+.*");
   public static Pattern TOKEN_LINE = Pattern.compile("^[0-9]+\t.*");
 
   /**
@@ -78,7 +76,7 @@ public class CoNLLUReader {
    * <p>
    * conllu.extraColumns = CoreAnnotations.TrueCaseAnnotation,CoreAnnotations.CategoryAnnotation
    */
-  private HashMap<Integer, Class> extraColumns = new HashMap<>();
+  public HashMap<Integer, Class> extraColumns = new HashMap<>();
 
   public CoNLLUReader() throws ClassNotFoundException {
     this(new Properties());
@@ -91,7 +89,7 @@ public class CoNLLUReader {
     } else {
       int extraColumnIndex = 10;
       for (String className : props.getProperty("conllu.extraColumns").split(",")) {
-        if (classShorthandToFull.containsKey(className))
+        if (classShorthandToFull.keySet().contains(className))
           className = classShorthandToFull.get(className) + className;
         Class clazz = Class.forName(className);
         extraColumns.put(extraColumnIndex, clazz);
@@ -141,13 +139,13 @@ public class CoNLLUReader {
     // data for the sentence contained in # key values
     public HashMap<String, String> sentenceData = new HashMap<>();
     // map indices in token list to mwt data if there is any
-    HashMap<Integer, Integer> mwtData = new HashMap<>();
+    public HashMap<Integer, Integer> mwtData = new HashMap<>();
     // mwt tokens
-    List<String> mwtTokens = new ArrayList<>();
+    public List<String> mwtTokens = new ArrayList<>();
     // mwt misc info
-    List<String> mwtMiscs = new ArrayList<>();
+    public List<String> mwtMiscs = new ArrayList<>();
     // indexes of last CoreLabel for each MWT
-    List<Integer> mwtLastCoreLabels = new ArrayList<>();
+    public List<Integer> mwtLastCoreLabels = new ArrayList<>();
 
     /**
      * Process line for current sentence.  Return true if processing empty line (indicating sentence end)
@@ -179,7 +177,7 @@ public class CoNLLUReader {
     /**
      * Add mwt data for this mwt line
      **/
-    void addMWTData(String mwtDataLine) {
+    public void addMWTData(String mwtDataLine) {
       String[] mwtFields = mwtDataLine.split("\t");
       String[] mwtRange = mwtFields[CoNLLU_IndexField].split("-");
       String mwtText = mwtFields[CoNLLU_WordField];
@@ -210,7 +208,9 @@ public class CoNLLUReader {
     List<String> conllXLines = new ArrayList<String>();
     for (CoNLLUDocument doc : docs) {
       for (CoNLLUSentence sentence : doc.sentences) {
-        conllXLines.addAll(sentence.tokenLines);
+        for (String tokenLine : sentence.tokenLines) {
+          conllXLines.add(tokenLine);
+        }
         // add a blank line between sentences
         conllXLines.add("");
       }
@@ -252,7 +252,7 @@ public class CoNLLUReader {
    * one sentence per line, separated by newline.
    **/
   public Annotation convertCoNLLUDocumentToAnnotation(CoNLLUDocument doc) {
-    Annotation finalAnnotation = new Annotation(doc.docText);
+    Annotation finalAnnotation = new Annotation();
     // build sentences
     List<CoreMap> sentences = new ArrayList<>();
     for (CoNLLUSentence sent : doc.sentences) {
@@ -281,6 +281,7 @@ public class CoNLLUReader {
       }
       sentenceIdx++;
     }
+    finalAnnotation.set(CoreAnnotations.TextAnnotation.class, doc.docText);
     return finalAnnotation;
   }
 
@@ -309,7 +310,7 @@ public class CoNLLUReader {
       }
       cl.setIndex(sentenceTokenIndex);
 
-      /*
+      /**
        * analyze MISC field for this token
        *
        * MISC should be a "|" separated list in the final column
@@ -321,9 +322,10 @@ public class CoNLLUReader {
        * - SpaceAfter (e.g. No if next token is punctuation mark)
        *
        */
+
       if (!fields.get(CoNLLU_MiscField).equals("_")) {
         HashMap<String, String> miscKeyValues = new HashMap<>();
-        Arrays.stream(fields.get(CoNLLU_MiscField).split("\\|")).forEach(
+        Arrays.asList(fields.get(CoNLLU_MiscField).split("\\|")).stream().forEach(
             kv -> miscKeyValues.put(kv.split("=")[0], kv.split("=")[1]));
         // unless SpaceAfter=No, add a space after this token
         if (!miscKeyValues.getOrDefault("SpaceAfter", "Yes").equals("No")) {
@@ -425,12 +427,15 @@ public class CoNLLUReader {
     }
     // build SemanticGraph
     SemanticGraph depParse = SemanticGraphFactory.makeFromEdges(graphEdges);
-    // build sentence CoreMap with full text
-    Annotation sentenceCoreMap = new Annotation(doc.docText.substring(sentenceCharBegin).trim());
+    // build sentence CoreMap
+    Annotation sentenceCoreMap = new Annotation();
     // add tokens
     sentenceCoreMap.set(CoreAnnotations.TokensAnnotation.class, coreLabels);
     // add dependency graph
     sentenceCoreMap.set(SemanticGraphCoreAnnotations.BasicDependenciesAnnotation.class, depParse);
+    // add full text
+    String sentenceText = doc.docText.substring(sentenceCharBegin).trim();
+    sentenceCoreMap.set(CoreAnnotations.TextAnnotation.class, sentenceText);
     return sentenceCoreMap;
   }
 
