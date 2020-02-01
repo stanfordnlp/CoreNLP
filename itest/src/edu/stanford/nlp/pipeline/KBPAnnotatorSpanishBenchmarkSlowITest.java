df715ff7c32a7cd5e7cfecd808c0fcaee161fd41
J38 <jebolton@stanford.edu>
2020-01-31 21:51:52 -0800

    update spanish benchmark

--- itest/src/edu/stanford/nlp/pipeline/KBPAnnotatorSpanishBenchmarkSlowITest.java
+++ itest/src/edu/stanford/nlp/pipeline/KBPAnnotatorSpanishBenchmarkSlowITest.java
@@ -10,7 +10,7 @@ public class KBPAnnotatorSpanishBenchmarkSlowITest extends KBPAnnotatorBenchmark
     // set the English specific settings
     KBP_DOCS_DIR = "/u/nlp/data/kbp-resources/benchmark/spanish/kbp-docs-spanish";
     GOLD_RELATIONS_PATH = "/u/nlp/data/kbp-resources/benchmark/spanish/kbp-gold-relations-spanish.txt";
-    KBP_MINIMUM_SCORE = .3348;
+    KBP_MINIMUM_SCORE = .3425;
     // load the gold relations from gold relations file
     loadGoldData();
     // set up the pipeline
