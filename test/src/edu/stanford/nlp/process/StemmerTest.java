9fed217d84e0432a3bee06c66f6d896f1fb2c2d6
Jan von der Assen <jan.vda@gmx.ch>
2020-03-19 10:25:08 +0100

    Replace wildcard imports since they are disregarded in the project

--- test/src/edu/stanford/nlp/process/StemmerTest.java
+++ test/src/edu/stanford/nlp/process/StemmerTest.java
@@ -1,6 +1,6 @@
 package edu.stanford.nlp.process;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
--- test/src/edu/stanford/nlp/process/StopListTest.java
+++ test/src/edu/stanford/nlp/process/StopListTest.java
@@ -1,6 +1,6 @@
 package edu.stanford.nlp.process;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
