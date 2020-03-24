4f770ff920b830dc6950169c0f8c9c3b40c4b0c9
John Bauer <horatio@gmail.com>
2020-03-24 12:30:04 -0700

    Let a user of SegDemo specify their own dict location

--- src/edu/stanford/nlp/wordseg/demo/SegDemo.java
+++ src/edu/stanford/nlp/wordseg/demo/SegDemo.java
@@ -31,6 +31,7 @@ public class SegDemo {
 
   private static final String basedir = System.getProperty("basedir", "data");
   private static final String MODEL = System.getProperty("model", basedir + "/ctb.gz");
+  private static final String DICT = System.getProperty("dict", basedir + "/dict-chris6.ser.gz");
 
   public static void main(String[] args) throws Exception {
     System.setOut(new PrintStream(System.out, true, "utf-8"));
@@ -40,7 +41,7 @@ public class SegDemo {
     // props.setProperty("NormalizationTable", "data/norm.simp.utf8");
     // props.setProperty("normTableEncoding", "UTF-8");
     // below is needed because CTBSegDocumentIteratorFactory accesses it
-    props.setProperty("serDictionary", basedir + "/dict-chris6.ser.gz");
+    props.setProperty("serDictionary", DICT);
     if (args.length > 0) {
       props.setProperty("testFile", args[0]);
     }
