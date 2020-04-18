564c8eaad8ebe7481eaaaa917562b9b79d54c4a0
RinorSefa <rinorsefa@hotmail.com>
2020-04-18 22:37:03 +0200

    removed code smells of indirect testing

--- test/src/edu/stanford/nlp/ling/CategoryWordTagFactoryTest.java
+++ test/src/edu/stanford/nlp/ling/CategoryWordTagFactoryTest.java
@@ -5,10 +5,6 @@ import junit.framework.TestCase;
 public class CategoryWordTagFactoryTest extends TestCase {
   public void testCopy() {
     CategoryWordTag tag = new CategoryWordTag("A", "B", "C");
-    assertEquals("A", tag.category());
-    assertEquals("B", tag.word());
-    assertEquals("C", tag.tag());
-
     CategoryWordTagFactory lf = new CategoryWordTagFactory();
     Label label = lf.newLabel(tag);
     assertTrue(label instanceof CategoryWordTag);
