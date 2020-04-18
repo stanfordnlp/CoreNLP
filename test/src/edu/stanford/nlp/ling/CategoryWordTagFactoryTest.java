e58520ad6c87570efbdcdb3271686ce2fa92538d
RinorSefa <rinorsefa@hotmail.com>
2020-04-18 22:42:41 +0200

    removed code smells of eagerTest by extracting method in CategoryWordTagFactoryTest

--- test/src/edu/stanford/nlp/ling/CategoryWordTagFactoryTest.java
+++ test/src/edu/stanford/nlp/ling/CategoryWordTagFactoryTest.java
@@ -1,6 +1,7 @@
 package edu.stanford.nlp.ling;
 
 import junit.framework.TestCase;
+import org.junit.Test;
 
 public class CategoryWordTagFactoryTest extends TestCase {
   public void testCopy() {
@@ -12,5 +13,13 @@ public class CategoryWordTagFactoryTest extends TestCase {
     assertEquals("A", tag2.category());
     assertEquals("B", tag2.word());
     assertEquals("C", tag2.tag());
+  }
+
+  @Test
+  public void testNewLabelMethodReturnsInstanceOfCategoryWordTag(){
+      CategoryWordTag tag = new CategoryWordTag("A", "B", "C");
+      CategoryWordTagFactory lf = new CategoryWordTagFactory();
+      Label label = lf.newLabel(tag);
+      assertTrue(label instanceof CategoryWordTag);
   }
 }
