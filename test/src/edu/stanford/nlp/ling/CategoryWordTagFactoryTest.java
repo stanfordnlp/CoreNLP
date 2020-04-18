e41eab182b47a83619d4af930c10301290607c06
RinorSefa <rinorsefa@hotmail.com>
2020-04-18 23:04:37 +0200

    removed code smells of Indirect Testing! in CategoryWordTagFactoryTest class.  Method gets tested at its own class

--- test/src/edu/stanford/nlp/ling/CategoryWordTagFactoryTest.java
+++ test/src/edu/stanford/nlp/ling/CategoryWordTagFactoryTest.java
@@ -1,25 +1,14 @@
 package edu.stanford.nlp.ling;
 
 import junit.framework.TestCase;
-import org.junit.Test;
 
 public class CategoryWordTagFactoryTest extends TestCase {
-  public void testCopy() {
+
+  public void testNewLabelMethodReturnsInstanceOfCategoryWordTag(){
     CategoryWordTag tag = new CategoryWordTag("A", "B", "C");
     CategoryWordTagFactory lf = new CategoryWordTagFactory();
     Label label = lf.newLabel(tag);
-    assertTrue(label instanceof CategoryWordTag);
-    CategoryWordTag tag2 = (CategoryWordTag) label;
-    assertEquals("A", tag2.category());
-    assertEquals("B", tag2.word());
-    assertEquals("C", tag2.tag());
-  }
 
-  @Test
-  public void testNewLabelMethodReturnsInstanceOfCategoryWordTag(){
-      CategoryWordTag tag = new CategoryWordTag("A", "B", "C");
-      CategoryWordTagFactory lf = new CategoryWordTagFactory();
-      Label label = lf.newLabel(tag);
-      assertTrue(label instanceof CategoryWordTag);
+    assertTrue(label instanceof CategoryWordTag);
   }
 }
