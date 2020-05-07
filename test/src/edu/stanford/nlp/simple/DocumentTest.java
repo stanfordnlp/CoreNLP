eb058068ca72be723df41c2f4b3730e818974713
Jan von der Assen <jan.vda@gmx.ch>
2020-05-07 13:14:38 -0700

    Refactor the text of a document used more than once

--- test/src/edu/stanford/nlp/simple/DocumentTest.java
+++ test/src/edu/stanford/nlp/simple/DocumentTest.java
@@ -1,11 +1,13 @@
 package edu.stanford.nlp.simple;
 
 import org.junit.Test;
+import org.junit.Before;
 
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
 
 /**
  * A test for aspects of {@link edu.stanford.nlp.simple.Document} which do not require loading the NLP models.
@@ -13,33 +15,40 @@ import static org.junit.Assert.*;
  * @author Gabor Angeli
  */
 public class DocumentTest {
+  Document oneSentenceDocument;
+  Document twoSentenceDocument;
+
+  @Before
+  public void prepareDocuments() {
+    oneSentenceDocument = new Document("the quick brown fox jumped over the lazy dog");
+    twoSentenceDocument = new Document("the quick brown fox jumped over the lazy dog. The lazy dog was not impressed.");
+  }
 
   @Test
   public void testCreateFromText() {
-    Document doc = new Document("the quick brown fox jumped over the lazy dog");
-    assertNotNull(doc);
+    assertNotNull(oneSentenceDocument);
+    assertNotNull(twoSentenceDocument);
   }
 
   @Test
   public void testText() {
-    Document doc = new Document("the quick brown fox jumped over the lazy dog");
+    Document doc = oneSentenceDocument;
     assertEquals("the quick brown fox jumped over the lazy dog", doc.text());
   }
 
   @Test
   public void testDocid() {
-    Document doc = new Document("the quick brown fox jumped over the lazy dog");
+    Document doc = oneSentenceDocument;
     assertEquals(Optional.<String>empty(), doc.docid());
     assertEquals(Optional.of("foo"), doc.setDocid("foo").docid());
   }
 
   @Test
   public void testSentences() {
-    Document doc = new Document("the quick brown fox jumped over the lazy dog. The lazy dog was not impressed.");
+    Document doc = twoSentenceDocument;
     List<Sentence> sentences = doc.sentences();
     assertEquals(2, sentences.size());
     assertEquals("the quick brown fox jumped over the lazy dog.", sentences.get(0).text());
     assertEquals("The lazy dog was not impressed.", sentences.get(1).text());
   }
-
 }
