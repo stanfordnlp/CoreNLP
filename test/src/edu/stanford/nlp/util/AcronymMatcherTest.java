ac543c58544f9f020ad4b438a3435a248c73e586
RinorSefa <rinorsefa@hotmail.com>
2020-06-04 12:46:18 +0200

    Possible bug at AcronymMatcher.isAcronym

--- test/src/edu/stanford/nlp/util/AcronymMatcherTest.java
+++ test/src/edu/stanford/nlp/util/AcronymMatcherTest.java
@@ -14,7 +14,6 @@ public class AcronymMatcherTest {
 
   @Test
   public void testBasic() {
-    assertTrue(AcronymMatcher.isAcronym("B", "B".split("\\s+")));
     assertTrue(AcronymMatcher.isAcronym("IBM", "International Business Machines".split("\\s+")));
     assertTrue(AcronymMatcher.isAcronym("SIWI", "Stockholm International Water Institute".split("\\s+")));
     assertTrue(AcronymMatcher.isAcronym("CBRC", "China Banking Regulatory Commission".split("\\s+")));
@@ -25,6 +24,13 @@ public class AcronymMatcherTest {
   public void testFilterStopWords() {
     assertTrue(AcronymMatcher.isAcronym("CML", "Council of Mortgage Lenders".split("\\s+")));
     assertTrue(AcronymMatcher.isAcronym("AAAS", "American Association for the Advancement of Science".split("\\s+")));
+  }
+
+  @Test
+  public void testIsAcronymWithBoundaries() {
+    assertFalse(AcronymMatcher.isAcronym("B", "".split("\\s+")));
+    assertTrue(AcronymMatcher.isAcronym("B", "B".split("\\s+")));
+    assertFalse(AcronymMatcher.isAcronym("", "B".split("\\s+")));
   }
 
   @Test
