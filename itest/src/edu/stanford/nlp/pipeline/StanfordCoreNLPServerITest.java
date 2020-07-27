f11714957d5026ab098be9e901a231bf9bc2acb4
John Bauer <horatio@gmail.com>
2020-07-26 18:57:59 -0700

    Fix this test

--- itest/src/edu/stanford/nlp/pipeline/StanfordCoreNLPServerITest.java
+++ itest/src/edu/stanford/nlp/pipeline/StanfordCoreNLPServerITest.java
@@ -139,7 +139,7 @@ public class StanfordCoreNLPServerITest {
    */
   @Test
   public void testSemgrexFilter() throws IOException {
-    String expected="{ \"sentences\": [ true, false ]}";
+    String expected="{ \"sentences\": [ true, false ]}".replaceAll(" ", "");
 
     String query = "The dog ate a fish.  He went home.";
     byte[] message = query.getBytes("utf-8");
@@ -150,7 +150,6 @@ public class StanfordCoreNLPServerITest {
                                        URLEncoder.encode(PropertiesUtils.propsAsJsonString(props), "utf-8"));
     URL serverURL = new URL("http", "localhost", port, "/semgrex?" + queryParams);
     String response = postURL(serverURL, message);
-    System.out.println(response);
 
     Assert.assertEquals(expected, response.replaceAll(" ", "").replaceAll("\n", ""));
   }
