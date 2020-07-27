c2105b5c6b1d4594999445e1f00067b5465c1708
John Bauer <horatio@gmail.com>
2020-07-26 18:57:57 -0700

    Add a test for the existing filter method for the semgrex requests

--- itest/src/edu/stanford/nlp/pipeline/StanfordCoreNLPServerITest.java
+++ itest/src/edu/stanford/nlp/pipeline/StanfordCoreNLPServerITest.java
@@ -132,4 +132,26 @@ public class StanfordCoreNLPServerITest {
 
     Assert.assertEquals(expected, response.replaceAll(" ", "").replaceAll("\n", ""));
   }
+
+
+  /**
+   * The first sentence should match this query, but the second one should not.
+   */
+  @Test
+  public void testSemgrexFilter() throws IOException {
+    String expected="{ \"sentences\": [ true, false ]}";
+
+    String query = "The dog ate a fish.  He went home.";
+    byte[] message = query.getBytes("utf-8");
+    Properties props = new Properties();
+    props.setProperty("annotators", "tokenize,ssplit,pos,parse");
+    String queryParams = String.format("pattern=%s&properties=%s&filter=true",
+                                       URLEncoder.encode("{}=verb >obj {}=obj"),
+                                       URLEncoder.encode(PropertiesUtils.propsAsJsonString(props), "utf-8"));
+    URL serverURL = new URL("http", "localhost", port, "/semgrex?" + queryParams);
+    String response = postURL(serverURL, message);
+    System.out.println(response);
+
+    Assert.assertEquals(expected, response.replaceAll(" ", "").replaceAll("\n", ""));
+  }
 }
