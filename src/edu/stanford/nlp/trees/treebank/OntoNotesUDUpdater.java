f7480ab8f7f71f80a1c99ddc9056e006445ff30e
John Bauer <horatio@gmail.com>
2020-10-22 00:34:30 -0700

    Rearrange a couple pieces of OntoNotes trees which are in the wrong place

--- src/edu/stanford/nlp/trees/treebank/OntoNotesUDUpdater.java
+++ src/edu/stanford/nlp/trees/treebank/OntoNotesUDUpdater.java
@@ -2,6 +2,8 @@ package edu.stanford.nlp.trees.treebank;
 
 import edu.stanford.nlp.trees.*;
 import edu.stanford.nlp.trees.tregex.*;
+import edu.stanford.nlp.trees.tregex.tsurgeon.*;
+import edu.stanford.nlp.util.Pair;
 
 import java.io.*;
 import java.util.*;
@@ -16,6 +18,17 @@ public class OntoNotesUDUpdater {
 
   /** Trees to filter out **/
   public static TregexPattern badLabelsPattern = TregexPattern.compile("/X|EDITED|EMBED/ < /.*/");
+
+  public static List<Pair<TregexPattern, TsurgeonPattern>> updates = new ArrayList<>();
+  static {
+    // some OntoNotes sentences have `` outside the enclosing S
+    updates.add(new Pair<>(TregexPattern.compile("__ !> __ <1 /``/=bad <2 S=good"),
+                           Tsurgeon.parseOperation("[move bad >1 good]")));
+    // also the final punct sometimes is outside the proper subtree
+    updates.add(new Pair<>(TregexPattern.compile("__ !> __ <2 /[.]/=bad <1 /S|SQ|SINV/=good"),
+                           Tsurgeon.parseOperation("[move bad >-1 good]")));
+  }
+
   public static TregexPattern substitutionLabelsPattern =
       TregexPattern.compile("/-LCB-|-RCB-|-LSB-|-RSB-|TOP/ < /.*/");
 
@@ -40,12 +53,17 @@ public class OntoNotesUDUpdater {
     Tree t = tr.readTree();
     while(t != null) {
       if (!badLabelsPattern.matcher(t).find()) {
+        for (Pair<TregexPattern, TsurgeonPattern> update : updates) {
+          t = Tsurgeon.processPattern(update.first, update.second, t);
+        }
+
         TregexMatcher replacementTreesMatcher = substitutionLabelsPattern.matcher(t);
         while (replacementTreesMatcher.find()) {
           Tree replacementTree = replacementTreesMatcher.getMatch();
           String newLabel = labelSubstitutions.get(replacementTree);
           replacementTree.label().setValue(newLabel);
         }
+
         System.out.println(t);
       }
       t = tr.readTree();
