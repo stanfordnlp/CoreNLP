b0df783f2190062cd6a3aa0f2ace2a6b5f9bec69
John Bauer <horatio@gmail.com>
2020-11-20 09:35:06 -0800

    Add a convenience constructor

--- src/edu/stanford/nlp/io/RegExFileFilter.java
+++ src/edu/stanford/nlp/io/RegExFileFilter.java
@@ -14,6 +14,10 @@ public class RegExFileFilter implements FileFilter {
 
   private final Pattern pattern;
 
+  public RegExFileFilter(String string) {
+    this(Pattern.compile(string));
+  }
+
   /**
    * Sets up a RegExFileFilter which checks if the file name (not the
    * entire path) matches the passed in {@link java.util.regex.Pattern}.
