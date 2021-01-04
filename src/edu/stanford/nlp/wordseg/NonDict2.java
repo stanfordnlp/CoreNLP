7680af5a5ecaaa3e948627d04ee9bbc9b3c71bb6
John Bauer <horatio@gmail.com>
2021-01-04 09:43:50 -0800

    Make the NonDict2 threadsafe

--- src/edu/stanford/nlp/wordseg/NonDict2.java
+++ src/edu/stanford/nlp/wordseg/NonDict2.java
@@ -8,36 +8,32 @@ import edu.stanford.nlp.sequences.SeqClassifierFlags;
 public class NonDict2  {
 
   //public String sighanCorporaDict = "/u/nlp/data/chinese-segmenter/";
-  public String corporaDict = "/u/nlp/data/gale/segtool/stanford-seg/data/";
-  private static CorpusDictionary cd = null;
+  public static final String DEFAULT_HOME = "/u/nlp/data/gale/segtool/stanford-seg/data/";
+  public final String corporaDict;
+  private final CorpusDictionary cd;
 
   private static Redwood.RedwoodChannels logger = Redwood.channels(NonDict2.class);
 
   public NonDict2(SeqClassifierFlags flags) {
-    if (cd == null) {
-
-      if (flags.sighanCorporaDict != null) {
-        corporaDict = flags.sighanCorporaDict; // use the same flag for Sighan 2005,
-        // but our list is extracted from ctb
-      }
-      String path;
-      if (flags.useAs || flags.useHk || flags.useMsr) {
-        throw new RuntimeException("only support settings for CTB and PKU now.");
-      } else if ( flags.usePk ) {
-        path = corporaDict+"/dict/pku.non";
-      } else { // CTB
-        path = corporaDict+"/dict/ctb.non";
-      }
-
-      cd = new CorpusDictionary(path);
-      // just output the msg...
-      if (flags.useAs || flags.useHk || flags.useMsr) {
-      } else if ( flags.usePk ) {
-        logger.info("INFO: flags.usePk=true | building NonDict2 from "+path);
-      } else { // CTB
-        logger.info("INFO: flags.usePk=false | building NonDict2 from "+path);
-      }
+    if (flags.sighanCorporaDict != null) {
+      corporaDict = flags.sighanCorporaDict; // use the same flag for Sighan 2005,
+      // but our list is extracted from ctb
+    } else {
+      corporaDict = DEFAULT_HOME;
     }
+
+    String path;
+    if (flags.useAs || flags.useHk || flags.useMsr) {
+      throw new RuntimeException("only support settings for CTB and PKU now.");
+    } else if ( flags.usePk ) {
+      path = corporaDict+"/dict/pku.non";
+      logger.info("INFO: flags.usePk=true | building NonDict2 from "+path);
+    } else { // CTB
+      path = corporaDict+"/dict/ctb.non";
+      logger.info("INFO: flags.usePk=false | building NonDict2 from "+path);
+    }
+
+    cd = new CorpusDictionary(path);
   }
 
   public String checkDic(String c2, SeqClassifierFlags flags) {
