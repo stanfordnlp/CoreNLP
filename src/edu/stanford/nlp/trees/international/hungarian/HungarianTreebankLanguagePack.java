a863b6ca2d8e239895d6f364a6100ef2bafd0eef
John Bauer <horatio@gmail.com>
2021-06-04 10:17:05 -0700

    TLP & TBPP for Hungarian
    Includes a TreeNormalizer which completely strips the subcategories when reading the SPMRL treebank
    
    Uses a right headfinder instead of a left

---
+++ src/edu/stanford/nlp/trees/international/hungarian/HungarianTreebankLanguagePack.java
@@ -0,0 +1,67 @@
+package edu.stanford.nlp.trees.international.hungarian;
+
+import edu.stanford.nlp.trees.AbstractTreebankLanguagePack;
+import edu.stanford.nlp.trees.HeadFinder;
+import edu.stanford.nlp.trees.LeftHeadFinder;
+
+/**
+ * Treebank language pack suitable for the Hungarian section of SPMRL
+ */
+public class HungarianTreebankLanguagePack extends AbstractTreebankLanguagePack {
+
+  private static final long serialVersionUID = -7982635612452142L;
+
+  // both sentence final and mid-sentence punctuation use PUNC
+  // the UD tagger will redo the tags to be PUNCT
+  private static final String[] punctTags = { "PUNC", "PUNCT" };
+
+  private static final String[] punctWords = { "!", "\"", "&", "'", "§", "(", ")", "+", ",", "-", ".", "...", "/", "—", ":", ";", "==", "?" };
+  
+  private static final String[] startSymbols = { "ROOT" };
+
+  private static final String[] SFPunctWords = {".", "!", "?"};
+
+  @Override
+  public String[] punctuationTags() {
+    return punctTags;
+  }
+
+  @Override
+  public String[] punctuationWords() {
+    return punctWords;
+  }
+
+  @Override
+  public String[] sentenceFinalPunctuationTags() {
+    return punctTags;
+  }
+
+  @Override
+  public String[] sentenceFinalPunctuationWords() {
+    return SFPunctWords;
+  }
+
+  @Override
+  public String[] startSymbols() {
+    return startSymbols;
+  }
+  
+  /** {@inheritDoc} */
+  @Override
+  public String treebankFileExtension() {
+    return "ptb";
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public HeadFinder headFinder() {
+    return new LeftHeadFinder();
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public HeadFinder typedDependencyHeadFinder() {
+    return new LeftHeadFinder();
+  }
+
+}
