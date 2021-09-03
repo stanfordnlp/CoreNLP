64bf2ade8618cefb9d39aef99dc982c485a2b7e0
yuliangfan <yf3@illinois.edu>
2021-09-03 00:24:40 -0500

    try to solve flaky test on utils.CollectionValuedMapTest.java

--- test/src/edu/stanford/nlp/util/CollectionValuedMapTest.java
+++ test/src/edu/stanford/nlp/util/CollectionValuedMapTest.java
@@ -160,13 +160,18 @@ public class CollectionValuedMapTest {
   @Test
   public void testAddRemove() {
     CollectionValuedMap<Integer, Integer> fooMap = new CollectionValuedMap<>();
+    CollectionValuedMap<Integer, Integer> tmp = new CollectionValuedMap<>();
     for (int i = 0; i < 4; i++) {
       for (int j = 0; j < 4; j++) {
         fooMap.add(new Integer(i), new Integer(j));
+        if (i!=2){
+          tmp.add(new Integer(i), new Integer(j));
+        }
       }
     }
     fooMap.remove(new Integer(2));
-    Assert.assertEquals("{0=[0, 1, 2, 3], 1=[0, 1, 2, 3], 3=[0, 1, 2, 3]}", fooMap.toString());
+    Assert.assertEquals(true, fooMap.equals(tmp)); 
+    // Assert.assertEquals("{0=[0, 1, 2, 3], 1=[0, 1, 2, 3], 3=[0, 1, 2, 3]}", fooMap.toString());
   }
 
   /**
