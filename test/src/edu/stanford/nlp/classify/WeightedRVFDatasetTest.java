2d91585135e4a589bd92bcba0754b80f8ba84e47
Christopher Manning <manning@cs.stanford.edu>
2021-09-18 21:33:04 -0700

    Convert 2 tests to junit4

--- test/src/edu/stanford/nlp/classify/WeightedRVFDatasetTest.java
+++ test/src/edu/stanford/nlp/classify/WeightedRVFDatasetTest.java
@@ -1,39 +1,45 @@
 package edu.stanford.nlp.classify;
 
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 import edu.stanford.nlp.ling.RVFDatum;
 import edu.stanford.nlp.stats.Counters;
-import junit.framework.TestCase;
 
-import java.util.Arrays;
 
 /**
  * TODO(gabor) JavaDoc
  *
  * @author Gabor Angeli
  */
-public class WeightedRVFDatasetTest extends TestCase {
+public class WeightedRVFDatasetTest {
 
+  @Test
   public void testWeightingWorks() {
     WeightedRVFDataset<String, String> dataset = new WeightedRVFDataset<>();
     RVFDatum<String, String> datum1 = newRVFDatum(null, "a", "b", "a");
     dataset.add(datum1, 42.0f);
     RVFDatum<String, String> datum2 = newRVFDatum(null, "a", "b", "a");
     dataset.add(datum2, 7.3f);
-    assertEquals(42.0f, dataset.getWeights()[0], 1e-10);
-    assertEquals(7.3f, dataset.getWeights()[1], 1e-10);
+    Assert.assertEquals(42.0f, dataset.getWeights()[0], 1e-10);
+    Assert.assertEquals(7.3f, dataset.getWeights()[1], 1e-10);
   }
 
+  @Test
   public void testBackwardsCompatibility() {
-    RVFDataset<String, String> dataset = new WeightedRVFDataset<>();
+    WeightedRVFDataset<String, String> dataset = new WeightedRVFDataset<>();
     RVFDatum<String, String> datum1 = newRVFDatum(null, "a", "b", "a");
     dataset.add(datum1);
     RVFDatum<String, String> datum2 = newRVFDatum(null, "a", "b", "a");
     dataset.add(datum2);
-    assertEquals(1.0f, ((WeightedRVFDataset<String, String>) dataset).getWeights()[0], 1e-10);
-    assertEquals(1.0f, ((WeightedRVFDataset<String, String>) dataset).getWeights()[1], 1e-10);
+    Assert.assertEquals(1.0f, dataset.getWeights()[0], 1e-10);
+    Assert.assertEquals(1.0f, dataset.getWeights()[1], 1e-10);
   }
 
 
+  @Test
   public void testMixedCompatibility() {
     WeightedRVFDataset<String, String> dataset = new WeightedRVFDataset<>();
     RVFDatum<String, String> datum1 = newRVFDatum(null, "a", "b", "a");
@@ -42,13 +48,15 @@ public class WeightedRVFDatasetTest extends TestCase {
     dataset.add(datum2);
     RVFDatum<String, String> datum3 = newRVFDatum(null, "a", "b", "a");
     dataset.add(datum3, 7.3f);
-    assertEquals(42.0f, dataset.getWeights()[0], 1e-10);
-    assertEquals(1.0f, dataset.getWeights()[1], 1e-10);
-    assertEquals(7.3f, dataset.getWeights()[2], 1e-10);
+    Assert.assertEquals(42.0f, dataset.getWeights()[0], 1e-10);
+    Assert.assertEquals(1.0f, dataset.getWeights()[1], 1e-10);
+    Assert.assertEquals(7.3f, dataset.getWeights()[2], 1e-10);
   }
 
 
+  @SafeVarargs
   private static <L, F> RVFDatum<L, F> newRVFDatum(L label, F ... items) {
     return new RVFDatum<>(Counters.asCounter(Arrays.asList(items)), label);
   }
+
 }
