8be896bd958a3e2efa071f657d8c1be441dc1e38
J38 <jebolton@stanford.edu>
2021-10-02 19:30:44 -0700

    revert scores

--- itest/src/edu/stanford/nlp/pipeline/POSTaggerBenchmarkITest.java
+++ itest/src/edu/stanford/nlp/pipeline/POSTaggerBenchmarkITest.java
@@ -69,13 +69,13 @@ public class POSTaggerBenchmarkITest extends TestCase {
   public void testGermanDevPOS()  throws IOException {
     runPOSTest("edu/stanford/nlp/models/pos-tagger/german-ud.tagger",
             String.format("format=TSV,wordColumn=1,tagColumn=3,%s/depparser/nn/models-4.0.0/data/clean/de_gsd-ud-dev.conllu.clean", TestPaths.testHome()),
-            95.37);
+            93.07);
   }
 
   public void testGermanTestPOS()  throws IOException {
     runPOSTest("edu/stanford/nlp/models/pos-tagger/german-ud.tagger",
             String.format("format=TSV,wordColumn=1,tagColumn=3,%s/depparser/nn/models-4.0.0/data/clean/de_gsd-ud-test.conllu.clean", TestPaths.testHome()),
-            92.82);
+            92.84);
   }
 
   public void testSpanishDevPOS()  throws IOException {
