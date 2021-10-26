c96a7365417f708d3150b7481842a57432f92521
Auzel <filey6@hotmail.com>
2021-10-26 00:23:27 -0500

    Fix flaky test in testConnectedComponents

--- test/src/edu/stanford/nlp/graph/DirectedMultiGraphTest.java
+++ test/src/edu/stanford/nlp/graph/DirectedMultiGraphTest.java
@@ -153,13 +153,16 @@ public class DirectedMultiGraphTest extends TestCase {
   public void testConnectedComponents() {
 
     System.out.println("graph is " + graph.toString());
-    List<Set<Integer>> ccs = graph.getConnectedComponents();
+    Set<Set<Integer>> ccs = new HashSet<>(graph.getConnectedComponents());
     for (Set<Integer> cc : ccs) {
       System.out.println("Connected component: " + cc);
-    }
-    assertEquals(ccs.size(), 4);
-    assertEquals(CollectionUtils.sorted(ccs.get(0)),
-                 Arrays.asList(1, 2, 3, 4));
+    } 
+    Set<Integer> edge1 = new HashSet<>(Arrays.asList(1, 2, 3, 4));
+    Set<Integer> edge2 = new HashSet<>(Arrays.asList(5, 6, 7));
+    Set<Integer> edge3 = new HashSet<>(Arrays.asList(8));
+    Set<Integer> edge4 = new HashSet<>(Arrays.asList(9,10));
+    Set<Set<Integer>> expectedCcs = new HashSet<>(Arrays.asList(edge1,edge2,edge3,edge4));
+    assertEquals(expectedCcs, ccs);
   }
 
   public void testEdgesNodes() {
