d4caf43eae40fb0937cd4ca06c76804959ca3e39
James Zow <jameszow@163.com>
2022-01-21 11:08:52 +0800

    Update StanfordCoreNLPChineseTestApp.java

--- examples/sample-maven-project/src/main/java/edu/stanford/nlp/StanfordCoreNLPChineseTestApp.java
+++ examples/sample-maven-project/src/main/java/edu/stanford/nlp/StanfordCoreNLPChineseTestApp.java
@@ -1,7 +1,6 @@
 package edu.stanford.nlp;
 
 import java.util.*;
-
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.*;
 
@@ -13,10 +12,8 @@ public class StanfordCoreNLPChineseTestApp {
     public static void main(String[] args) {
         Properties properties = new Properties();
         properties.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,parse,coref,kbp,entitylink");
-
         StanfordCoreNLP pipeline = new StanfordCoreNLP(properties);
         CoreDocument document = pipeline.processToCoreDocument("巴拉克·奥巴马是美国总统。他在2008年当选");
-
         for (CoreLabel tok : document.tokens()) {
             System.out.println(tok.word() + ":" + tok.tag() + "\n");
         }
