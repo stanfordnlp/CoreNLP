b4349fcb0cb94a4bfd30e710482ca3791bf31c4a
John Bauer <horatio@gmail.com>
2022-03-31 19:59:50 -0700

    Add an ith leaf relation to tregex

--- test/src/edu/stanford/nlp/trees/tregex/TregexTest.java
+++ test/src/edu/stanford/nlp/trees/tregex/TregexTest.java
@@ -1081,6 +1081,18 @@ public class TregexTest extends TestCase {
     runTest("A <<< (b . c=foo) <<< =foo", "(ROOT (A (B b) (C c)))", "(A (B b) (C c))");
   }
 
+  public void testAncestorOfIthLeaf() {
+    runTest("A <<<1 b", "(ROOT (A (B b)))", "(A (B b))");
+    runTest("A <<<2 b", "(ROOT (A (B b)))");
+    runTest("A <<<-1 b", "(ROOT (A (B b)))", "(A (B b))");
+    runTest("A <<<1 b", "(ROOT (A (B z) (C b)))");
+    runTest("A <<<2 b", "(ROOT (A (B z) (C b)))", "(A (B z) (C b))");
+    runTest("A <<<-1 b", "(ROOT (A (B z) (C b)))", "(A (B z) (C b))");
+    runTest("A <<<-2 b", "(ROOT (A (B z) (C b)))");
+    runTest("A <<<-1 z", "(ROOT (A (B z) (C b)))");
+    runTest("A <<<-2 z", "(ROOT (A (B z) (C b)))", "(A (B z) (C b))");
+  }
+
   public void testHeadOfPhrase() {
     runTest("NP <# NNS", "(NP (NN work) (NNS practices))", "(NP (NN work) (NNS practices))");
     runTest("NP <# NN", "(NP (NN work) (NNS practices))"); // should have no results
