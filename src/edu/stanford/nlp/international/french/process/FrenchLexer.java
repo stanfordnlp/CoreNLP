cd4e49f909009c586715e51253d78b40fa0f0f9d
Christopher Manning <manning@cs.stanford.edu>
2022-07-17 17:56:54 -0700

    Correct checked in java lexer to match jflex file [oops]

--- src/edu/stanford/nlp/international/french/process/FrenchLexer.java
+++ src/edu/stanford/nlp/international/french/process/FrenchLexer.java
@@ -12458,7 +12458,7 @@ class FrenchLexer {
 
 
     /** Turn on to find out how things were tokenized. */
-    private static final boolean DEBUG = true;
+    private static final boolean DEBUG = false;
 
     /** A logger for this class */
     private static final Redwood.RedwoodChannels logger = Redwood.channels(FrenchLexer.class);
