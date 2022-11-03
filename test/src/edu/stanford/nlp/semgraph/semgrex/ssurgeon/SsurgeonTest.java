23a8952aa00e7c07f1ba59e31907c1f0eff77d10
John Bauer <horatio@gmail.com>
2022-11-02 23:00:33 -0700

    Align some whitespace for better readability in Ssurgeon

--- test/src/edu/stanford/nlp/semgraph/semgrex/ssurgeon/SsurgeonTest.java
+++ test/src/edu/stanford/nlp/semgraph/semgrex/ssurgeon/SsurgeonTest.java
@@ -14,48 +14,48 @@ import edu.stanford.nlp.trees.EnglishGrammaticalRelations;
 
 public class SsurgeonTest {
 
-	/**
-	 * Simple test of an Ssurgeon edit script.  This instances a simple semantic graph,
-	 * a semgrex pattern, and then the resulting actions over the named nodes in the
-	 * semgrex match.
-	 */
-	@Test
-	public void simpleTest() throws Exception {
-		SemanticGraph sg = SemanticGraph.valueOf("[mixed/VBN nsubj>[Joe/NNP appos>[bartender/NN det>the/DT]]  obj>[drink/NN det>a/DT]]");
-		SemgrexPattern semgrexPattern = SemgrexPattern.compile("{}=a1 >appos=e1 {}=a2 <nsubj=e2 {}=a3");
-		SsurgeonPattern pattern = new SsurgeonPattern(semgrexPattern);
+  /**
+   * Simple test of an Ssurgeon edit script.  This instances a simple semantic graph,
+   * a semgrex pattern, and then the resulting actions over the named nodes in the
+   * semgrex match.
+   */
+  @Test
+  public void simpleTest() throws Exception {
+    SemanticGraph sg = SemanticGraph.valueOf("[mixed/VBN nsubj>[Joe/NNP appos>[bartender/NN det>the/DT]]  obj>[drink/NN det>a/DT]]");
+    SemgrexPattern semgrexPattern = SemgrexPattern.compile("{}=a1 >appos=e1 {}=a2 <nsubj=e2 {}=a3");
+    SsurgeonPattern pattern = new SsurgeonPattern(semgrexPattern);
 
-		System.out.println("Start = "+sg.toCompactString());
+    System.out.println("Start = "+sg.toCompactString());
 
-		// Find and snip the appos and root to nsubj links
-		SsurgeonEdit apposSnip = new RemoveNamedEdge("e1", "a1", "a2");
-		pattern.addEdit(apposSnip);
+    // Find and snip the appos and root to nsubj links
+    SsurgeonEdit apposSnip = new RemoveNamedEdge("e1", "a1", "a2");
+    pattern.addEdit(apposSnip);
 
-		SsurgeonEdit nsubjSnip = new RemoveNamedEdge("e2", "a3", "a1");
-		pattern.addEdit(nsubjSnip);
+    SsurgeonEdit nsubjSnip = new RemoveNamedEdge("e2", "a3", "a1");
+    pattern.addEdit(nsubjSnip);
 
-		// Attach Joe to be the nsubj of bartender
-		SsurgeonEdit reattachSubj = new AddEdge("a2", "a1", EnglishGrammaticalRelations.NOMINAL_SUBJECT);
-		pattern.addEdit(reattachSubj);
+    // Attach Joe to be the nsubj of bartender
+    SsurgeonEdit reattachSubj = new AddEdge("a2", "a1", EnglishGrammaticalRelations.NOMINAL_SUBJECT);
+    pattern.addEdit(reattachSubj);
 
-		// Attach copula
-		IndexedWord isNode = new IndexedWord();
-		isNode.set(CoreAnnotations.TextAnnotation.class, "is");
-		isNode.set(CoreAnnotations.LemmaAnnotation.class, "is");
-		isNode.set(CoreAnnotations.OriginalTextAnnotation.class, "is");
-		isNode.set(CoreAnnotations.PartOfSpeechAnnotation.class, "VBN");
-		SsurgeonEdit addCopula = new AddDep("a2", EnglishGrammaticalRelations.COPULA, isNode);
-		pattern.addEdit(addCopula);
+    // Attach copula
+    IndexedWord isNode = new IndexedWord();
+    isNode.set(CoreAnnotations.TextAnnotation.class, "is");
+    isNode.set(CoreAnnotations.LemmaAnnotation.class, "is");
+    isNode.set(CoreAnnotations.OriginalTextAnnotation.class, "is");
+    isNode.set(CoreAnnotations.PartOfSpeechAnnotation.class, "VBN");
+    SsurgeonEdit addCopula = new AddDep("a2", EnglishGrammaticalRelations.COPULA, isNode);
+    pattern.addEdit(addCopula);
 
-		// Destroy subgraph
-		SsurgeonEdit destroySubgraph = new DeleteGraphFromNode("a3");
-		pattern.addEdit(destroySubgraph);
+    // Destroy subgraph
+    SsurgeonEdit destroySubgraph = new DeleteGraphFromNode("a3");
+    pattern.addEdit(destroySubgraph);
 
-		// Process and output modified
-		Collection<SemanticGraph> newSgs = pattern.execute(sg);
-		for (SemanticGraph newSg : newSgs)
-			System.out.println("Modified = "+newSg.toCompactString());
-		String firstGraphString = newSgs.iterator().next().toCompactString().trim();
-		assertEquals(firstGraphString, "[bartender cop>is nsubj>Joe det>the]");
-	}
+    // Process and output modified
+    Collection<SemanticGraph> newSgs = pattern.execute(sg);
+    for (SemanticGraph newSg : newSgs)
+      System.out.println("Modified = "+newSg.toCompactString());
+    String firstGraphString = newSgs.iterator().next().toCompactString().trim();
+    assertEquals(firstGraphString, "[bartender cop>is nsubj>Joe det>the]");
+  }
 }
