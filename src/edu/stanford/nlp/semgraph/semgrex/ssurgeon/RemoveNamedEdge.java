aeccc44eb8bf6fc03111de3486e84e2fc079e618
John Bauer <horatio@gmail.com>
2023-03-01 14:21:24 -0800

    Remove obsolete documentation from RemoveNamedEdge

--- src/edu/stanford/nlp/semgraph/semgrex/ssurgeon/RemoveNamedEdge.java
+++ src/edu/stanford/nlp/semgraph/semgrex/ssurgeon/RemoveNamedEdge.java
@@ -8,8 +8,7 @@ import edu.stanford.nlp.semgraph.SemanticGraph;
 import edu.stanford.nlp.semgraph.SemanticGraphEdge;
 
 /**
- * Given a named edge, governor, and dependent, removes that edge
- * from the SemanticGraph.
+ * Given a named edge, removes that edge from the SemanticGraph.
  *
  * NOTE: you should manually reassign roots for dangling subtrees,
  * or delete them outright.  This does not perform any new root 
@@ -39,11 +38,6 @@ public class RemoveNamedEdge extends SsurgeonEdit {
 
   /**
    * Removes the named edge from the graph, if it exists.
-   *<br>
-   * TODO: it should not be necessary to have node names for this to work.
-   * Any edge that gets matched by the edge matcher should just work
-   * This operation is not used anywhere, even in RTE, so we should be
-   * able to change its semantics to not include the node names
    */
   @Override
   public boolean evaluate(SemanticGraph sg, SemgrexMatcher sm) {
