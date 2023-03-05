54294c623ee3e3dd2fafc5187b0eeb49f9b7f4c2
John Bauer <horatio@gmail.com>
2023-03-05 11:14:23 -0800

    Add utilities to check the min & max index of a SemanticGraph

--- test/src/edu/stanford/nlp/semgraph/SemanticGraphUtilsTest.java
+++ test/src/edu/stanford/nlp/semgraph/SemanticGraphUtilsTest.java
@@ -35,4 +35,34 @@ public class SemanticGraphUtilsTest {
     assertEquals("{word: ate; tag: null; ner: null}=ate  >subj=E1 {word: bill; tag: null; ner: null}=Bill", pat.trim());
   }
 
+  @Test
+  public void testMaxIndex() {
+    SemanticGraph sg = SemanticGraph.valueOf("[has-2 nsubj> Jennifer-1 obj> [antennae-4 dep> blue-3]]");
+    assertEquals(4, SemanticGraphUtils.maxIndex(sg));
+
+    // with a weird index
+    sg = SemanticGraph.valueOf("[has-2 nsubj> Jennifer-1 obj> [antennae-5 dep> blue-3]]");
+    assertEquals(5, SemanticGraphUtils.maxIndex(sg));
+
+    sg = SemanticGraph.valueOf("[has-1 nsubj> Jennifer-0 obj> [antennae-3 dep> blue-2]]");
+    assertEquals(3, SemanticGraphUtils.maxIndex(sg));
+
+    sg = SemanticGraph.valueOf("[foo-1]");
+    assertEquals(1, SemanticGraphUtils.maxIndex(sg));
+  }
+
+  @Test
+  public void testMinIndex() {
+    SemanticGraph sg = SemanticGraph.valueOf("[has-2 nsubj> Jennifer-1 obj> [antennae-4 dep> blue-3]]");
+    assertEquals(1, SemanticGraphUtils.minIndex(sg));
+
+    sg = SemanticGraph.valueOf("[has-2 nsubj> Jennifer-1 obj> [antennae-5 dep> blue-3]]");
+    assertEquals(1, SemanticGraphUtils.minIndex(sg));
+
+    sg = SemanticGraph.valueOf("[has-1 nsubj> Jennifer-0 obj> [antennae-3 dep> blue-2]]");
+    assertEquals(0, SemanticGraphUtils.minIndex(sg));
+
+    sg = SemanticGraph.valueOf("[foo-1]");
+    assertEquals(1, SemanticGraphUtils.minIndex(sg));
+  }
 }
