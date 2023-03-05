e3e01a5c231c4b6d0cf4d65aadbf3f948a322c54
John Bauer <horatio@gmail.com>
2023-03-05 11:10:46 -0800

    Make this test junit 4 and make it not hide failures

--- test/src/edu/stanford/nlp/semgraph/SemanticGraphUtilsTest.java
+++ test/src/edu/stanford/nlp/semgraph/SemanticGraphUtilsTest.java
@@ -1,5 +1,8 @@
 package edu.stanford.nlp.semgraph;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.List;
@@ -7,7 +10,6 @@ import java.util.Map;
 import java.util.Arrays;
 import java.util.function.Function;
 
-import junit.framework.TestCase;
 import edu.stanford.nlp.ling.IndexedWord;
 import edu.stanford.nlp.trees.EnglishGrammaticalRelations;
 import edu.stanford.nlp.trees.LabeledScoredTreeFactory;
@@ -19,23 +21,18 @@ import edu.stanford.nlp.util.Generics;
  *
  * @author Sonal Gupta
  */
-public class SemanticGraphUtilsTest extends TestCase {
+public class SemanticGraphUtilsTest {
 
-  SemanticGraph graph;
-
+  @Test
   public void testCreateSemgrexPattern(){
-    try{
     SemanticGraph graph = SemanticGraph.valueOf("[ate subj>Bill]");
 
     Function<IndexedWord, String> transformNode = o ->{
       return "{word: " + o.word().toLowerCase() + "; tag: " + o.tag() +"; ner: " + o.ner() + "}";
-      };
+    };
 
     String pat = SemanticGraphUtils.semgrexFromGraphOrderedNodes(graph, null, null, transformNode);
     assertEquals("{word: ate; tag: null; ner: null}=ate  >subj=E1 {word: bill; tag: null; ner: null}=Bill", pat.trim());
-    }catch(Exception e){
-      e.printStackTrace();
-    }
   }
 
 }
