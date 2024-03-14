fd6800faada86a3ef724baf5a7fa5f2016ba3474
John Bauer <horatio@gmail.com>
2024-03-13 18:32:43 -0700

    Tiny test of the UniversalPOSMapper

---
+++ test/src/edu/stanford/nlp/trees/UniversalPOSMapperTest.java
@@ -0,0 +1,17 @@
+package edu.stanford.nlp.trees;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import edu.stanford.nlp.trees.Tree;
+
+public class UniversalPOSMapperTest {
+  @Test
+  public void testMap() {
+    Tree tree = Tree.valueOf("(ROOT (S (NP (DT This)) (VP (VBZ is) (NP (DT a) (JJ simple) (NN test)))))");
+    Tree newTree = UniversalPOSMapper.mapTree(tree);
+    Tree expected = Tree.valueOf("(ROOT (S (NP (PRON This)) (VP (VERB is) (NP (DET a) (ADJ simple) (NOUN test)))))");
+    Assert.assertEquals(expected, newTree);
+  }
+}
+
